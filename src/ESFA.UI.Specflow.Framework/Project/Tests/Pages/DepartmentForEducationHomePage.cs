b1c25aaf060a50ccd3c4fd8ced13e47112ac0c18
SATYA <SATYA>
2017-09-08 15:30:40 +0100

    Ready to use framework with all required support classes

---
+++ src/ESFA.UI.Specflow.Framework/Project/Tests/Pages/DepartmentForEducationHomePage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ESFA.UI.Specflow.Framework.Project.Framework.Helpers;
+using ESFA.UI.Specflow.Framework.Project.Tests.TestSupport;
+using OpenQA.Selenium;
+
+namespace ESFA.UI.Specflow.Framework.Project.Tests.Pages
+{
+    class DepartmentForEducationHomePage : BasePage
+    {
+        private static String PAGE_TITLE = "Department\r\nfor Education";
+
+        public DepartmentForEducationHomePage(IWebDriver webDriver) : base(webDriver)
+        {
+            SelfVerify();
+        }
+
+        protected override bool SelfVerify()
+        {
+            return PageInteractionHelper.VerifyPageHeading(this.GetPageHeading(), PAGE_TITLE);
+        }
+    }
+}
