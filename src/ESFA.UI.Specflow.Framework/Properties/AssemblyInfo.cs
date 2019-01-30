c1b8bec424c1b429a2218b71d0d7e63da8b27d75
subhamano <45993702+subhamano@users.noreply.github.com>
2019-01-30 18:24:59 +0000

    Implementing the no of thread count in assembly level

--- src/ESFA.UI.Specflow.Framework/Properties/AssemblyInfo.cs
+++ src/ESFA.UI.Specflow.Framework/Properties/AssemblyInfo.cs
@@ -15,6 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: Parallelizable(ParallelScope.Fixtures)]
+[assembly: LevelOfParallelism(12)]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
