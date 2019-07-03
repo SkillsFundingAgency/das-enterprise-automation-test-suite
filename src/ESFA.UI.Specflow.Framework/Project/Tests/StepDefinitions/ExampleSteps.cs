d92834d7466e9088f561e771cd6b2c78f7003730
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2019-07-03 15:00:55 +0100

    resolved objects using .net core DI

--- src/ESFA.UI.Specflow.Framework/Project/Tests/StepDefinitions/ExampleSteps.cs
+++ src/ESFA.UI.Specflow.Framework/Project/Tests/StepDefinitions/ExampleSteps.cs
@@ -1,6 +1,4 @@
-﻿using BoDi;
-using ESFA.UI.Specflow.Framework.Project.Tests.Pages;
-using ESFA.UI.Specflow.Framework.Project.Tests.TestSupport;
+﻿using ESFA.UI.Specflow.Framework.Project.Tests.Pages;
 using OpenQA.Selenium;
 using TechTalk.SpecFlow;
 
@@ -10,39 +8,39 @@ namespace ESFA.UI.Specflow.Framework.Project.Tests.StepDefinitions
     public class ExampleSteps
     {
         private readonly ConfigurationOptions _configuration;
-        private readonly IWebDriver WebDriver;
+        private readonly IWebDriver _webDriver;
 
-        public ExampleSteps(ObjectContainer objectContainer)
+        public ExampleSteps(IWebDriver webDriver, ConfigurationOptions configuration)
         {
-            _configuration = objectContainer.Resolve<ConfigurationOptions>();
-            WebDriver = objectContainer.Resolve<IWebDriver>();
+            _configuration = configuration;
+            _webDriver = webDriver;
         }
 
         [Given(@"I navigate to GOV.UK home page")]
         public void NavigateToGovUkHomePage()
         {
             var url = _configuration.BaseUrl;
-            WebDriver.Url = url; 
+            _webDriver.Url = url; 
         }
 
         [When(@"I search for (.*)")]
         public void SearchForText(string searchText)
         {
-            var welcomeToGovUkPage = new WelcomeToGovUkPage(WebDriver);
+            var welcomeToGovUkPage = new WelcomeToGovUkPage(_webDriver);
             welcomeToGovUkPage.EnterSearchTextAndSubmit(searchText);
         }
 
         [When(@"I click on DFE link")]
         public void ClickOnDfeLink()
         {
-            var searchResultsPage = new SearchResultsPage(WebDriver);
+            var searchResultsPage = new SearchResultsPage(_webDriver);
             searchResultsPage.ClickDfeLink();
         }
 
         [Then(@"I should be on DFE home page")]
         public void ShouldBeOnDfeHomePage()
         {
-            var departmentForEducationHomePage = new DepartmentForEducationHomePage(WebDriver);
+            var departmentForEducationHomePage = new DepartmentForEducationHomePage(_webDriver);
             departmentForEducationHomePage.IsPageHeadingMacthing();
         }
     }
