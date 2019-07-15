fde5688cfa38de1d14413f94c37e78c71f745d34
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2019-07-15 16:16:15 +0100

    added example step to use the object Context

---
+++ src/ESFA.UI.Specflow.TestProject/Project/Tests/StepDefinitions/ExampleSteps1.cs
@@ -0,0 +1,30 @@
+﻿using ESFA.UI.Specflow.Framework.Project.Tests.Pages;
+using ESFA.UI.Specflow.Framework.TestSupport;
+using TestContext = NUnit.Framework.TestContext;
+using TechTalk.SpecFlow;
+
+namespace ESFA.UI.Specflow.TestProject.Project.Tests.StepDefinitions
+{
+    [Binding]
+    public class ExampleSteps1
+    {
+        private readonly ScenarioContext _context;
+        private readonly ObjectContext _objecContext;
+
+        public ExampleSteps1(ScenarioContext context, ObjectContext objecContext)
+        {
+            _context = context;
+            _objecContext = objecContext;
+        }
+
+        [When(@"I click the same link")]
+        public void WhenIClickTheSameLink()
+        {
+            var searchResultsPage = new SearchResultsPage(_context);
+            var searchText = _objecContext.Get("searchText");
+            TestContext.Progress.WriteLine($"Naivgating to {searchText} page");
+            searchResultsPage.ClickDfeLink(searchText);
+        }
+
+    }
+}
