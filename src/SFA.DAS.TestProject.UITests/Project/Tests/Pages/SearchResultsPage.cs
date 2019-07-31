08b526b469433ec5909e1a0eb3d830ba2a82b07a
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2019-07-31 10:49:43 +0100

    Fix failing tests

--- src/SFA.DAS.TestProject.UITests/Project/Tests/Pages/SearchResultsPage.cs
+++ src/SFA.DAS.TestProject.UITests/Project/Tests/Pages/SearchResultsPage.cs
@@ -3,7 +3,7 @@ using SFA.DAS.UI.FrameworkHelpers;
 using SFA.DAS.UI.Framework.TestSupport;
 using OpenQA.Selenium;
 using TechTalk.SpecFlow;
-
+using System.Linq;
 
 namespace SFA.DAS.TestProject.UITests.Project.Tests.Pages
 {
@@ -13,10 +13,16 @@ namespace SFA.DAS.TestProject.UITests.Project.Tests.Pages
         private readonly PageInteractionHelper _pageInteractionHelper;
         private readonly FormCompletionHelper _formCompletionHelper;
         private readonly ScenarioContext _context;
+        private readonly IWebDriver _webDriver;
         #endregion
 
         #region Page Object Elements
-        private By DfeUrl(string searchText) => By.LinkText(searchText);
+
+        private By DfeUrlLinks => By.CssSelector(".gem-c-document-list__item a");
+        private IWebElement FindDfeUrlElement(string searchText)
+        {
+            return _webDriver.FindElements(DfeUrlLinks).ToList().First(x => x.Text == searchText);
+        }
         #endregion
 
         protected override string PageTitle => "";
@@ -24,6 +30,7 @@ namespace SFA.DAS.TestProject.UITests.Project.Tests.Pages
         public SearchResultsPage(ScenarioContext context) : base(context)
         {
             _context = context;
+            _webDriver = context.GetWebDriver();
             _pageInteractionHelper = context.Get<PageInteractionHelper>();
             _formCompletionHelper = context.Get<FormCompletionHelper>();
             VerifyPage();
@@ -31,7 +38,7 @@ namespace SFA.DAS.TestProject.UITests.Project.Tests.Pages
 
         internal HomePage OpenDesiredPage(string searchText)
         {
-            _formCompletionHelper.ClickElement(DfeUrl(searchText));
+            _formCompletionHelper.ClickElement(FindDfeUrlElement(searchText));
             return new HomePage(_context);
         }
     }
