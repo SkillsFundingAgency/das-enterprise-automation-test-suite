607d9438a948694ec0319ceeae283b5e07091fa8
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2019-08-05 11:19:52 +0100

    added SFA.DAS.Approvals.UITests skeleton

---
+++ src/SFA.DAS.Approvals.UITests/Project/ProjectSpecificConfigurationSetup.cs
@@ -0,0 +1,30 @@
+﻿using SFA.DAS.UI.Framework.TestSupport;
+using SFA.DAS.UI.FrameworkHelpers;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Approvals.UITests.Project
+{
+    [Binding]
+    public class ProjectSpecificConfigurationSetup          
+    {
+        private readonly ScenarioContext _context;
+        private readonly ObjectContext _objectContext;
+        private readonly IConfigSection _configSection;
+
+        public ProjectSpecificConfigurationSetup(ScenarioContext context)
+        {
+            _context = context;
+            _configSection = context.Get<IConfigSection>();
+            _objectContext = context.Get<ObjectContext>();
+        }
+
+        [BeforeScenario(Order = 2)]
+        public void SetUpProjectSpecificConfiguration()
+        {
+            var config = _configSection.GetConfigSection<ProjectConfig>();
+            _context.SetProjectConfig(config);
+
+            _objectContext.SetBrowser(config.TP_Browser);
+        }
+    }
+}
