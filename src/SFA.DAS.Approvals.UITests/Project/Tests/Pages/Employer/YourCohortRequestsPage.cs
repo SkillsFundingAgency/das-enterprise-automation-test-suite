8360670389a0a3d3451de518fc1c26ac7a399dc5
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2019-09-02 15:04:11 +0100

    added more pages

--- src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Employer/YourCohortRequestsPage.cs
+++ src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Employer/YourCohortRequestsPage.cs
@@ -40,37 +40,5 @@ namespace SFA.DAS.Approvals.UITests.Project.Tests.Pages.Employer
             throw new Exception("No cohorts available for review");
         }
     }
-
-    public class CohortsForReviewPage : BasePage
-    {
-        protected override string PageTitle => "Your cohort requests";
-
-        #region Helpers and Context
-        private readonly PageInteractionHelper _pageInteractionHelper;
-        private readonly FormCompletionHelper _formCompletionHelper;
-        private readonly ScenarioContext _context;
-        private readonly ApprovalsConfig _config;
-        private readonly ObjectContext _objectContext;
-        private readonly TableRowHelper _tableRowHelper;
-        #endregion
-
-
-        public CohortsForReviewPage(ScenarioContext context) : base(context)
-        {
-            _context = context;
-            _objectContext = context.Get<ObjectContext>();
-            _config = context.GetApprovalsConfig<ApprovalsConfig>();
-            _pageInteractionHelper = context.Get<PageInteractionHelper>();
-            _formCompletionHelper = context.Get<FormCompletionHelper>();
-            _tableRowHelper = context.Get<TableRowHelper>();
-            VerifyPage();
-        }
-
-        public ReviewYourCohortPage SelectViewCurrentCohortDetails()
-        {
-            _tableRowHelper.SelectRowFromTable("Details", _objectContext.GetCohortReference());
-            return new ReviewYourCohortPage(_context);
-        }
-    }
 }
 
--- src/SFA.DAS.Registration.UITests/Project/Tests/StepDefinitions/ExistingAccountSteps.cs
+++ src/SFA.DAS.Registration.UITests/Project/Tests/StepDefinitions/ExistingAccountSteps.cs
@@ -1,7 +1,4 @@
-﻿using NUnit.Framework;
-using SFA.DAS.Registration.UITests.Project.Tests.Pages;
-using SFA.DAS.UI.Framework.TestSupport;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 using TechTalk.SpecFlow;
@@ -48,37 +45,6 @@ namespace SFA.DAS.Registration.UITests.Project.Tests.StepDefinitions
             _loginHelper.SetLoginCredentials(eoiUser);
 
             _loginHelper.Login(eoiUser);
-        }
-    }
-
-    public class LoginHelper
-    {
-        private readonly ScenarioContext _context;
-        private readonly ObjectContext _objectContext;
-
-        public LoginHelper(ScenarioContext context)
-        {
-            _context = context;
-            _objectContext = context.Get<ObjectContext>();
-        }
-
-        public bool IsIndexPageDisplayed()
-        {
-            return new CheckIndexPage(_context)
-                .IsIndexPageDisplayed();
-        }
-
-        public HomePage Login(LoginUser loginUser)
-        {
-            return new IndexPage(_context)
-                .SignIn()
-                .Login(loginUser);
-        }
-
-        internal void SetLoginCredentials(LoginUser loginUser)
-        {
-            _objectContext.SetLoginCredentials(loginUser.Username, loginUser.Password);
-            TestContext.Progress.WriteLine($"Email : {loginUser.Username}");
         }
     }
 }
