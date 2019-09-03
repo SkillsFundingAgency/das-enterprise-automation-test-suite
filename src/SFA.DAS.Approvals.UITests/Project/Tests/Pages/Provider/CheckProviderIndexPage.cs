951b1215b46739160b2c2e9f618c8b697b2dd3bc
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2019-09-03 16:59:27 +0100

    Added ability to check page before invoking any action

---
+++ src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Provider/CheckProviderIndexPage.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using SFA.DAS.Registration.UITests.Project.Tests.Pages;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Approvals.UITests.Project.Tests.Pages.Provider
+{
+    public class CheckProviderIndexPage : CheckPage
+    {
+        protected override string PageTitle { get; }
+
+        protected override By Identifier => By.CssSelector(".button-start");
+
+        public CheckProviderIndexPage(ScenarioContext context) : base(context)
+        {
+
+        }
+    }
+}
---
+++ src/SFA.DAS.Registration.UITests/Project/Tests/Pages/CheckPage.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using SFA.DAS.UI.Framework.TestSupport;
+using SFA.DAS.UI.FrameworkHelpers;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Registration.UITests.Project.Tests.Pages
+{
+    public abstract class CheckPage : BasePage
+    {
+        protected override string PageTitle { get; }
+
+        protected abstract By Identifier { get; }
+
+        #region Helpers and Context
+        private readonly PageInteractionHelper _pageInteractionHelper;
+        #endregion
+
+        public CheckPage(ScenarioContext context) : base(context)
+        {
+            _pageInteractionHelper = context.Get<PageInteractionHelper>();
+        }
+        public bool IsIndexPageDisplayed()
+        {
+            return _pageInteractionHelper.IsElementDisplayed(Identifier);
+        }
+    }
+}
