4092d56f849cfc7669e2ecd8a9fe35bdb72f64bd
Subha Manoharan <subha.manoharan@digital.education.gov.uk>
2019-10-09 14:01:55 +0100

    initial commit

---
+++ src/SFA.DAS.ManageFunding.UITests/Project/Helpers/StepsHelper/EmployerReservationStepsHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+using SFA.DAS.Registration.UITests.Project.Helpers;
+using SFA.DAS.Registration.UITests.Project;
+using SFA.DAS.UI.Framework.TestSupport;
+using SFA.DAS.ManageFunding.UITests.Project.Tests.Pages;
+using SFA.DAS.ManageFunding.UITests.Project.Tests.Pages.Employer;
+
+namespace SFA.DAS.ManageFunding.UITests.Project.Helpers.StepsHelper
+{
+    internal class EmployerReservationStepsHelper
+    {
+        private readonly ObjectContext _objectContext;
+        private readonly ScenarioContext _context;
+        private readonly RegistrationConfig _registrationConfig;
+        private readonly EmployerPortalLoginHelper _loginHelper;
+
+        internal EmployerReservationStepsHelper(ScenarioContext context)
+        {
+            _context = context;
+            _objectContext = _context.Get<ObjectContext>();
+            _registrationConfig = _context.GetRegistrationConfig<RegistrationConfig>();
+            _loginHelper = new EmployerPortalLoginHelper(_context);
+        }
+
+        internal MakingChangesPage CreateReservation()
+        {
+            return new YourFundingReservationsHomePage(_context).OpenYourFundingReservations()
+                .ClickReserveMoreFundingLink()
+                .ClickReserveFundingButton()
+                .ClickYesRadioButton()
+                .EnterSelectForACourseAndSubmit()
+                .ClickSaveAndContinueButton()
+                .ClickMonthRadioButton()
+                .ClickSaveAndContinueButton()
+                .ClickYesReserveFundingNowRadioButton()
+                .ClickConfirmButton();
+        }
+    }
+}
--- src/SFA.DAS.ManageFunding.UITests/Project/Tests/StepDefinitions/ManageFundingNonLevyEoiEmployerSteps.cs
+++ src/SFA.DAS.ManageFunding.UITests/Project/Tests/StepDefinitions/ManageFundingNonLevyEoiEmployerSteps.cs
@@ -1,4 +1,5 @@
-﻿using SFA.DAS.ManageFunding.UITests.Project.Tests.Pages.Employer;
+﻿using SFA.DAS.ManageFunding.UITests.Project.Helpers.StepsHelper;
+using SFA.DAS.ManageFunding.UITests.Project.Tests.Pages.Employer;
 using SFA.DAS.ManageFunding.UITests.Project.Tests.Pages;
 using SFA.DAS.Registration.UITests.Project;
 using SFA.DAS.Registration.UITests.Project.Helpers;
@@ -17,33 +18,34 @@ namespace SFA.DAS.ManageFunding.UITests.Project.Tests.StepDefinitions
         private readonly ScenarioContext _context;
         private MakingChangesPage _sucessMessage;
         private readonly EmployerPortalLoginHelper _loginHelper;
+        private readonly EmployerReservationStepsHelper _reservationHelper;
 
         public ManageFundingNonLevyEoiEmployerSteps(ScenarioContext context)
         {
             _objectContext = context.Get<ObjectContext>();
             _context = context;
             _loginHelper = new EmployerPortalLoginHelper(context);
+            _reservationHelper = new EmployerReservationStepsHelper(context);
         }
 
         [When(@"the Employer reserves funding for an apprenticeship course")]
         public void TheEmployerReservesFundingForAnApprenticeshipCourse()
         {
-            _sucessMessage = new YourFundingReservationsHomePage(_context).OpenYourFundingReservations()
-                .ClickReserveMoreFundingLink()
-                .ClickReserveFundingButton()
-                .ClickYesRadioButton()
-                .EnterSelectForACourseAndSubmit()
-                .ClickSaveAndContinueButton()
-                .ClickMonthRadioButton()
-                .ClickSaveAndContinueButton()
-                .ClickYesReserveFundingNowRadioButton()
-                .ClickConfirmButton();
+            _reservationHelper.CreateReservation();
         }
 
         [Then(@"Verify funding is successfully reserved")]
         public void VerifyFundingIsSuccessfullyReserved()
         {
             _sucessMessage.IsReserveFundingSuccessMessageUpdated();
+        }
+
+
+        [Given(@"the Employer has created a reservation")]
+        public void GivenTheEmployerHasCreatedAReservation()
+        {
+            _loginHelper.Login(_context.GetUser<EoiUser>(), false);
+            _reservationHelper.CreateReservation();
         }
     }
 }
