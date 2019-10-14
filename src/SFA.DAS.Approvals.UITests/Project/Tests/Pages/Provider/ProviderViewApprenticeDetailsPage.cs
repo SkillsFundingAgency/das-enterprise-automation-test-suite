2eb4e9a6681900ab14dee8463af3f882e3d6b645
SreekanthBadigenchula <sreekanth.badigenchula@digital.education.gov.uk>
2019-10-14 14:44:26 +0100

    Added Automation-96 script (Provider views cohort details)

---
+++ src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Provider/ProviderViewApprenticeDetailsPage.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using SFA.DAS.UI.Framework.TestSupport;
+using SFA.DAS.UI.FrameworkHelpers;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Approvals.UITests.Project.Tests.Pages.Provider
+{
+    class ProviderViewApprenticeDetailsPage : BasePage
+    {
+        protected override string PageTitle => "View apprentice details";
+
+        #region Helpers and Context
+        private readonly FormCompletionHelper _formCompletionHelper;
+        private readonly ScenarioContext _context;
+        #endregion
+
+        public ProviderViewApprenticeDetailsPage(ScenarioContext context) : base(context)
+        {
+            _context = context;
+            _formCompletionHelper = context.Get<FormCompletionHelper>();
+            VerifyPage();
+        }
+
+        private By returnToCohortViewLink = By.LinkText("Return to cohort view");
+
+        internal ProviderViewYourCohortPage SelectReturnToCohortView()
+        {
+            _formCompletionHelper.ClickElement(returnToCohortViewLink);
+            return new ProviderViewYourCohortPage(_context);
+        }
+    }
+}
---
+++ src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Provider/ProviderViewYourCohortPage.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using SFA.DAS.UI.Framework.TestSupport;
+using SFA.DAS.UI.FrameworkHelpers;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Approvals.UITests.Project.Tests.Pages.Provider
+{
+    class ProviderViewYourCohortPage : BasePage
+    {
+        protected override string PageTitle => "View your cohort";
+        private By viewApprenticeLink = By.LinkText("View");
+
+        #region Helpers and Context
+        private readonly FormCompletionHelper _formCompletionHelper;
+        private readonly PageInteractionHelper _pageInteractionHelper;
+        private readonly ScenarioContext _context;
+        #endregion
+
+        public ProviderViewYourCohortPage(ScenarioContext context) : base(context)
+        {
+            _context = context;
+            _pageInteractionHelper = context.Get<PageInteractionHelper>();
+            _formCompletionHelper = context.Get<FormCompletionHelper>();
+            VerifyPage();
+        }
+
+        public int TotalNoOfApprentices()
+        {
+            return _pageInteractionHelper.FindElements(By.LinkText("View")).Count;
+        }
+
+        internal ProviderViewApprenticeDetailsPage SelectViewApprentice(int apprenticeNumber = 0)
+        {
+            IList<IWebElement> viewApprenticeLinks = _pageInteractionHelper.FindElements(viewApprenticeLink);
+            _formCompletionHelper.ClickElement(viewApprenticeLinks[apprenticeNumber]);
+            return new ProviderViewApprenticeDetailsPage(_context);
+        }
+    }
+}
