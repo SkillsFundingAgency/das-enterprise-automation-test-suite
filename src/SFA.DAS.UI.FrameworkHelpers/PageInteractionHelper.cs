e417d2ab201d79d065ee58218bcc4e0f3a3bd427
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2019-11-04 15:35:26 +0000

    code review changes

--- src/SFA.DAS.UI.FrameworkHelpers/PageInteractionHelper.cs
+++ src/SFA.DAS.UI.FrameworkHelpers/PageInteractionHelper.cs
@@ -7,45 +7,6 @@ using System.Linq;
 
 namespace SFA.DAS.UI.FrameworkHelpers
 {
-    public abstract class WebElementInteractionHelper
-    {
-        private readonly IWebDriver _webDriver;
-
-        protected By LinkCssSelector => By.CssSelector("a");
-
-        protected By ButtonCssSelector => By.CssSelector(".button");
-
-        protected By RadioButtonCssSelector => By.CssSelector("label.selection-button-radio");
-
-        protected By CheckBoxCssSelector => By.CssSelector("label.selection-button-checkbox");
-
-        public WebElementInteractionHelper(IWebDriver webDriver)
-        {
-            _webDriver = webDriver;
-        }
-
-        protected IWebElement GetElementByText(By locator, String text)
-        {
-            IList<IWebElement> elements = _webDriver.FindElements(locator);
-
-            for (int i = 0; i < elements.Count; i++)
-            {
-                String str = elements.ElementAt(i).Text ?? elements.ElementAt(i).GetAttribute("innertext");
-                if (str.Contains(text))
-                {
-                    return elements.ElementAt(i);
-                }
-            }
-            return null;
-        }
-
-        protected SelectElement SelectElement(IWebElement element)
-        {
-            return new SelectElement(element);
-        }
-    }
-
-
     public class PageInteractionHelper : WebElementInteractionHelper
     {
         private readonly IWebDriver _webDriver;
