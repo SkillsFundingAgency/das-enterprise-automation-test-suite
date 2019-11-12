eafa469c7752de566ad46cf369d8792a8d07c349
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2019-11-12 14:40:07 +0000

    updated pages

---
+++ src/SFA.DAS.RAA-V1.UITests/Project/Tests/Pages/RAA/RAA_ChangeVacancyDatePreviewPage.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using SFA.DAS.UI.Framework.TestSupport;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.RAA_V1.UITests.Project.Tests.Pages.RAA
+{
+    public class RAA_ChangeVacancyDatePreviewPage : BasePage
+    {
+        protected override By PageHeader => By.CssSelector("#SuccessMessageText");
+
+        protected override string PageTitle => "Your changes have been saved successfully";
+
+        public RAA_ChangeVacancyDatePreviewPage(ScenarioContext context) : base(context)
+        {
+            VerifyPage();
+        }
+    }
+}
