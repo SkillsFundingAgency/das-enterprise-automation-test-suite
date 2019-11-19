4183cfcc54d3005cfaa02ebc4fdc81eef5b4b646
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2019-11-19 18:18:35 +0000

    simplified cosmos helper

--- src/SFA.DAS.UI.FrameworkHelpers/CosmosActionsPerformerHelper.cs
+++ src/SFA.DAS.UI.FrameworkHelpers/CosmosActionsPerformerHelper.cs
@@ -8,35 +8,20 @@ using SFA.DAS.CosmosDb;
 
 namespace SFA.DAS.UI.FrameworkHelpers
 {
-    public class CosmoDbInfo
-    {
-        public CosmoDbInfo(string url, string authKey, string dbName, string collectionName, bool isPartitionKey, object partitionKey)
-        {
-            Url = url;
-            AuthKey = authKey;
-            DbName = dbName;
-            CollectionName = collectionName;
-            IsPartitionKey = isPartitionKey;
-            PartitionKey = partitionKey;
-        }
-
-        public string Url { get; private set; }
-        public string AuthKey { get; private set; }
-        public string DbName { get; private set; }
-        public string CollectionName { get; private set; }
-        public bool IsPartitionKey { get; private set; }
-        public object PartitionKey { get; private set; }
-    }
-
     public class CosmosActionsPerformerHelper
     {
         public static void RemoveProviderPermissionDoc(string url, string authKey, string dbName, string collectionName, long ukprn, object partitionKey)
         {
-            var (docs, requestOptions, db) = QueryDb(new CosmoDbInfo(url, authKey, dbName, collectionName, true, partitionKey), (a) => a.Ukprn == ukprn);
-            RemoveDoc(docs, requestOptions, db);
+            var db = CosmosConnectionHelper.CreateCosmosDbRepoHelper<Aple>(url, authKey, dbName, collectionName);
+
+            var docs = Query(db, (a) => a.Ukprn == ukprn);
+            
+            var requestOptions = new RequestOptions { PartitionKey = new PartitionKey(partitionKey) };
+
+            RemoveDoc(db, docs, requestOptions);
         }
 
-        private static void RemoveDoc(IQueryable<Aple> docs, RequestOptions requestOptions, DocumentRepository<Aple> db)
+        private static void RemoveDoc(DocumentRepository<Aple> db, IQueryable<Aple> docs,  RequestOptions requestOptions)
         {
             foreach (var doc in docs)
             {
@@ -44,35 +29,7 @@ namespace SFA.DAS.UI.FrameworkHelpers
             }
         }
 
-        private static (IQueryable<Aple> , RequestOptions, DocumentRepository<Aple>) QueryDb(CosmoDbInfo cosmoDbInfo, Expression<Func<Aple, bool>> expression)
-        {
-            var documentrepository = PermissionsDocumentRepository(cosmoDbInfo.Url, cosmoDbInfo.AuthKey, cosmoDbInfo.DbName, cosmoDbInfo.CollectionName);
-
-            var docsfound = Query(documentrepository, expression);
-
-            var queryrequestOptions = QueryRequestOptions(cosmoDbInfo.IsPartitionKey, cosmoDbInfo.PartitionKey);
-
-            return (docsfound, queryrequestOptions, documentrepository);
-        }
-
-        private static DocumentRepository<Aple> PermissionsDocumentRepository(string url, string authKey, string dbName, string collectionName) => CosmosConnectionHelper.CreateCosmosDbRepoHelper<Aple>(url, authKey, dbName, collectionName);
-
-        private static IQueryable<Aple> Query(DocumentRepository<Aple> db, Expression<Func<Aple,bool>> expression)
-        {
-            var option = new FeedOptions { EnableCrossPartitionQuery = true };
-
-            return db.CreateQuery(option).Select(x => x).Where(expression);
-        }
-
-        private static RequestOptions QueryRequestOptions(bool isPartitionKey, object partitionKey)
-        {
-            var requestOptions = new RequestOptions();
-            if (isPartitionKey)
-            {
-                requestOptions.PartitionKey = new PartitionKey(partitionKey);
-            }
-            return requestOptions;
-        }
+        private static IQueryable<Aple> Query(DocumentRepository<Aple> db, Expression<Func<Aple,bool>> expression) => db.CreateQuery(new FeedOptions { EnableCrossPartitionQuery = true }).Select(x => x).Where(expression);
 
         // Add more properties in Aple as required
         class Aple : Document
