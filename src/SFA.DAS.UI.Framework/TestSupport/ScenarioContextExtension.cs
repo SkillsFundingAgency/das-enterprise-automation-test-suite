c4bdd3463b82c8dc5fb0240997af82a3967826d1
SreekanthBadigenchula <sreekanth.badigenchula@digital.education.gov.uk>
2019-12-06 23:43:12 +0000

    formats

--- src/SFA.DAS.UI.Framework/TestSupport/ScenarioContextExtension.cs
+++ src/SFA.DAS.UI.Framework/TestSupport/ScenarioContextExtension.cs
@@ -7,17 +7,17 @@ namespace SFA.DAS.UI.Framework.TestSupport
     public static class ScenarioContextExtension
     {
         #region Constants
-        private const string TestProjectConfigKey = "testprojectconfig";
-        private const string RegistrationProjectConfigKey = "registrationprojectconfig";
-        private const string SupportConsoleProjectConfigKey = "SupportConsoleprojectconfig";
+        private const string TestProjectConfigKey = "testprojectconfigkey";
+        private const string RegistrationProjectConfigKey = "registrationprojectconfigkey";
+        private const string SupportConsoleProjectConfigKey = "supportconsoleprojectconfigkey";
         private const string RAAV1ProjectConfigKey = "raav1projectconfigkey";
-        private const string FAAProjectConfigKey = "fAAprojectconfigKey";
-        private const string ApprovalsProjectConfigKey = "approvalsprojectconfig";
+        private const string FAAProjectConfigKey = "faaprojectconfigkey";
+        private const string ApprovalsProjectConfigKey = "approvalsprojectconfigkey";
         private const string ProviderPermissionConfigKey = "providerpermissionconfigkey";
-        private const string TransfersProjectConfigKey = "transfersprojectconfig";
+        private const string TransfersProjectConfigKey = "transfersprojectconfigkey";
         private const string FATProjectConfigKey = "fatprojectconfigkey";
-        private const string MongoDbConfigKey = "mongodbconfig";
-        private const string WebDriverKey = "webdriver";
+        private const string MongoDbConfigKey = "mongodbconfigkey";
+        private const string WebDriverKey = "webdriverkey";
         #endregion
 
         public static void SetTestProjectConfig<T>(this ScenarioContext context, T value)
