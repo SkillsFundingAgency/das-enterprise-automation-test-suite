5bcbbfe5909b2acf3d235c5f0ca834ea83105cb9
SreekanthBadigenchula <sreekanth.badigenchula@digital.education.gov.uk>
2019-12-16 13:32:15 +0000

    Created basic project structure files

---
+++ src/SFA.DAS.EPAO.UITests/Project/EPAOConfigurationSetup.cs
@@ -0,0 +1,25 @@
+﻿using SFA.DAS.UI.Framework.TestSupport;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.EPAO.UITests.Project
+{
+    [Binding]
+    public class EPAOConfigurationSetup
+    {
+        private readonly ScenarioContext _context;
+        private readonly IConfigSection _configSection;
+
+        public EPAOConfigurationSetup(ScenarioContext context)
+        {
+            _context = context;
+            _configSection = context.Get<IConfigSection>();
+        }
+
+        [BeforeScenario(Order = 2)]
+        public void SetUpSupportConsoleProjectConfiguration()
+        {
+            var config = _configSection.GetConfigSection<EPAOConfig>();
+            _context.SetEPAOConfig(config);
+        }
+    }
+}
