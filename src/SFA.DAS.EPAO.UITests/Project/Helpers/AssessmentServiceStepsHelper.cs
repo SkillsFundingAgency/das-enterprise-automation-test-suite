60f6a0ffad18311e1a256f4b1fc4d7c1bcfc2660
SreekanthBadigenchula <sreekanth.badigenchula@digital.education.gov.uk>
2020-01-14 11:46:24 +0000

    Removed Address search from AutoSuggest options logic as this was intermittently failing the tests

--- src/SFA.DAS.EPAO.UITests/Project/Helpers/AssessmentServiceStepsHelper.cs
+++ src/SFA.DAS.EPAO.UITests/Project/Helpers/AssessmentServiceStepsHelper.cs
@@ -27,10 +27,10 @@ namespace SFA.DAS.EPAO.UITests.Project.Helpers
             new AS_WhatGradePage(_context).SelectGradeAndEnterDate(grade);
 
             if (grade == "Passed")
-                new AS_SearchEmployerAddressPage(_context).SearchAndSelectEmployerAddress()
-               .ClickContinueInSearchEmployerAddressPage()
-               .ClickContinueInConfirmEmployerAddressPage()
-               .EnterRecipientDetailsAndContinue();
+            new AS_SearchEmployerAddressPage(_context).ClickEnterAddressManuallyLinkInSearchEmployerPage()
+                .EnterEmployerAddressAndContinue()
+                .ClickContinueInConfirmEmployerAddressPage()
+                .EnterRecipientDetailsAndContinue();
         }
 
         public void CertifyPrivatelyFundedApprentice(bool invalidDateScenario = false)
