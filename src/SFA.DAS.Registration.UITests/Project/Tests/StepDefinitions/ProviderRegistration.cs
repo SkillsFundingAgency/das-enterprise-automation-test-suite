4045a9d228da54b3e32458f59db0491c55b1799b
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2020-01-15 10:19:18 +0000

    draft scenario - RE_CA_01A Provider Registration

---
+++ src/SFA.DAS.Registration.UITests/Project/Tests/StepDefinitions/ProviderRegistration.cs
@@ -0,0 +1,22 @@
+﻿using SFA.DAS.ConfigurationBuilder;
+using SFA.DAS.Registration.UITests.Project.Helpers;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Registration.UITests.Project.Tests.StepDefinitions
+{
+    [Binding]
+    public class ProviderRegistration
+    {
+        private readonly ScenarioContext _context;
+        private readonly ObjectContext _objectContext;
+        private readonly RegistrationDatahelpers _dataHelper;
+
+        public ProviderRegistration(ScenarioContext context)
+        {
+            _context = context;
+            _objectContext = context.Get<ObjectContext>();
+            _dataHelper = context.Get<RegistrationDatahelpers>();
+        }
+
+    }
+}
