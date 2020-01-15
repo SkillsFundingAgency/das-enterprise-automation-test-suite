41eb155c1f72d214b3b8970f47e15980dc991eb2
Sultan <sultan.mahmood@education.gov.uk>
2020-01-15 10:59:00 +0000

    Initial Jamal changes

---
+++ src/SFA.DAS.Campaigns.UITests/Project/Tests/Features/Apprenticeships/VerifyJamalPanelVideo.feature
@@ -0,0 +1,11 @@
+﻿Feature: VerifyJamalPanelVideo
+	As a user
+	I want to be able to navigate to Fire It Up home page
+	So that I can use the benefits of apprentice services and information 
+
+@regression
+Scenario: User verifies Jamal Panel
+	Given I navigate to Fire It Up home page
+	And I launch the Jamal page
+	Then I verify the content on jamal Panel
+	And I can play the first video on the screen
---
+++ src/SFA.DAS.Campaigns.UITests/Project/Tests/Pages/JamalTheCallingPage.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using SFA.DAS.UI.Framework.TestSupport;
+using SFA.DAS.UI.FrameworkHelpers;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Campaigns.UITests.Project.Tests.Pages
+{
+    internal class JamalTheCallingPage: BasePage
+    {
+
+        protected override string PageTitle => "";
+        #region Helpers and Context
+        private readonly PageInteractionHelper _pageInteractionHelper;
+        private readonly FormCompletionHelper _formCompletionHelper;
+        private readonly ScenarioContext _context;
+        #endregion
+
+        #region Constants
+        private const string ExpectedTheCallingHeader = "";
+        #endregion
+
+        #region Page Objects Elements
+        private readonly By _hireAnApprenticeHeader = By.ClassName("heading-xl");
+        #endregion
+
+        public JamalTheCallingPage(ScenarioContext context) : base(context)
+        {
+            _context = context;
+            _pageInteractionHelper = context.Get<PageInteractionHelper>();
+            _formCompletionHelper = context.Get<FormCompletionHelper>();
+        }
+
+    }
+}
