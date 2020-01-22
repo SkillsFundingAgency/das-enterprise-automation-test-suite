625226dd976c7343823c1da1537916f6a2a51774
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2020-01-22 10:08:08 +0000

    added Roatp feature file

---
+++ src/SFA.DAS.Roatp.UITests/Project/Tests/Features/E2EJourney/RP_E2E_01.feature
@@ -0,0 +1,7 @@
+﻿Feature: RP_E2E_01
+
+Scenario: RP_E2E_01_MainRoute-Company
+Given the provider initates an application as main route company
+When the provider completes Your Organisation section
+And the provider completes Criminal and Compliance section
+Then the provider completes Finish section
---
+++ src/SFA.DAS.Roatp.UITests/Project/Tests/StepDefinitions/End2EndSteps.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Roatp.UITests.Project.Tests.StepDefinitions
+{
+    [Binding]
+    public class End2EndSteps
+    {
+        [Given(@"the provider initates an application as main route company")]
+        public void GivenTheProviderInitatesAnApplicationAsMainRouteCompany()
+        {
+            throw new PendingStepException();
+        }
+
+    }
+}
