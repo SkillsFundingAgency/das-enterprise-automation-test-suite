f919d0b92ba64ec4705c3b399451f27171062333
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2020-01-22 10:21:42 +0000

    added ClearDownDataHelpers

---
+++ src/SFA.DAS.Roatp.UITests/Project/Helpers/ClearDownDataHelpers.cs
@@ -0,0 +1,62 @@
+﻿using SFA.DAS.UI.FrameworkHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFA.DAS.Roatp.UITests.Project.Helpers
+{
+    public class ClearDownDataHelpers
+    {
+        private readonly SqlDatabaseConnectionHelper _sqlDatabase;
+        public readonly string _roatpDatabaseConnectionString;
+        public readonly string _applyDatabaseConnectionString;
+        public readonly string _qnaDatabaseConnectionString;
+
+        public ClearDownDataHelpers(RoatpConfig roatpConfig, SqlDatabaseConnectionHelper sqlDatabase)
+        {
+            _sqlDatabase = sqlDatabase;
+            _roatpDatabaseConnectionString = roatpConfig.RoatpDatabaseConnectionString;
+            _applyDatabaseConnectionString = roatpConfig.ApplyDatabaseConnectionString;
+            _qnaDatabaseConnectionString = roatpConfig.QnaDatabaseConnectionString;
+        }
+
+        public void DeleteTrainingProvider(String ukprn)
+        {
+            String DeleteProviderQuery = $"DELETE FROM Organisations WHERE UKPRN ='{ukprn}'";
+            _sqlDatabase.ExecuteSqlCommand(DeleteProviderQuery, _roatpDatabaseConnectionString);
+        }
+        public string ClearDownDataFromApply(String email)
+        {
+            String GetApplicationIdQuery = $"SELECT ApplicationId from dbo.Apply a " +
+                                           $"inner join Contacts c on a.OrganisationId = c.ApplyOrganisationID " +
+                                           $"where c.Email ='{email}' ";
+
+            var queryResult = _sqlDatabase.ReadDataFromDataBase(GetApplicationIdQuery, _applyDatabaseConnectionString);
+
+            if (queryResult == null || queryResult.Count == 0)
+            {
+                return Guid.Empty.ToString();
+            }
+
+            String applicationId = queryResult[0][0].ToString();
+
+            String DeleteDataFromApplyQuery = $"DECLARE @OrganisationID UNIQUEIDENTIFIER; DECLARE @Email VARCHAR(256) ; SET @Email = '{email}'	;" +
+                $"SELECT @OrganisationID = ApplyOrganisationId FROM dbo.Contacts WHERE Email = @Email ;" +
+                $"DELETE FROM dbo.Apply WHERE ApplicationId = '{applicationId}' ;" +
+                $"UPDATE dbo.Contacts SET ApplyOrganisationID = NULL WHERE Email = @Email ;" +
+                $"DELETE FROM dbo.Organisations WHERE Id = @OrganisationID ;";
+            _sqlDatabase.ExecuteSqlCommand(DeleteDataFromApplyQuery, _applyDatabaseConnectionString);
+
+            return applicationId;
+        }
+        public void ClearDownDataFromQna(string applicationId)
+        {
+            String DeleteDataFromQnaQuery =
+                $"DELETE FROM ApplicationSections WHERE applicationid = '{applicationId}'" +
+                $"DELETE FROM ApplicationSequences WHERE applicationid = '{applicationId}' " +
+                $"DELETE FROM Applications WHERE id = '{applicationId}' ;";
+
+            _sqlDatabase.ExecuteSqlCommand(DeleteDataFromQnaQuery, _qnaDatabaseConnectionString);
+        }
+    }
+}
---
+++ src/SFA.DAS.Roatp.UITests/Project/Hooks.cs
@@ -0,0 +1,24 @@
+﻿using SFA.DAS.ConfigurationBuilder;
+using SFA.DAS.UI.Framework.TestSupport;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Roatp.UITests.Project
+{
+    [Binding]
+    public class Hooks
+    {
+        private readonly ScenarioContext _context;
+        private readonly RoatpConfig _config;
+        private readonly ObjectContext _objectContext;
+        
+        public Hooks(ScenarioContext context)
+        {
+            _context = context;
+            _config = context.GetRoatpConfig<RoatpConfig>();
+            _objectContext = context.Get<ObjectContext>();
+        }
+    }
+}
