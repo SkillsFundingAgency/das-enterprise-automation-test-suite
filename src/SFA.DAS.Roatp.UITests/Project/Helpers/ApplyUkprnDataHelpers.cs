a66bbca1f159dffe0214bbe2f463e738c3548cec
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2020-02-13 10:58:19 +0000

    updated test data

--- src/SFA.DAS.Roatp.UITests/Project/Helpers/ApplyUkprnDataHelpers.cs
+++ src/SFA.DAS.Roatp.UITests/Project/Helpers/ApplyUkprnDataHelpers.cs
@@ -87,14 +87,14 @@ namespace SFA.DAS.Roatp.UITests.Project.Helpers
             _applyData.Add("rps107",
                 new List<KeyValuePair<string, string>>
                 {
-                    new KeyValuePair<string, string>(emailkey, ""),
-                    new KeyValuePair<string, string>(ukprnkey, ""),
+                    new KeyValuePair<string, string>(emailkey, "sudhakar.chinoor+D7@digital.education.gov.uk"),
+                    new KeyValuePair<string, string>(ukprnkey, "10056340"),
                 });
             _applyData.Add("rps108",
                 new List<KeyValuePair<string, string>>
                 {
-                    new KeyValuePair<string, string>(emailkey, ""),
-                    new KeyValuePair<string, string>(ukprnkey, ""),
+                    new KeyValuePair<string, string>(emailkey, "sudhakar.chinoor+D8@digital.education.gov.uk"),
+                    new KeyValuePair<string, string>(ukprnkey, "10057614"),
                 });
         }
     }
