ffadc500d4ce4ce3229ed966357d7a7633285295
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2020-02-24 12:23:06 +0000

    updated step definition

--- src/SFA.DAS.EmployerFinance.UITests/Project/Tests/StepDefinitions/EmployerFinanceSteps.cs
+++ src/SFA.DAS.EmployerFinance.UITests/Project/Tests/StepDefinitions/EmployerFinanceSteps.cs
@@ -15,13 +15,13 @@ namespace SFA.DAS.EmployerFinance.UITests.Project.Tests.StepDefinitions
         [Then(@"'Check funding availability and make a reservation' link is displayed on the Employer Home Page")]
         public void ThenLinkIsDisplayedOnTheEmployerHomePage() => _homePageFinancesSectionPage = new HomePageFinancesSection(_context).VerifyFundingAvailabilityLink();
 
-        [Then(@"'(Your funding reservations)' and '(Your finances)' links are displayed in the Finances section")]
+        [Then(@"'Your funding reservations' and 'Your finances' links are displayed in the Finances section")]
         public void ThenAndLinksAreDisplayedInTheFinancesSection() => _homePageFinancesSectionPage = _homePageFinancesSectionPage.VerifyYourFinancesSectionLinks();
 
         [When(@"the employer navigates to Your finances Page")]
         public void WhenTheEmployerNavigatesToYourFinancesPage() => _financePage = _homePageFinancesSectionPage.NavigateToFinancePage();
 
-        [Then(@"'(View transactions)', '(Download transactions)' and '(Transfers)' links are displayed")]
+        [Then(@"'View transactions', 'Download transactions' and 'Transfers' links are displayed")]
         public void ThenAndLinksAreDisplayed() => _financePage.IsViewTransactionsLinkPresent().IsDownloadTransactionsLinkPresent().IsTransfersLinkPresent();
     }
 }
