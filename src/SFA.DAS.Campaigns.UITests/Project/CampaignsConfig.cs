ac7f95c6f3f7356a645be12b1b5ff78e0c381a4f
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2020-02-27 10:48:48 +0000

    added Campaigns project

---
+++ src/SFA.DAS.Campaigns.UITests/Project/CampaignsConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFA.DAS.Campaigns.UITests.Project
+{
+    public class CampaignsConfig
+    { 
+        public string CA_BaseUrl { get; set; }
+        public string EmployerUserName {get;set;}       
+        public string EmployerPassword { get; set; }
+    }
+}
