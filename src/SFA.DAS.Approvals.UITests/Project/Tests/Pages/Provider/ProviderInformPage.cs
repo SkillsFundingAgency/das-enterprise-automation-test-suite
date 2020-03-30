7c541b834c57c66c0fa254c5f330fe2d4522850b
Akshay Chugh <akshay.chugh30@outlook.com>
2020-03-30 12:12:53 +0100

    Provider Inform page

---
+++ src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Provider/ProviderInformPage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using SFA.DAS.UI.Framework.TestSupport;
+using SFA.DAS.UI.FrameworkHelpers;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Approvals.UITests.Project.Tests.Pages.Provider
+{
+    public class ProviderInformPage : BasePage
+    {
+        #region Helpers and Context
+        private readonly FormCompletionHelper _formCompletionHelper;
+        private readonly ScenarioContext _context;
+        #endregion
+
+        private By ChangeTheEmployerButton => By.Id("change-the-employer-button");
+
+        public ProviderInformPage(ScenarioContext context) : base(context)
+        {
+            _context = context;
+            _formCompletionHelper = context.Get<FormCompletionHelper>();
+            VerifyPage();
+        }
+
+        protected override string PageTitle => "What you'll need";
+
+        public ProviderSelectEmployerPage SelectChangeTheEmployer()
+        {
+            _formCompletionHelper.Click(ChangeTheEmployerButton);
+            return new ProviderSelectEmployerPage(_context);
+        }
+    }
+}
