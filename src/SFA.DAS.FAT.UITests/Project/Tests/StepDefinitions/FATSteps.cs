239d0543532f1ac95c407974275592b60aba3074
SreekanthBadigenchula <sreekanth.badigenchula@digital.education.gov.uk>
2020-04-21 23:40:34 +0100

    base scripts

---
+++ src/SFA.DAS.FAT.UITests/Project/Tests/StepDefinitions/FATSteps.cs
@@ -0,0 +1,28 @@
+﻿using SFA.DAS.FAT.UITests.Project.Tests.Pages;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.FAT.UITests.Project.Tests.StepDefinitions
+{
+    [Binding]
+    public class FATSteps
+    {
+        private readonly ScenarioContext _context;
+        private SearchResultsPage searchResultsPage;
+
+        public FATSteps(ScenarioContext context) => _context = context;
+
+        [Given(@"the User navigates to the Search Results page")]
+        [When(@"the User navigates to the Search Results page")]
+        public void WhenTheUserNavigatesToTheSearchResultsPage() =>
+            searchResultsPage = new FATIndexPage(_context).ClickStartButton().SearchApprenticeshipInFindApprenticeshipTrainingSearchPage();
+
+        [Then(@"the Search Results page features are displayed")]
+        public void ThenTheSearchResultsPageFeaturesAreDisplayed() => searchResultsPage.VerifyFilterAndSortByFields();
+
+        [When(@"the User selects level (2|3|4|5|6|7) to filter results")]
+        public void WhenTheUserSelectsLevelToFilterResults(string level) => searchResultsPage = searchResultsPage.SelectLevelAndFilterResults(level);
+
+        [Then(@"only the level (2|3|4|5|6|7) Search Results are displayed")]
+        public void ThenOnlyTheLevelSearchResultsAreDisplayed(string level) => searchResultsPage = searchResultsPage.VerifyLevelInfoFromSearchResults(level);
+    }
+}
