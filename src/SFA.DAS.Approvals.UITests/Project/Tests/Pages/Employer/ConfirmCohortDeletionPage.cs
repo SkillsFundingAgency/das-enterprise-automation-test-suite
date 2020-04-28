0953f4ef9db056f1ef86fcf2c08c327f345d968b
Najamuddin-Muhammad <najamuddin.muhammad@digital.education.gov.uk>
2020-04-28 09:23:42 +0100

    DC & SA fixes

--- src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Employer/ConfirmCohortDeletionPage.cs
+++ src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Employer/ConfirmCohortDeletionPage.cs
@@ -8,21 +8,24 @@ namespace SFA.DAS.Approvals.UITests.Project.Tests.Pages.Employer
     public class ConfirmCohortDeletionPage : BasePage
     {
         protected override string PageTitle => "Confirm deletion";
+        protected override By ContinueButton => By.Id("continue-button");
 
         #region Helpers and Context
         private readonly ScenarioContext _context;
+        private readonly FormCompletionHelper _formCompletionHelper;
         #endregion
 
         public ConfirmCohortDeletionPage(ScenarioContext context) : base(context)
         {
             _context = context;
+            _formCompletionHelper = context.Get<FormCompletionHelper>();
             VerifyPage();
         }
 
         public YourCohortRequestsPage ConfirmDeleteAndSubmit()
         {
             SelectRadioOptionByForAttribute("confirm-yes");
-            Continue();
+            _formCompletionHelper.ClickElement(ContinueButton);
             return new YourCohortRequestsPage(_context);
         }
     }
--- src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Employer/NewStopDateApprenticeDetailsPage.cs
+++ src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Employer/NewStopDateApprenticeDetailsPage.cs
@@ -6,7 +6,7 @@ namespace SFA.DAS.Approvals.UITests.Project.Tests.Pages.Employer
     public class NewStopDateApprenticeDetailsPage : ApprenticeDetailsPage
     {
         protected override string PageTitle => "New stop date confirmed";
-        protected override By PageHeader => By.CssSelector(".heading-large");
+        protected override By PageHeader => By.CssSelector("h1.heading-large");
 
         public NewStopDateApprenticeDetailsPage(ScenarioContext context) : base(context)
         {
