b7af1e03ccbebb323255f116dc8f510dbbaaa047
Raj <reachash1@hotmail.com>
2020-05-01 10:43:15 +0100

    ElementChangedForFailingTests

--- src/SFA.DAS.EPAO.UITests/Project/Tests/Pages/AssessmentService/AS_WhichLearningOptionPage.cs
+++ src/SFA.DAS.EPAO.UITests/Project/Tests/Pages/AssessmentService/AS_WhichLearningOptionPage.cs
@@ -1,4 +1,5 @@
 ﻿using TechTalk.SpecFlow;
+using SFA.DAS.UI.FrameworkHelpers;
 
 namespace SFA.DAS.EPAO.UITests.Project.Tests.Pages.AssessmentService
 {
@@ -6,16 +7,17 @@ namespace SFA.DAS.EPAO.UITests.Project.Tests.Pages.AssessmentService
     {
         protected override string PageTitle => "Which learning option did the apprentice take?";
         private readonly ScenarioContext _context;
-
+        private readonly FormCompletionHelper _formCompletionHelper;
         public AS_WhichLearningOptionPage(ScenarioContext context) : base(context)
         {
             _context = context;
+            _formCompletionHelper = context.Get<FormCompletionHelper>();
             VerifyPage();
         }
 
         public AS_WhatGradePage SelectLearningOptionAndContinue()
         {
-            SelectRadioOptionByForAttribute("options_Overheadlines");
+            _formCompletionHelper.SelectRadioOptionByText(RadioLabels, "Overhead Lines");
             Continue();
             return new AS_WhatGradePage(_context);
         }
