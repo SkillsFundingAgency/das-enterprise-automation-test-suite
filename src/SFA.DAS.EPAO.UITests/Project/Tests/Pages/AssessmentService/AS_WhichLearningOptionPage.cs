818f7eb6b96da5ae84b09273f85e9f3952ddf939
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2020-05-01 14:29:35 +0100

    minor changes

--- src/SFA.DAS.EPAO.UITests/Project/Tests/Pages/AssessmentService/AS_WhichLearningOptionPage.cs
+++ src/SFA.DAS.EPAO.UITests/Project/Tests/Pages/AssessmentService/AS_WhichLearningOptionPage.cs
@@ -7,17 +7,16 @@ namespace SFA.DAS.EPAO.UITests.Project.Tests.Pages.AssessmentService
     {
         protected override string PageTitle => "Which learning option did the apprentice take?";
         private readonly ScenarioContext _context;
-        private readonly FormCompletionHelper _formCompletionHelper;
+
         public AS_WhichLearningOptionPage(ScenarioContext context) : base(context)
         {
             _context = context;
-            _formCompletionHelper = context.Get<FormCompletionHelper>();
             VerifyPage();
         }
 
         public AS_WhatGradePage SelectLearningOptionAndContinue()
         {
-            _formCompletionHelper.SelectRadioOptionByText(RadioLabels, "Overhead Lines");
+            formCompletionHelper.SelectRadioOptionByText(RadioLabels, "Overhead Lines");
             Continue();
             return new AS_WhatGradePage(_context);
         }
