b7cf817b5a8705d18162c406bcbef13136573af4
44780 <44780@DESKTOP-5CF0C2V>
2020-05-05 14:14:00 +0100

    LegalNamePageAdded

---
+++ src/SFA.DAS.RoatpAdmin.UITests/Project/Tests/Pages/GateWay/OrganisationChecks/LegalNamePage.cs
@@ -0,0 +1,23 @@
+﻿using SFA.DAS.Roatp.UITests.Project.Tests.Pages.RoatpApply;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.RoatpAdmin.UITests.Project.Tests.Pages.GateWay.OrganisationChecks
+{
+    public class LegalNamePage : RoatpGateWayBasePage
+    {
+        protected override string PageTitle => "";
+
+        #region Helpers and Context
+        private readonly ScenarioContext _context;
+        #endregion
+
+        public LegalNamePage(ScenarioContext context) : base(context)
+        {
+            _context = context;
+            VerifyPage();
+        }
+    }
+ }
---
+++ src/SFA.DAS.RoatpAdmin.UITests/Project/Tests/Pages/GateWay/RoatpGateWayBasePage.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using SFA.DAS.Roatp.UITests.Project.Helpers.RoatpApply;
+using SFA.DAS.UI.Framework;
+using System;
+using System.Linq;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Roatp.UITests.Project.Tests.Pages.RoatpApply
+{
+    public abstract class RoatpGateWayBasePage : RoatpBasePage
+    {
+        #region Helpers and Context
+        private readonly ScenarioContext _context;
+        private readonly FrameworkConfig _frameworkConfig;
+        #endregion
+
+        protected By EnterCommentsForFail => By.Id("OptionFailText");
+
+        public RoatpGateWayBasePage(ScenarioContext context) : base(context)
+        {
+            _context = context;
+            _frameworkConfig = context.Get<FrameworkConfig>();
+        }
+
+        protected void SelectPassAndContinue()
+        {
+            SelectRadioOptionByText("Pass");
+            Continue();
+        }
+
+        protected void SelectFailAndContinue(string text)
+        {
+            SelectRadioOptionByText("Fail");
+            formCompletionHelper.EnterText(EnterCommentsForFail, text);
+            Continue();
+        }
+
+        protected void SelectInProgressAndContinue()
+        {
+            SelectRadioOptionByText("InProgress");
+            Continue();
+        }
+
+    }
+}
