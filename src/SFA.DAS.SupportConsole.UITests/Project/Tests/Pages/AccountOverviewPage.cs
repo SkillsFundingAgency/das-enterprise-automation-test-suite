bc84f407cfe7bb7b673fddc279e4cf4388494284
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2020-05-07 09:28:44 +0100

    Challenge feature

--- src/SFA.DAS.SupportConsole.UITests/Project/Tests/Pages/AccountOverviewPage.cs
+++ src/SFA.DAS.SupportConsole.UITests/Project/Tests/Pages/AccountOverviewPage.cs
@@ -5,6 +5,60 @@ using SFA.DAS.UI.Framework.TestSupport;
 
 namespace SFA.DAS.SupportConsole.UITests.Project.Tests.Pages
 {
+    public class ChallengePage : BasePage
+    {
+        protected override string PageTitle => "Enter the following information to verify the caller";
+
+        protected override By PageHeader => By.CssSelector(".lede");
+
+        #region Helpers and Context
+        private readonly ScenarioContext _context;
+        private readonly FormCompletionHelper _formCompletionHelper;
+        private readonly PageInteractionHelper _pageInteractionHelper;
+        #endregion
+
+        private By Challenge1 => By.Id("challenge1");
+        private By Challenge2 => By.Id("challenge2");
+        private By LevyBalance => By.Id("balance");
+        private By Challenge_ErrorMessage => By.CssSelector(".error-summary");
+
+        public ChallengePage(ScenarioContext context) : base(context)
+        {
+            _context = context;
+            _formCompletionHelper = context.Get<FormCompletionHelper>();
+            _pageInteractionHelper = context.Get<PageInteractionHelper>();
+        }
+
+        public void EnterChallenge(string challenge1, string challenge2)
+        {
+            _formCompletionHelper.EnterText(Challenge1, challenge1);
+            _formCompletionHelper.EnterText(Challenge2, challenge2);
+        }
+
+        public void EnterCurrentLevybalance(string balance) => _formCompletionHelper.EnterText(LevyBalance, balance);
+
+        public void VerifyChallengeResponseErrorMessage(string errorMessage) => _pageInteractionHelper.VerifyText(Challenge_ErrorMessage, errorMessage);
+    }
+
+    public class FinancePage : BasePage
+    {
+        protected override string PageTitle => "Finanace";
+
+        protected override By PageHeader => By.CssSelector(".heading-xlarge");
+
+        #region Helpers and Context
+        private readonly ScenarioContext _context;
+        private readonly FormCompletionHelper _formCompletionHelper;
+        private readonly SupportConsoleConfig _config;
+        #endregion
+
+        public FinancePage(ScenarioContext context) : base(context)
+        {
+            _context = context;
+        }
+
+    }
+
     public class AccountOverviewPage : BasePage
     {
         protected override string PageTitle => "Department for Education";
@@ -21,6 +75,7 @@ namespace SFA.DAS.SupportConsole.UITests.Project.Tests.Pages
         private By PageHeaderWithAccountDetails => By.CssSelector(".heading-secondary");
         private By OrganisationsMenuLink => By.LinkText("Organisations");
         private By CommitmentsMenuLink => By.LinkText("Commitments");
+        private By FinanceLink => By.LinkText("Finance");
         #endregion
 
         public AccountOverviewPage(ScenarioContext context) : base(context)
@@ -38,6 +93,8 @@ namespace SFA.DAS.SupportConsole.UITests.Project.Tests.Pages
             _formCompletionHelper.Click(CommitmentsMenuLink);
             return new CommitmentsSearchPage(_context);
         }
+
+        public void ClickFinanceMenuLink() => _formCompletionHelper.Click(FinanceLink);
 
         private AccountOverviewPage ClickOrganisationsLink()
         {
---
+++ src/SFA.DAS.SupportConsole.UITests/Project/Tests/StepDefinitions/MaSteps.cs
@@ -0,0 +1,42 @@
+﻿using SFA.DAS.SupportConsole.UITests.Project.Helpers;
+using SFA.DAS.SupportConsole.UITests.Project.Tests.Pages;
+using SFA.DAS.UI.Framework.TestSupport;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.SupportConsole.UITests.Project.Tests.StepDefinitions
+{
+    [Binding]
+    public class MaSteps
+    {
+        private readonly ScenarioContext _context;
+        private readonly StepsHelper _stepsHelper;
+        private readonly SupportConsoleConfig _config;
+        private ChallengePage _challengePage;
+        
+        public MaSteps(ScenarioContext context)
+        {
+            _context = context;
+            _stepsHelper = new StepsHelper(_context);
+            _config = context.GetSupportConsoleConfig<SupportConsoleConfig>();
+        }
+        
+        [When(@"the user navigates to finance page")]
+        public void WhenTheUserNavigatesToFinancePage() => new AccountOverviewPage(_context).ClickFinanceMenuLink();
+
+        [Then(@"the user is redirected to a challenge page")]
+        public void ThenTheUserIsRedirectedToAChallengePage() => _challengePage = new ChallengePage(_context);
+
+        [When(@"the user enters invalid payscheme")]
+        public void WhenTheUserEntersInvalidPayscheme() => _challengePage.EnterChallenge("1", "1");
+
+        [When(@"enters correct levybalance")]
+        public void WhenEntersCorrectLevybalance() => _challengePage.EnterCurrentLevybalance(_config.CurrentLevyBalance);
+
+        [Then(@"the user should see the error message (.*)")]
+        public void ThenTheUserShouldSeeTheErrorMessage(string message)
+        {
+            throw new PendingStepException();
+        }
+
+    }
+}
