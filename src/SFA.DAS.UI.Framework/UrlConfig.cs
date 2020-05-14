e03acd0a38c656701fcd55c6be01e489305ca776
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2020-05-14 11:02:30 +0100

    fixed campaingns

--- src/SFA.DAS.UI.Framework/UrlConfig.cs
+++ src/SFA.DAS.UI.Framework/UrlConfig.cs
@@ -4,7 +4,7 @@
     {
         public static string AdminBaseUrl => $"https://{EnvironmentConfig.EnvironmentName}-admin.apprenticeships.education.gov.uk/";
         public static string ApplyBaseUrl => $"https://{EnvironmentConfig.EnvironmentName}-apply.apprenticeships.education.gov.uk/";
-        public static string CABaseURL => $"https://{EnvironmentConfig.EnvironmentName}.apprenticeships.gov.uk/";
+        public static string CABaseURL => EnvironmentConfig.IsPPEnvironment ? "https://preprod.apprenticeships.gov.uk/" : $"https://{EnvironmentConfig.EnvironmentName}.apprenticeships.gov.uk/";
         public static string EPAOAssessmentServiceUrl => $"https://{EnvironmentConfig.EnvironmentName}-assessors.apprenticeships.education.gov.uk";
         public static string EmployerApprenticeshipServiceBaseURL => $"https://{EnvironmentConfig.EnvironmentName}-eas.apprenticeships.education.gov.uk/";
         public static string FAABaseUrl => $"https://{EnvironmentConfig.EnvironmentName}.findapprenticeship.service.gov.uk/";
