7c515a982972a62fa30b2dc7ebb6201e531898c3
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2020-05-19 15:28:09 +0100

    updated interim pages

---
+++ src/SFA.DAS.Registration.UITests/Project/Tests/Pages/InterimPages/InterimManageFundingHomePage.cs
@@ -0,0 +1,35 @@
+﻿using SFA.DAS.UI.FrameworkHelpers;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Registration.UITests.Project.Tests.Pages.InterimPages
+{
+    public class InterimManageFundingHomePage : InterimEmployerBasePage
+    {
+        protected override string PageTitle => "Your funding reservations";
+
+        protected override string Linktext => "Your funding reservations";
+
+        public InterimManageFundingHomePage(ScenarioContext context, bool navigate) : this(context, navigate, false) { }
+
+        public InterimManageFundingHomePage(ScenarioContext context, bool navigate, bool gotourl) : base(context, OpenYourFundingReservations(context, navigate), gotourl) { }
+
+        private static Action OpenYourFundingReservations(ScenarioContext context, bool navigate)
+        {
+            if (navigate)
+            {
+                return () =>
+                {
+                    new HomePage(context, true);
+
+                    var helper = context.Get<FormCompletionHelper>();
+
+                    helper.ClickLinkByText("Your funding reservations");
+                };
+            }
+            return null;
+            
+        }
+    }
+}
+
