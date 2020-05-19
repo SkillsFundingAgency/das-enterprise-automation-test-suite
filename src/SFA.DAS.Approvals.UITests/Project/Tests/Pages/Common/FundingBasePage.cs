f9af0d58632344994f3bbd691fd4b9c754c914c0
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2020-05-19 15:28:27 +0100

    updated manage fundign pages

--- src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Common/FundingBasePage.cs
+++ src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Common/FundingBasePage.cs
@@ -1,6 +1,4 @@
-﻿using OpenQA.Selenium;
-using SFA.DAS.UI.Framework.TestSupport;
-using SFA.DAS.ConfigurationBuilder;
+﻿using SFA.DAS.UI.Framework.TestSupport;
 using SFA.DAS.UI.FrameworkHelpers;
 using TechTalk.SpecFlow;
 
@@ -9,30 +7,17 @@ namespace SFA.DAS.Approvals.UITests.Project.Tests.Pages.Common
     public abstract class FundingBasePage : BasePage
     {
         #region Helpers and Context
-        private readonly string _reservationId;
+
         #endregion
  
         #region Helpers and Context
         protected readonly FormCompletionHelper formCompletionHelper;
-        private readonly ScenarioContext _context;
         #endregion
- 
-        protected By AddApprenticeLink => By.CssSelector($"table a[href*='?reservationId={_reservationId}']");
-
-        protected By DeleteFundingLink => By.CssSelector($"table a[href*='{_reservationId}/delete']");
 
         public FundingBasePage(ScenarioContext context) : base(context)
         {
-            _context = context;
             formCompletionHelper = context.Get<FormCompletionHelper>();
-            _reservationId = context.Get<ObjectContext>().GetReservationId();
-            VerifyPage();
-        }
-
-        internal DeleteReservationPage DeleteTheReservedFunding()
-        {
-            formCompletionHelper.ClickElement(DeleteFundingLink);
-            return new DeleteReservationPage(_context);
+            
         }
     }
 }
--- src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/ManageFunding/Provider/ProviderFundingForNonLevyEmployersPage.cs
+++ src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/ManageFunding/Provider/ProviderFundingForNonLevyEmployersPage.cs
@@ -1,27 +1,46 @@
-﻿using SFA.DAS.Approvals.UITests.Project.Tests.Pages.Common;
+﻿using OpenQA.Selenium;
+using SFA.DAS.Approvals.UITests.Project.Tests.Pages.Common;
 using SFA.DAS.Approvals.UITests.Project.Tests.Pages.Provider;
+using SFA.DAS.ConfigurationBuilder;
 using SFA.DAS.UI.Framework.TestSupport;
+using SFA.DAS.UI.FrameworkHelpers;
 using TechTalk.SpecFlow;
 
 namespace SFA.DAS.Approvals.UITests.Project.Tests.Pages.ManageFunding.Provider
 {
-    public class ProviderFundingForNonLevyEmployersPage : FundingBasePage
+    public class ProviderFundingForNonLevyEmployersPage : BasePage
     {
         protected override string PageTitle => "Funding for non-levy employers";
 
         #region Helpers and Context
         private readonly ScenarioContext _context;
+        protected readonly FormCompletionHelper _formCompletionHelper;
+        private readonly string _reservationId;
         #endregion
+
+        protected By AddApprenticeLink => By.CssSelector($"table a[href*='?reservationId={_reservationId}']");
+
+        protected By DeleteFundingLink => By.CssSelector($"table a[href*='{_reservationId}/delete']");
 
         public ProviderFundingForNonLevyEmployersPage(ScenarioContext context) : base(context)
         {
             _context = context;
+            _reservationId = context.Get<ObjectContext>().GetReservationId();
+            _formCompletionHelper = context.Get<FormCompletionHelper>();
+            VerifyPage();
         }
 
         internal ProviderAddApprenticeDetailsPage AddApprenticeWithReservedFunding()
         {
-            formCompletionHelper.ClickElement(AddApprenticeLink);
+            _formCompletionHelper.ClickElement(AddApprenticeLink);
             return new ProviderAddApprenticeDetailsPage(_context);
         }
+
+        internal DeleteReservationPage DeleteTheReservedFunding()
+        {
+            _formCompletionHelper.ClickElement(DeleteFundingLink);
+            return new DeleteReservationPage(_context);
+        }
+
     }
 }
