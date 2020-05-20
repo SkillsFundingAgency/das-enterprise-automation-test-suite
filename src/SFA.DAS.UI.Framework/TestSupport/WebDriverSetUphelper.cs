e2ab3d558dcf9aed78d538d54bb2da4f74c8591d
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2020-05-20 14:57:04 +0100

    added LocalFileDetector

--- src/SFA.DAS.UI.Framework/TestSupport/WebDriverSetUphelper.cs
+++ src/SFA.DAS.UI.Framework/TestSupport/WebDriverSetUphelper.cs
@@ -6,6 +6,7 @@ using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
 using TechTalk.SpecFlow;
 using SFA.DAS.ConfigurationBuilder;
+using OpenQA.Selenium.Remote;
 
 namespace SFA.DAS.UI.Framework.TestSupport
 {
