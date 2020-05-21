8dc5660cf2e66a8cece07e5729909610a411bd67
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2020-05-21 16:44:58 +0100

    review comments

--- src/SFA.DAS.Registration.UITests/Project/Tests/Pages/EmployerHelpPage.cs
+++ src/SFA.DAS.Registration.UITests/Project/Tests/Pages/EmployerHelpPage.cs
@@ -3,13 +3,13 @@ using TechTalk.SpecFlow;
 
 namespace SFA.DAS.Registration.UITests.Project.Tests.Pages
 {
-    public class HelpArticlesPage : RegistrationBasePage
+    public class EmployerHelpPage : RegistrationBasePage
     {
         protected override string PageTitle => "Sign in to Apprenticeship Service Support SANDBOX";
 
         protected override By PageHeader => By.CssSelector(".title");
 
-        public HelpArticlesPage(ScenarioContext context) : base(context)
+        public EmployerHelpPage(ScenarioContext context) : base(context)
         {
             tabHelper.SwitchToFrame();
             VerifyPage();
