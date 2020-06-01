8284b814dcdadffcd2f2737e6f2fbc3e4a914b3d
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2020-06-01 11:12:18 +0100

    resolved warning messages

--- src/SFA.DAS.RAA-V2.Service/Project/Tests/Pages/EditVacancyConfirmationPage.cs
+++ src/SFA.DAS.RAA-V2.Service/Project/Tests/Pages/EditVacancyConfirmationPage.cs
@@ -7,10 +7,6 @@ namespace SFA.DAS.RAA_V2.Service.Project.Tests.Pages
     {
         protected override string PageTitle => $"The vacancy '{dataHelper.VacancyTitle}' has been updated.";
 
-        #region Helpers and Context
-        private readonly string _vacancyTitle;
-        #endregion
-
         private By Info => By.CssSelector(".info-summary");
 
         public EditVacancyConfirmationPage(ScenarioContext context) : base(context, false) => VerifyPage(() => pageInteractionHelper.FindElements(Info));
--- src/SFA.DAS.RAA-V2.Service/Project/Tests/Pages/ManageCloseVacancyPage.cs
+++ src/SFA.DAS.RAA-V2.Service/Project/Tests/Pages/ManageCloseVacancyPage.cs
@@ -10,11 +10,6 @@ namespace SFA.DAS.RAA_V2.Service.Project.Tests.Pages
 
         protected override By PageHeader => By.CssSelector(".info-summary");
 
-        #region Helpers and Context
-        private readonly string _vacRef;
-        private readonly string _vacancyTitle;
-        #endregion
-
         public ManageCloseVacancyPage(ScenarioContext context) : base(context) { }
     }
 }
