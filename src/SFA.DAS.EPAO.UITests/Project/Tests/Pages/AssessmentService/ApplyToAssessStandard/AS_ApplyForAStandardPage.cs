f13fd8aa9209d65168ef76c1ee709bb8ab312add
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2020-06-12 15:51:20 +0100

    updated css selector

--- src/SFA.DAS.EPAO.UITests/Project/Tests/Pages/AssessmentService/ApplyToAssessStandard/AS_ApplyForAStandardPage.cs
+++ src/SFA.DAS.EPAO.UITests/Project/Tests/Pages/AssessmentService/ApplyToAssessStandard/AS_ApplyForAStandardPage.cs
@@ -1,5 +1,4 @@
-﻿using OpenQA.Selenium;
-using TechTalk.SpecFlow;
+﻿using TechTalk.SpecFlow;
 
 namespace SFA.DAS.EPAO.UITests.Project.Tests.Pages.AssessmentService.ApplyToAssessStandard
 {
@@ -8,13 +7,11 @@ namespace SFA.DAS.EPAO.UITests.Project.Tests.Pages.AssessmentService.ApplyToAsse
         protected override string PageTitle => "Apply for a standard";
         private readonly ScenarioContext _context;
 
-        private By StartButton => By.CssSelector("#main-content .govuk-button");
-
         public AS_ApplyForAStandardPage(ScenarioContext context) : base(context) => _context = context;
 
         public AS_WhatStandardPage Start()
         {
-            formCompletionHelper.ClickElement(StartButton);
+            Continue();
             return new AS_WhatStandardPage(_context);
         }
     }
