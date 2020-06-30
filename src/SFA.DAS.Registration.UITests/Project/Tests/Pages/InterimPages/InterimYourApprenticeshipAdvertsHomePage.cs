b9945eb9410b97221b55887660e828bfde7f19c1
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2020-06-30 07:16:31 +0100

    updated page title

--- src/SFA.DAS.Registration.UITests/Project/Tests/Pages/InterimPages/InterimYourApprenticeshipAdvertsHomePage.cs
+++ src/SFA.DAS.Registration.UITests/Project/Tests/Pages/InterimPages/InterimYourApprenticeshipAdvertsHomePage.cs
@@ -4,7 +4,7 @@ namespace SFA.DAS.Registration.UITests.Project.Tests.Pages.InterimPages
 {
     public class InterimYourApprenticeshipAdvertsHomePage : InterimEmployerBasePage
     {
-        protected override string PageTitle => "Your apprenticeship adverts";
+        protected override string PageTitle => "Recruitment";
 
         protected override string Linktext => "Adverts";
 
