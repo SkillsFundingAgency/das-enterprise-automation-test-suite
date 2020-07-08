abaf1c38d6725cc2a09521dfefed7a8ba42eb8c0
Najamuddin-Muhammad <najamuddin.muhammad@digital.education.gov.uk>
2020-07-08 09:55:50 +0100

    fixed drafts tab failure

--- src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Employer/ApprenticeRequestsPage.cs
+++ src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Employer/ApprenticeRequestsPage.cs
@@ -14,7 +14,7 @@ namespace SFA.DAS.Approvals.UITests.Project.Tests.Pages.Employer
 
         private By NumberofApprenticeRequestsReadyForReview => By.CssSelector("span[id='Review'] span[class*='das-tabs-boxes__figure']");
         private By NumberOfTransferRejectedCohorts => By.CssSelector(".block-one .bold-xxlarge");
-        private By NumberOfDrafts => By.CssSelector("span[id='Draft'] span[class*='das-tabs-boxes__figure']");
+        private By NumberOfDrafts => By.CssSelector("a[href*='draft'] span[class*='das-tabs-boxes__figure']");
 
         public ApprenticeRequestsPage(ScenarioContext context) : base(context) => _context = context;
 
@@ -29,13 +29,13 @@ namespace SFA.DAS.Approvals.UITests.Project.Tests.Pages.Employer
 
             throw new Exception("No cohorts available for review");
         }
-        public DraftCohortsPage GoToDrafts()
+        public ApprenticeRequestDraftsPage GoToDrafts()
         {
             var employerDraftCohorts = Convert.ToInt32(pageInteractionHelper.GetText(NumberOfDrafts));
             if (employerDraftCohorts > 0)
             {
                 formCompletionHelper.ClickElement(NumberOfDrafts);
-                return new DraftCohortsPage(_context);
+                return new ApprenticeRequestDraftsPage(_context);
             }
 
             throw new Exception("No cohorts available in Draft");
