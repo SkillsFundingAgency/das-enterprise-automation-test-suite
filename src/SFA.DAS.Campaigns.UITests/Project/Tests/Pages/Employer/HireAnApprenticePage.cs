458f1d2dcbbc51a6b66f1ea49d67994a64ba945b
Sultan <sultan.mahmood@education.gov.uk>
2020-08-11 09:49:55 +0000

    Change of header and css

--- src/SFA.DAS.Campaigns.UITests/Project/Tests/Pages/Employer/HireAnApprenticePage.cs
+++ src/SFA.DAS.Campaigns.UITests/Project/Tests/Pages/Employer/HireAnApprenticePage.cs
@@ -9,7 +9,7 @@ namespace SFA.DAS.Campaigns.UITests.Project.Tests.Pages.Employer
 
         #region Page Object Elements
         private readonly By _subHeading1 = By.XPath("//h2[contains (@class, 'heading-m' ) and contains(text(), 'Hiring outside your organisation')]");
-        private readonly By _subHeading3 = By.XPath("//h2[contains (@class, 'heading-m' ) and contains(text(), 'Advertising apprenticeships')]");
+        private readonly By _subHeading3 = By.XPath("//h2[contains (@class, 'heading-m' ) and contains(text(), 'Advertising your apprenticeship')]");
         private readonly By _subHeading4 = By.XPath("//h2[contains (@class, 'heading-m' ) and contains(text(), 'Choosing the right apprentice')]");
         private readonly By _subHeading5 = By.XPath("//h2[contains (@class, 'heading-m' ) and contains(text(), 'Interview questions')]");
         #endregion
@@ -18,7 +18,7 @@ namespace SFA.DAS.Campaigns.UITests.Project.Tests.Pages.Employer
         private void VerifyHeadings()
         {
             pageInteractionHelper.VerifyText(_subHeading1, "HIRING OUTSIDE YOUR ORGANISATION");
-            pageInteractionHelper.VerifyText(_subHeading3, "ADVERTISING APPRENTICESHIPS");
+            pageInteractionHelper.VerifyText(_subHeading3, "ADVERTISING YOUR APPRENTICESHIP");
             pageInteractionHelper.VerifyText(_subHeading4, "CHOOSING THE RIGHT APPRENTICE");
             pageInteractionHelper.VerifyText(_subHeading5, "INTERVIEW QUESTIONS");
         }
