2fd759dec2cfea438b1cc28e1407076d87820edb
44780 <44780@DESKTOP-5CF0C2V>
2020-08-13 13:13:52 +0100

    Task Status Selector Change

--- src/SFA.DAS.Roatp.UITests/Project/Tests/Pages/RoatpApply/ApplicationOverviewPage.cs
+++ src/SFA.DAS.Roatp.UITests/Project/Tests/Pages/RoatpApply/ApplicationOverviewPage.cs
@@ -23,7 +23,7 @@ namespace SFA.DAS.Roatp.UITests.Project.Tests.Pages.RoatpApply
 
         private By TaskName => By.CssSelector(".app-task-list__task-name > .govuk-link");
 
-        private By TaskStatus => By.CssSelector(".govuk-tag");
+        private By TaskStatus => By.CssSelector(".app-task-list__task-completed, .app-task-list__task-notrequired, .app-task-list__task-next");
 
         public ApplicationOverviewPage(ScenarioContext context) : base(context)
         {
