76bddf5ea32e05a361cf861618e8bb6c7aaae698
Narendra Nogothu <nagothumm@gmail.com>
2020-08-17 12:43:45 +0100

    content changes

--- src/SFA.DAS.RAA-V2.Service/Project/Tests/Pages/DeleteVacancyQuestionPage.cs
+++ src/SFA.DAS.RAA-V2.Service/Project/Tests/Pages/DeleteVacancyQuestionPage.cs
@@ -8,7 +8,7 @@ namespace SFA.DAS.RAA_V2.Service.Project.Tests.Pages
     {
         protected override string PageTitle => rAAV2DataHelper.VacancyTitle;
 
-        private string SubTitle => isRaaV2Employer ? "Are you sure you want to delete the advert?" : "Are you sure you want to delete the vacancy?";
+        private string SubTitle => isRaaV2Employer ? "Are you sure you want to delete this advert?" : "Are you sure you want to delete the vacancy?";
 
         private By SubHeader => By.CssSelector(".govuk-heading-l");
 
