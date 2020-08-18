e9ef7ad967a6b9f20792ff6bd3656d83629cbd56
Siji Odunlami <siji.odunlami@gmail.com>
2020-08-18 16:12:40 +0100

    Added new project and dependancies

---
+++ src/SFA.DAS.FAT-V2.UITests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace SFA.DAS.FAT_V2.UITests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
