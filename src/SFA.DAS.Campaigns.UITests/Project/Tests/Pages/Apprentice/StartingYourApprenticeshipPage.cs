56f581c5e75cbf8e73bf78cbe31a4a0a34aa96ef
Sultan <sultan.mahmood@education.gov.uk>
2020-09-08 14:46:43 +0000

    Employer tests updated

---
+++ src/SFA.DAS.Campaigns.UITests/Project/Tests/Pages/Apprentice/StartingYourApprenticeshipPage.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Campaigns.UITests.Project.Tests.Pages.Apprentice
+{
+    public class StartingYourApprenticeshipPage : ApprenticeBasePage
+    {
+        protected override string PageTitle => "Page not found";
+
+        public StartingYourApprenticeshipPage(ScenarioContext context) : base(context) => VerifyHeadings();
+
+        private void VerifyHeadings()
+        {
+            pageInteractionHelper.VerifyPageLoad(PageHeader, PageTitle);
+        }
+    }
+}
---
+++ src/SFA.DAS.Campaigns.UITests/Project/Tests/StepDefinitions/CA_AP_CA_AP_AreTheyRightForYouSteps.cs
@@ -0,0 +1,36 @@
+﻿using SFA.DAS.Campaigns.UITests.Project.Helpers;
+using SFA.DAS.Campaigns.UITests.Project.Tests.Pages.Apprentice;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Campaigns.UITests.Project.Tests.Features.Apprentice
+{
+    [Binding]
+    public class CA_AP_AreTheyRightForYouSteps
+    {
+        private readonly CampaignsStepsHelper _stepsHelper;
+        private AreApprenticeShipRightForMePage areApprenticeShipRightForMePage;
+        private readonly ScenarioContext _context;
+        
+        public CA_AP_AreTheyRightForYouSteps(ScenarioContext context)
+        {
+            _context = context;
+            _stepsHelper = new CampaignsStepsHelper(context);
+        }
+
+        [Given(@"the user navigates to Are ApprenticeShip Right For You Page")]
+        public void GivenTheUserNavigatesToAreApprenticeShipRightForYouPage()
+        {
+            _stepsHelper.GoToFireItUpHomePage()
+                .NavigateToApprenticeshipHubPage()
+                .NavigateToAreApprenticeShipRightForMe();
+        }
+        [Then(@"check that RealStories Page loads")]
+        public void ThenCheckThatRealStoriesPageLoads()
+        {
+            areApprenticeShipRightForMePage = new AreApprenticeShipRightForMePage(_context);
+            areApprenticeShipRightForMePage.NavigateToRealStoriesPage();
+
+        }
+    }
+}
