10258e868ec69d8301b7143082c90f907311b639
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2020-10-15 16:24:09 +0100

    updated data helper

--- src/SFA.DAS.ConsolidatedSupport.UITests/Project/Helpers/ConsolidateSupportDataHelper.cs
+++ src/SFA.DAS.ConsolidatedSupport.UITests/Project/Helpers/ConsolidateSupportDataHelper.cs
@@ -4,6 +4,11 @@ namespace SFA.DAS.ConsolidatedSupport.UITests.Project.Helpers
 {
     public class ConsolidateSupportDataHelper
     {
+        public ConsolidateSupportDataHelper()
+        {
+            CommentBody = $"Created on {DateTime.Now}";
+        }
+
         public string SubmitAsNewComments => "Comment - Submit as New";
 
         public string SubmitAsOpenComments => "Comment - Submit as Open";
@@ -16,7 +21,7 @@ namespace SFA.DAS.ConsolidatedSupport.UITests.Project.Helpers
 
         public string Subject => $"Zendesk UI Testing";
 
-        public string CommentBody => $"Created on";
+        public string CommentBody { get; }
 
         public string OrganisationName { get; set; }
 
