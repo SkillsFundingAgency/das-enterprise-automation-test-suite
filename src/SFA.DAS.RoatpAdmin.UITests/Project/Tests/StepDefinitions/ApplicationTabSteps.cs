f3aca45b92166b9fa563261e6540d23b7a418b25
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2020-11-06 14:05:41 +0000

    updated roatp binding

--- src/SFA.DAS.RoatpAdmin.UITests/Project/Tests/StepDefinitions/ApplicationTabSteps.cs
+++ src/SFA.DAS.RoatpAdmin.UITests/Project/Tests/StepDefinitions/ApplicationTabSteps.cs
@@ -3,13 +3,12 @@ using TechTalk.SpecFlow;
 
 namespace SFA.DAS.RoatpAdmin.UITests.Project.Tests.StepDefinitions
 {
+    [Binding]
     public class ApplicationTabSteps
     {
         private readonly ScenarioContext _context;
-        public ApplicationTabSteps(ScenarioContext context)
-        {
-            _context = context;
-        }
+
+        public ApplicationTabSteps(ScenarioContext context) => _context = context;
 
         [Then(@"the Outcome tab is updated as (PASS|FAIL)")]
         public void ThenTheOutcomeTabIsUpdated(string expectedStatus) => new RoatpApplicationsHomePage(_context).VerifyOutcomeStatus(expectedStatus);
