c3c08cd8efb48d3044eff2892bad877a8f1521ab
SreekanthBadigenchula <sreekanth.badigenchula@digital.education.gov.uk>
2020-11-09 12:51:28 +0000

    testdata feature refactor

--- src/SFA.DAS.EmployerIncentives.UITests/Project/Tests/Features/TestData/EI_TD3_AddCommitmentsToAnExistingAccount.feature
+++ src/SFA.DAS.EmployerIncentives.UITests/Project/Tests/Features/TestData/EI_TD3_AddCommitmentsToAnExistingAccount.feature
@@ -1,8 +1,9 @@
 ﻿Feature: EI_TD3_AddCommitmentsToAnExistingAccount
 
+@dfeuatachieveservice
 Scenario: EI_TD3_AddCommitmentsToAnExistingAccount
 	Given the Employer logins using existing EI Levy Account
-	And the Employer adds 2 apprentices AgedAbove25 as of 01AUG2020 with start date as Month 8 and Year 2020
+	And the Employer adds 2 apprentices AgedAbove25 as of 01AUG2020 with start date as Month 9 and Year 2020
 	And the Provider approves the apprenticeship request
 	Then the Employer is able to navigate to EI start page for Single entity account
 	And the Employer is able to submit the EI Application
--- src/SFA.DAS.EmployerIncentives.UITests/Project/Tests/Features/TestData/EI_TD3_AddCommitmentsToAnExistingAccount.feature.cs
+++ src/SFA.DAS.EmployerIncentives.UITests/Project/Tests/Features/TestData/EI_TD3_AddCommitmentsToAnExistingAccount.feature.cs
@@ -75,12 +75,14 @@ namespace SFA.DAS.EmployerIncentives.UITests.Project.Tests.Features.TestData
         
         [NUnit.Framework.TestAttribute()]
         [NUnit.Framework.DescriptionAttribute("EI_TD3_AddCommitmentsToAnExistingAccount")]
+        [NUnit.Framework.CategoryAttribute("dfeuatachieveservice")]
         public virtual void EI_TD3_AddCommitmentsToAnExistingAccount()
         {
-            string[] tagsOfScenario = ((string[])(null));
+            string[] tagsOfScenario = new string[] {
+                    "dfeuatachieveservice"};
             System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
             TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("EI_TD3_AddCommitmentsToAnExistingAccount", null, tagsOfScenario, argumentsOfScenario);
-#line 3
+#line 4
 this.ScenarioInitialize(scenarioInfo);
 #line hidden
             bool isScenarioIgnored = default(bool);
@@ -100,26 +102,26 @@ this.ScenarioInitialize(scenarioInfo);
             else
             {
                 this.ScenarioStart();
-#line 4
+#line 5
  testRunner.Given("the Employer logins using existing EI Levy Account", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
 #line hidden
-#line 5
- testRunner.And("the Employer adds 2 apprentices AgedAbove25 as of 01AUG2020 with start date as Mo" +
-                        "nth 8 and Year 2020", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
-#line hidden
 #line 6
- testRunner.And("the Provider approves the apprenticeship request", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+ testRunner.And("the Employer adds 2 apprentices AgedAbove25 as of 01AUG2020 with start date as Mo" +
+                        "nth 9 and Year 2020", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
 #line 7
- testRunner.Then("the Employer is able to navigate to EI start page for Single entity account", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+ testRunner.And("the Provider approves the apprenticeship request", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
 #line 8
- testRunner.And("the Employer is able to submit the EI Application", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+ testRunner.Then("the Employer is able to navigate to EI start page for Single entity account", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
 #line 9
- testRunner.When("the Employer Initiates EI Application journey for Single entity account again", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+ testRunner.And("the Employer is able to submit the EI Application", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
 #line 10
+ testRunner.When("the Employer Initiates EI Application journey for Single entity account again", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line hidden
+#line 11
  testRunner.Then("Select apprentices shutter page is displayed for selecting Yes option in Qualific" +
                         "ation page", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
