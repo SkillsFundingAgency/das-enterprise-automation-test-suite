ca5c8abf402e959ea7226fd7f37c67097ce99c38
Narendra Nogothu <nagothumm@gmail.com>
2020-11-10 16:23:11 +0000

    update elements

--- src/SFA.DAS.EPAO.UITests/Project/Tests/Pages/AssessmentService/ManageUsers/AS_EditUserPermissionsPage.cs
+++ src/SFA.DAS.EPAO.UITests/Project/Tests/Pages/AssessmentService/ManageUsers/AS_EditUserPermissionsPage.cs
@@ -11,12 +11,12 @@ namespace SFA.DAS.EPAO.UITests.Project.Tests.Pages.AssessmentService.ManageUsers
 
         #region Locators
         private By SaveButton => By.LinkText("Edit user permissions");
-        private By ChangeOrganisationDetailsCheckBox => By.Id("PrivilegeViewModels[0].Selected");
-        private By ChangPipelineCheckBox => By.Id("PrivilegeViewModels[1].Selected");
-        private By ChangeCompletedAssessmentsCheckBox => By.Id("PrivilegeViewModels[2].Selected");
-        private By ChangeApplyForAStandardCheckBox => By.Id("PrivilegeViewModels[3].Selected");
+        private By ChangeOrganisationDetailsCheckBox => By.Id("PrivilegeViewModels[1].Selected");
+        private By ChangPipelineCheckBox => By.Id("PrivilegeViewModels[5].Selected");
+        private By ChangeCompletedAssessmentsCheckBox => By.Id("PrivilegeViewModels[3].Selected");
+        private By ChangeApplyForAStandardCheckBox => By.Id("PrivilegeViewModels[0].Selected");
         private By ChangeManageUsersCheckBox => By.Id("PrivilegeViewModels[4].Selected");
-        private By ChangeRecordGradesCheckBox => By.Id("PrivilegeViewModels[5].Selected");
+        private By ChangeRecordGradesCheckBox => By.Id("PrivilegeViewModels[2].Selected");
         #endregion
 
         public AS_EditUserPermissionsPage(ScenarioContext context) : base(context)
