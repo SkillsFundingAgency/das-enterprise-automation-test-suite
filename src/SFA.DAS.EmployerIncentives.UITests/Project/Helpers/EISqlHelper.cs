6c3dcf2477ff7efc2ed29f513a7d77a4d70899bd
SreekanthBadigenchula <sreekanth.badigenchula@digital.education.gov.uk>
2020-11-11 11:20:37 +0000

    queries update

--- src/SFA.DAS.EmployerIncentives.UITests/Project/Helpers/EISqlHelper.cs
+++ src/SFA.DAS.EmployerIncentives.UITests/Project/Helpers/EISqlHelper.cs
@@ -9,11 +9,11 @@ namespace SFA.DAS.EmployerIncentives.UITests.Project.Helpers
         public void DeleteIncentiveApplication(string accountid)
         {
             var nullValue = "NULL";
-            var query = $"DELETE FROM [PendingPayment] WHERE AccountId = {accountid};" +
-                $"DELETE FROM [ApprenticeshipIncentive] WHERE AccountId = {accountid};" +
-                $"DELETE FROM [IncentiveApplicationApprenticeship] WHERE IncentiveApplicationId IN (SELECT Id FROM IncentiveApplication WHERE AccountId = {accountid});" +
-                $"DELETE FROM [IncentiveApplication] WHERE AccountId = {accountid};" +
-                $"UPDATE [Accounts] SET VrfCaseId = {nullValue}, VrfCaseStatus = {nullValue}, VrfCaseStatusLastUpdatedDateTime = {nullValue} WHERE Id = {accountid}";
+            var query = $"DELETE FROM [incentives].[PendingPayment] WHERE AccountId = {accountid};" +
+                $"DELETE FROM [incentives].[ApprenticeshipIncentive] WHERE AccountId = {accountid};" +
+                $"DELETE FROM [dbo].[IncentiveApplicationApprenticeship] WHERE IncentiveApplicationId IN (SELECT Id FROM IncentiveApplication WHERE AccountId = {accountid});" +
+                $"DELETE FROM [dbo].[IncentiveApplication] WHERE AccountId = {accountid};" +
+                $"UPDATE [dbo].[Accounts] SET VrfVendorId = {nullValue}, VrfCaseId = {nullValue}, VrfCaseStatus = {nullValue}, VrfCaseStatusLastUpdatedDateTime = {nullValue} WHERE Id = {accountid}";
             ExecuteSqlCommand(query);
         }
     }
