1942b4f61b7ba04e813c8cfdbee50f6c56f76dbf
44780 <44780@DESKTOP-5CF0C2V>
2020-11-30 14:07:07 +0000

    NewPageAddedinApply

---
+++ src/SFA.DAS.Roatp.UITests/Project/Tests/Pages/RoatpApply/TwoUnsuccessfulApplicationsPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Roatp.UITests.Project.Tests.Pages.RoatpApply
+{
+    public class TwoUnsuccessfulApplicationsPage : RoatpApplyBasePage
+    {
+        protected override string PageTitle => "Have you had 2 unsuccessful applications in the last 12 months?";
+
+        private readonly ScenarioContext _context;
+
+        public TwoUnsuccessfulApplicationsPage (ScenarioContext context) : base(context)
+        {
+            _context = context;
+            VerifyPage();   
+        }
+        public EnterUkprnPage SelectNoFor2ApplicaitonsAndContinue()
+        {
+            SelectNoAndContinue();
+            return new EnterUkprnPage(_context);
+        }
+    }
+}
