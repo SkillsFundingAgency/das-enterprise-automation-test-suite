959a1bb4b74597dd3ee3b7ec54e4562b1e7be328
Najam Muhammad <najamuddin.muhammad@digital.education.gov.uk>
2020-12-10 09:59:47 +0000

    Wip

---
+++ src/SFA.DAS.EPAO.UITests/Project/Tests/Pages/EPAOWithdrawalPages/AS_YourWithdrawalNotificationsPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.EPAO.UITests.Project.Tests.Pages.EPAOWithdrawalPages
+{
+    public class AS_YourWithdrawalNotificationsPage : EPAO_BasePage
+    {
+        protected override string PageTitle => "Your withdrawal notifications";
+        private readonly ScenarioContext _context;
+        public AS_YourWithdrawalNotificationsPage(ScenarioContext context) : base(context)
+        {
+            _context = context;
+            VerifyPage();
+        }
+
+    }
+}
