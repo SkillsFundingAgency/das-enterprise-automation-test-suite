428282b720f4bb0ce8a395760e997b7e2fa0b48f
Kola samuel <samuel.dev.dba@gmail.com>
2020-12-23 16:57:31 +0000

    Verify Page

--- src/SFA.DAS.EPAO.UITests/Project/Tests/Pages/EPAOWithdrawalPages/AS_HowWillYouSupportTheLearnersYouAreNotGoingToAssessPage.cs
+++ src/SFA.DAS.EPAO.UITests/Project/Tests/Pages/EPAOWithdrawalPages/AS_HowWillYouSupportTheLearnersYouAreNotGoingToAssessPage.cs
@@ -14,16 +14,20 @@ namespace SFA.DAS.EPAO.UITests.Project.Tests.Pages.EPAOWithdrawalPages
         private readonly bool _feedback;
         #region Locators
         private By HowWillYouSupportLearnersTextArea => By.Id("WR-03");
+
+        private By PageTitleLocator => By.XPath($"//label[contains(text(), '{PageTitle}')]");
+
         #endregion
         public AS_HowWillYouSupportTheLearnersYouAreNotGoingToAssessPage(ScenarioContext context, bool feedback = false) : base(context)
         {
             _context = context;
             _feedback = feedback;
-            //VerifyPage();
         }
 
         public AS_HowWillYouCommunicateMarketExitToCustomersPage EnterAnswerForHowWillYouSupportLearnerYouAreNotGoingToAssess()
         {
+            VerifyPage(PageTitleLocator, PageTitle);
+
             formCompletionHelper.Click(HowWillYouSupportLearnersTextArea);
             formCompletionHelper.EnterText(HowWillYouSupportLearnersTextArea, "Learners will be supported");
             Continue();
