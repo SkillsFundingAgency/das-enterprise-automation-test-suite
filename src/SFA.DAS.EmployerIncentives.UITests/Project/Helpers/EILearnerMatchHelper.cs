8ffea3849cc4bde3324824a4f05b1b77d5c41e9f
Michael Young <mick.young1981@googlemail.com>
2021-01-28 08:29:33 +0000

    Added payments process helper

--- src/SFA.DAS.EmployerIncentives.UITests/Project/Helpers/EILearnerMatchHelper.cs
+++ src/SFA.DAS.EmployerIncentives.UITests/Project/Helpers/EILearnerMatchHelper.cs
@@ -1,64 +1,26 @@
 ﻿using System;
 using System.Net.Http;
-using System.Threading;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 
 namespace SFA.DAS.EmployerIncentives.UITests.Project.Helpers
 {
-    public class EILearnerMatchHelper
+    public class EILearnerMatchHelper : EIFunctionAppHelper
     {
-        private OrchestratorStartResponse _orchestratorStartResponse;
-        private HttpClient _httpClient;
-        private string _baseUrl;
-        private string _authenticationCode;
-
-        public EILearnerMatchHelper(EIConfig config)
+        public EILearnerMatchHelper(EIConfig config) : base(config)
         {
-            _baseUrl = config.EI_PaymentsAppBaseUrl;
-            _authenticationCode = config.EI_PaymentsAppCode;
-            _httpClient = new HttpClient();
+            BaseUrl = config.EI_PaymentsAppBaseUrl;
+            AuthenticationCode = config.EI_PaymentsAppCode;
+            HttpClient = new HttpClient();
         }
 
         public async Task StartLearnerMatchOrchestrator()
         {
-            var response = await _httpClient.GetAsync($"{_baseUrl}/orchestrators/LearnerMatchingOrchestrator?code={_authenticationCode}");
-
-            if (!response.IsSuccessStatusCode)
-            {
-                throw new Exception();
-            }
-
-            var json = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-            _orchestratorStartResponse = JsonConvert.DeserializeObject<OrchestratorStartResponse>(json);
+            await StartOrchestrator("orchestrators/LearnerMatchingOrchestrator");
         }
 
         public async Task WaitUntilComplete(TimeSpan? timeout)
         {
-            using var cts = new CancellationTokenSource();
-            if (timeout != null)
-            {
-                cts.CancelAfter(timeout.Value);
-            }
-
-            while (!cts.Token.IsCancellationRequested)
-            {
-                var response = await _httpClient.GetAsync(_orchestratorStartResponse.StatusQueryGetUri);
-
-                if (!response.IsSuccessStatusCode)
-                {
-                    throw new Exception();
-                }
-
-                var json = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                var orchestratorStatusResponse = JsonConvert.DeserializeObject<OrchestratorStatusResponse>(json);
-                if (orchestratorStatusResponse.RuntimeStatus == "Completed")
-                {
-                    return;
-                }
-
-                await Task.Delay(TimeSpan.FromMilliseconds(100));
-            }
+            await WaitUntilStatus("Completed", timeout);
         }
     }
 }
---
+++ src/SFA.DAS.EmployerIncentives.UITests/Project/Helpers/EIPaymentsProcessHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EmployerIncentives.UITests.Project.Helpers
+{
+    public class EIPaymentsProcessHelper : EIFunctionAppHelper
+    {
+        public EIPaymentsProcessHelper(EIConfig config) : base(config)
+        {
+        }
+
+        public async Task StartPaymentProcessOrchestrator(short collectionPeriodYear, byte collectionPeriodNumber)
+        {
+            await StartOrchestrator($"orchestrators/IncentivePaymentOrchestrator/{collectionPeriodYear}/{collectionPeriodNumber}");
+        }
+
+        public async Task WaitUntilWaitingForPaymentApproval(TimeSpan? timeout)
+        {
+            await WaitUntilCustomStatus("Completed", timeout);
+        }
+
+        public async Task WaitUntilComplete(TimeSpan? timeout)
+        {
+            await WaitUntilStatus("Completed", timeout);
+        }
+
+        public async Task ApprovePayments()
+        {
+            await CallHttpTrigger($"orchestrators/approvePayments/{OrchestratorStartResponse.Id}");
+        }
+
+        public async Task RejectPayments()
+        {
+            await CallHttpTrigger($"orchestrators/rejectPayments/{OrchestratorStartResponse.Id}");
+        }
+    }
+}
