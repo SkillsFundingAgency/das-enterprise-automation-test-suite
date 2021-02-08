2f00cb074988cc626fdb8a03453b124fa5004564
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2021-02-08 12:54:55 +0000

    updated sql queryu

--- src/SFA.DAS.ApprenticeCommitments.APITests/Project/Helpers/ApprenticeCommitmentSqlHelper.cs
+++ src/SFA.DAS.ApprenticeCommitments.APITests/Project/Helpers/ApprenticeCommitmentSqlHelper.cs
@@ -12,10 +12,12 @@ namespace SFA.DAS.ApprenticeCommitments.APITests.Project.Helpers
 
         public (long accountid, long apprenticeshipid, string firstname, string lastname, string trainingname, string orgname) GetEmployerData()
         {
-            var query = "SELECT TOP 1 c.EmployerAccountId, a.id, a.FirstName, a.LastName, a.TrainingName " +
-                "FROM dbo.Apprenticeship a " +
-                "JOIN dbo.Commitment c " +
-                "ON a.CommitmentId = c.Id " +
+            var query = "SELECT TOP 1 Commitment.EmployerAccountId, Apprenticeship.id, FirstName, LastName, TrainingName " +
+                "FROM[dbo].[Apprenticeship] as Apprenticeship " +
+                "INNER JOIN Commitment on Apprenticeship.CommitmentId = Commitment.Id " +
+                "INNER JOIN Accounts on Accounts.Id = Commitment.EmployerAccountId " +
+                "INNER JOIN AccountLegalEntities on Commitment.AccountLegalEntityId = AccountLegalEntities.Id " +
+                "WHERE IsApproved in (0, 1) and IsDeleted = 0 and AccountLegalEntityId is not null and AccountLegalEntities.Deleted is null " +
                 "ORDER BY NEWID()";
 
             List<object[]> apprenticeData = SqlDatabaseConnectionHelper.ReadDataFromDataBase(query, _dbConfig.CommitmentsDbConnectionString);
