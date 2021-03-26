34f5efbca6913a1c1804e79a309ef92aa4a20624
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2021-03-26 07:39:42 +0000

    reverted framework changes

--- src/SFA.DAS.UI.Framework/Hooks/AfterScenario/ErrorMessagesTearDown.cs
+++ src/SFA.DAS.UI.Framework/Hooks/AfterScenario/ErrorMessagesTearDown.cs
@@ -23,9 +23,8 @@ namespace SFA.DAS.UI.Framework.Hooks.AfterScenario
         [AfterScenario(Order = 101)]
         public void ReportErrorMessages()
         {
-            if (_objectContext.GetAfterScenarioExceptions() == null) return;
-           
             var exception = _context.TestError;
+
             if (exception != null)
             {
                 var messages = new List<string>();
