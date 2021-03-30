453915266a0918e7f7730f4b5778705e03cd5ed3
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2021-03-30 16:48:26 +0100

    updated outer api config

--- src/SFA.DAS.API.Framework/Configs/Outer_ApiAuthTokenConfig.cs
+++ src/SFA.DAS.API.Framework/Configs/Outer_ApiAuthTokenConfig.cs
@@ -2,8 +2,6 @@
 {
     public class Outer_ApiAuthTokenConfig
     {
-        public string Fatv2ApiSubscriptionKey { get; set; }
-
-        public string ApprenticeCommitmentsApiSubscriptionKey { get; set; }
+        public string Apim_SubscriptionKey { get; set; }
     }
 }
