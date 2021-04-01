af8aa57e74a62daec6276a0aaf289100f38c6b6a
44780 <44780@DESKTOP-5CF0C2V>
2021-04-01 16:17:35 +0100

    Removed 1 in 12 months check and page titles updated

--- src/SFA.DAS.Roatp.UITests/Project/Tests/Pages/RoatpApply/CriminalAndCompliance_Section3/InsolvencyOrWindingUpProceedingsPage.cs
+++ src/SFA.DAS.Roatp.UITests/Project/Tests/Pages/RoatpApply/CriminalAndCompliance_Section3/InsolvencyOrWindingUpProceedingsPage.cs
@@ -5,7 +5,7 @@ namespace SFA.DAS.Roatp.UITests.Project.Tests.Pages.RoatpApply.CriminalAndCompli
 {
     public class InsolvencyOrWindingUpProceedingsPage : RoatpApplyBasePage
     {
-        protected override string PageTitle => "Has your organisation or any of its partner organisations been subject to insolvency or winding up proceedings in the last 3 years?";
+        protected override string PageTitle => "Has your organisation been subject to insolvency or winding up proceedings in the last 3 years?";
 
         #region Helpers and Context
         private readonly ScenarioContext _context;
