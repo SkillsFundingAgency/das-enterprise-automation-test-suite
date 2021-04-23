69a3f1197e2c9ff895685d227039edd4b35f1c6a
44780 <44780@DESKTOP-5CF0C2V>
2021-04-23 11:42:11 +0100

    Pagetitle changed to H1

--- src/SFA.DAS.Roatp.UITests/Project/Tests/Pages/RoatpApply/DeliveringApprenticeshipTraining_Section7/ChooseYourOrganisationSectorsPage.cs
+++ src/SFA.DAS.Roatp.UITests/Project/Tests/Pages/RoatpApply/DeliveringApprenticeshipTraining_Section7/ChooseYourOrganisationSectorsPage.cs
@@ -7,7 +7,6 @@ namespace SFA.DAS.Roatp.UITests.Project.Tests.Pages.RoatpApply.DeliveringApprent
     {
         protected override string PageTitle => "What sectors will your organisation deliver apprenticeship training in?";
 
-        protected override By PageHeader => By.CssSelector(".govuk-label");
 
         #region Helpers and Context
         private readonly ScenarioContext _context;
