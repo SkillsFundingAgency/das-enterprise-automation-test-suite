a6c1e18bd82dd972856063708db66618c2031e29
SreekanthBadigenchula <sreekanth.badigenchula@digital.education.gov.uk>
2021-05-05 18:31:06 +0100

    feature.cs file change

--- src/SFA.DAS.EmployerIncentives.UITests/Project/Tests/Features/ShutterPages/NewLevyAccount/QualAndApprenticesWrongCommitments/EI_QSApS_NewLevyAc_WC_02.feature.cs
+++ src/SFA.DAS.EmployerIncentives.UITests/Project/Tests/Features/ShutterPages/NewLevyAccount/QualAndApprenticesWrongCommitments/EI_QSApS_NewLevyAc_WC_02.feature.cs
@@ -75,11 +75,11 @@ namespace SFA.DAS.EmployerIncentives.UITests.Project.Tests.Features.ShutterPages
         
         [NUnit.Framework.TestAttribute()]
         [NUnit.Framework.DescriptionAttribute("EI_QSApS_NewLevyAc_WC_02_Validate Shutter pages for New Levy Account with One leg" +
-            "al entity having Commitments with start date After MAY 2021")]
+            "al entity having Commitments with start date After May 2021")]
         [NUnit.Framework.CategoryAttribute("regression")]
         [NUnit.Framework.CategoryAttribute("addlevyfunds")]
         [NUnit.Framework.CategoryAttribute("employerincentives")]
-        public virtual void EI_QSApS_NewLevyAc_WC_02_ValidateShutterPagesForNewLevyAccountWithOneLegalEntityHavingCommitmentsWithStartDateAfterMAR2021()
+        public virtual void EI_QSApS_NewLevyAc_WC_02_ValidateShutterPagesForNewLevyAccountWithOneLegalEntityHavingCommitmentsWithStartDateAfterMay2021()
         {
             string[] tagsOfScenario = new string[] {
                     "regression",
@@ -87,13 +87,13 @@ namespace SFA.DAS.EmployerIncentives.UITests.Project.Tests.Features.ShutterPages
                     "employerincentives"};
             System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
             TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("EI_QSApS_NewLevyAc_WC_02_Validate Shutter pages for New Levy Account with One leg" +
-                    "al entity having Commitments with start date After MAY 2021", null, tagsOfScenario, argumentsOfScenario);
+                    "al entity having Commitments with start date After May 2021", null, tagsOfScenario, argumentsOfScenario);
 #line 6
 this.ScenarioInitialize(scenarioInfo);
 #line hidden
             bool isScenarioIgnored = default(bool);
             bool isFeatureIgnored = default(bool);
-            if ((tagsOfScenario != null)
+            if ((tagsOfScenario != null))
             {
                 isScenarioIgnored = tagsOfScenario.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
             }
