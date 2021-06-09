1cff86ab30541226c79cc35703756c835cc6d9c5
eltonlovell <44670695+eltonlovell@users.noreply.github.com>
2021-06-09 15:54:50 +0100

    Adding LTM Tests

---
+++ src/SFA.DAS.Transfers.UITests/Project/Tests/Features/LTM_01_View_Levy_Transfers.feature
@@ -0,0 +1,11 @@
+﻿Feature: LTM_01_View_Levy_Transfers
+	Log in as a Levy Paying User and View Transfers
+
+@levy_transfers
+
+
+Scenario: View Levy Transfers
+	Given I am logged in as a Levy Payer
+	And I am on the Manage Apprenticeships Page
+	When I click on view my transfers
+	Then I am taken to the View Transfers Page
---
+++ src/SFA.DAS.Transfers.UITests/Project/Tests/StepDefinitions/LevyTransfersSteps.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using SFA.DAS.Registration.UITests.Project.Helpers;
+using SFA.DAS.Registration.UITests.Project;
+using SFA.DAS.Registration.UITests.Project.Tests.Pages;
+using NUnit.Framework;
+using SFA.DAS.UI.FrameworkHelpers;
+using SFA.DAS.ConfigurationBuilder;
+using SFA.DAS.Login.Service.Helpers;
+using SFA.DAS.Login.Service;
+using SFA.DAS.Transfers.UITests.Project.Helpers;
+
+namespace SFA.DAS.Transfers.UITests.Project.Tests.StepDefinitions
+{
+    [Binding]
+    public class LevyTransfersSteps
+    {
+        [Given(@"I am logged in as a Levy Payer")]
+        public void GivenIAmLoggedInAsALevyPayer()
+        {
+            Console.WriteLine("Test OK");
+        }
+        
+        [Given(@"I am on the Manage Apprenticeships Page")]
+        public void GivenIAmOnTheManageApprenticeshipsPage()
+        {
+            Console.WriteLine("Test OK");
+        }
+        
+        [When(@"I click on view my transfers")]
+        public void WhenIClickOnViewMyTransfers()
+        {
+            Console.WriteLine("Test OK");
+        }
+        
+        [Then(@"I am taken to the View Transfers Page")]
+        public void ThenIAmTakenToTheViewTransfersPage()
+        {
+            Console.WriteLine("Test OK");
+        }
+    }
+}
