13f2df7d82dcb6c1359205027a7fed714227ef6a
eltonlovell <44670695+eltonlovell@users.noreply.github.com>
2021-06-14 10:02:16 +0100

    Adding local secrets files

--- src/SFA.DAS.Transfers.UITests/Project/Tests/Features/LTM_01_View_Levy_Transfers.feature
+++ src/SFA.DAS.Transfers.UITests/Project/Tests/Features/LTM_01_View_Levy_Transfers.feature
@@ -1,9 +1,8 @@
-﻿Feature: LTM_01_View_Levy_Transfers
+﻿
+Feature: LTM_01_View_Levy_Transfers
 	Log in as a Levy Paying User and View Transfers
 
 @levy_transfers
-
-
 Scenario: View Levy Transfers
 	Given I am logged in as a Levy Payer
 	And I am on the Manage Apprenticeships Page
--- src/SFA.DAS.Transfers.UITests/Project/Tests/Features/LTM_01_View_Levy_Transfers.feature.cs
+++ src/SFA.DAS.Transfers.UITests/Project/Tests/Features/LTM_01_View_Levy_Transfers.feature.cs
@@ -82,7 +82,7 @@ namespace SFA.DAS.Transfers.UITests.Project.Tests.Features
                     "levy_transfers"};
             System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
             TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("View Levy Transfers", null, tagsOfScenario, argumentsOfScenario);
-#line 7
+#line 6
 this.ScenarioInitialize(scenarioInfo);
 #line hidden
             bool isScenarioIgnored = default(bool);
@@ -102,16 +102,16 @@ this.ScenarioInitialize(scenarioInfo);
             else
             {
                 this.ScenarioStart();
-#line 8
+#line 7
  testRunner.Given("I am logged in as a Levy Payer", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
 #line hidden
-#line 9
+#line 8
  testRunner.And("I am on the Manage Apprenticeships Page", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
-#line 10
+#line 9
  testRunner.When("I click on view my transfers", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
 #line hidden
-#line 11
+#line 10
  testRunner.Then("I am taken to the View Transfers Page", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
             }
