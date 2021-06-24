1459498f348a551b939085cb0846a812dfb779ff
eltonlovell <44670695+eltonlovell@users.noreply.github.com>
2021-06-24 14:45:41 +0100

    Changes to config to add Levy Transfers section

--- src/SFA.DAS.Transfers.UITests/Project/Tests/StepDefinitions/LevyTransfersSteps.cs
+++ src/SFA.DAS.Transfers.UITests/Project/Tests/StepDefinitions/LevyTransfersSteps.cs
@@ -12,27 +12,56 @@ using SFA.DAS.Transfers.UITests.Project.Helpers;
 
 namespace SFA.DAS.Transfers.UITests.Project.Tests.StepDefinitions
 {
+
+
     [Binding]
     public class LevyTransfersSteps
     {
-        [Given(@"I am logged in as a Levy Payer")]
-        public void GivenIAmLoggedInAsALevyPayer()
+        private readonly ScenarioContext _context;
+        private readonly MultipleAccountsLoginHelper _multipleAccountsLoginHelper;
+        private readonly EmployerPortalLoginHelper _employerPortalLoginHelper;
+        private readonly ObjectContext _objectContext;
+        private readonly TransfersEmployerStepsHelper _employerStepsHelper;
+        private readonly TransfersProviderStepsHelper _providerStepsHelper;
+        private readonly TransfersUser _transfersUser;
+        private readonly LevyUser _levyUser;
+        private HomePage _homePage;
+
+        private readonly string _sender;
+        private readonly string _receiver;
+
+        public LevyTransfersSteps(ScenarioContext context)
         {
-            Console.WriteLine("Test OK");
+            _context = context;
+            _transfersUser = context.GetUser<TransfersUser>();
+            _levyUser = context.GetUser<LevyUser>();
+            _sender = _transfersUser.OrganisationName;
+            _receiver = _transfersUser.SecondOrganisationName;
+            _multipleAccountsLoginHelper = new MultipleAccountsLoginHelper(context, _transfersUser);
+            _employerPortalLoginHelper = new EmployerPortalLoginHelper(context);
+            _employerStepsHelper = new TransfersEmployerStepsHelper(context);
+            _providerStepsHelper = new TransfersProviderStepsHelper(context);
+            _objectContext = context.Get<ObjectContext>();
+
+
         }
-        
+
+        [Given(@"I am logged in as a Levy Payer")]
+        public void GivenIAmLoggedInAsALevyPayer() => _homePage = _employerPortalLoginHelper.Login(_levyUser, true);
+        //_context.GetUser<LevyUser>()
+
         [Given(@"I am on the Manage Apprenticeships Page")]
         public void GivenIAmOnTheManageApprenticeshipsPage()
         {
             Console.WriteLine("Test OK");
         }
-        
+
         [When(@"I click on view my transfers")]
         public void WhenIClickOnViewMyTransfers()
         {
             Console.WriteLine("Test OK");
         }
-        
+
         [Then(@"I am taken to the View Transfers Page")]
         public void ThenIAmTakenToTheViewTransfersPage()
         {
@@ -40,3 +69,5 @@ namespace SFA.DAS.Transfers.UITests.Project.Tests.StepDefinitions
         }
     }
 }
+
+
--- src/SFA.DAS.Transfers.UITests/Project/TransfersConfigurationSetup.cs
+++ src/SFA.DAS.Transfers.UITests/Project/TransfersConfigurationSetup.cs
@@ -1,6 +1,7 @@
 ﻿using SFA.DAS.ConfigurationBuilder;
 using SFA.DAS.Login.Service;
 using SFA.DAS.Login.Service.Helpers;
+using SFA.DAS.UI.Framework.TestSupport;
 using TechTalk.SpecFlow;
 
 namespace SFA.DAS.Transfers.UITests.Project
@@ -10,19 +11,29 @@ namespace SFA.DAS.Transfers.UITests.Project
     {
         private readonly ScenarioContext _context;
         private readonly IConfigSection _configSection;
+        private readonly bool _isLocalhost;
+
 
         public TransfersConfigurationSetup(ScenarioContext context)
         {
             _context = context;
             _configSection = context.Get<IConfigSection>();
+            _isLocalhost = !Configurator.IsVstsExecution;
+
         }
+    
 
         [BeforeScenario(Order = 2)]
         public void SetUpTransfersConfiguration()
         {
-            _context.SetUser(_configSection.GetConfigSection<AgreementNotSignedTransfersUser>());
 
+            _context.SetUser(_configSection.GetConfigSection<AgreementNotSignedTransfersUser>());
             _context.SetUser(_configSection.GetConfigSection<TransfersUser>());
+            _context.SetUser(_configSection.GetConfigSection<LevyUser>());
+
+
+
         }
+
     }
 }
