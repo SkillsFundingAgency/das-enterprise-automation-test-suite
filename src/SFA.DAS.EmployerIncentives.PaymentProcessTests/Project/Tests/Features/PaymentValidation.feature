7d3f3fe0a07bc04d9624a45eba36db30a37a62e9
Dave Read <david.read@digital.education.gov.uk>
2021-06-28 14:38:56 +0100

    EI-1286 add payment validation feature and steps

---
+++ src/SFA.DAS.EmployerIncentives.PaymentProcessTests/Project/Tests/Features/PaymentValidation.feature
@@ -0,0 +1,10 @@
+﻿Feature: PaymentValidation
+	Test payment validation days in learning check
+@ignore - test will fail until 90 days from start of phase 2 application date has elapsed
+@employerincentivesPaymentsProcess
+Scenario: Validation 90 Day check
+
+Given an existing apprenticeship incentive
+   When the Payment Run occurs
+   Then the HasDaysInLearning Step in PendingPaymentValidationResult table is set to true
+   Then the payment record for the first earnings is created
---
+++ src/SFA.DAS.EmployerIncentives.PaymentProcessTests/Project/Tests/StepDefinitions/PaymentValidationSteps.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using SFA.DAS.EmployerIncentives.PaymentProcessTests.Models;
+using SFA.DAS.EmployerIncentives.PaymentProcessTests.Project.Tests.Builders;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.EmployerIncentives.PaymentProcessTests.Project.Tests.StepDefinitions
+{
+    [Binding]
+    [Scope(Feature = "PaymentValidation")]
+    public class PaymentValidationSteps : StepsBase
+    {
+        private PendingPayment _pendingPayment;
+
+        protected PaymentValidationSteps(ScenarioContext context) : base(context)
+        {
+            accountId = 14326;
+            apprenticeshipId = 133218;
+        }
+
+        [Given(@"an existing apprenticeship incentive")]
+        public async Task GivenAnExistingApprenticeshipIncentive()
+        {
+            var startDate = new DateTime(2021, 04, 02);
+
+            incentiveApplication = new IncentiveApplicationBuilder()
+                .WithAccountId(accountId)
+                .WithApprenticeship(apprenticeshipId, ULN, UKPRN, startDate, startDate.AddYears(-20))
+                .Create();
+
+            await SubmitIncentiveApplication(incentiveApplication);
+
+            var priceEpisode = new PriceEpisodeDtoBuilder()
+                .WithStartDate(startDate)
+                .WithEndDate("2022-10-15T00:00:00")
+                .WithPeriod(apprenticeshipId, 11)
+                .Create();
+
+            var learnerSubmissionData = new LearnerSubmissionDtoBuilder()
+                .WithUkprn(UKPRN)
+                .WithUln(ULN)
+                .WithAcademicYear(2021)
+                .WithIlrSubmissionDate("2020-11-12T09:11:46.82")
+                .WithIlrSubmissionWindowPeriod(11)
+                .WithStartDate(startDate)
+                .WithPriceEpisode(priceEpisode)
+                .Create();
+
+            await SetupLearnerMatchApiResponse(ULN, UKPRN, learnerSubmissionData);
+            await RunLearnerMatchOrchestrator();
+        }
+
+        [When(@"the Payment Run occurs")]
+        public async Task WhenThePaymentRunOccurs()
+        {
+            byte period = 11;
+            short year = 2021;
+            await SetActiveCollectionPeriod(period, year);
+            await RunLearnerMatchOrchestrator();
+        }
+
+        [Then(@"the (.*) Step in PendingPaymentValidationResult table is set to (.*)")]
+        public void ThenTheHasDaysInLearningStepInPendingPaymentValidationResultTableIsSetTo(string stepName, bool stepValue)
+        {
+            _pendingPayment = GetFromDatabase<PendingPayment>(x => x.ApprenticeshipIncentiveId == apprenticeshipIncentiveId
+                                                                  && x.EarningType == EarningType.FirstPayment);
+
+            var validationStep = _pendingPayment.ValidationResults.FirstOrDefault(x => x.Step == stepName);
+
+            validationStep.Should().NotBeNull();
+            validationStep.Result.Should().Be(stepValue);
+        }
+
+        [Then(@"the payment record for the first earnings is created")]
+        public void ThenThePaymentRecordForTheFirstEarningsIsCreated()
+        {
+            var payment = GetFromDatabase<Payment>(x => x.PendingPaymentId == _pendingPayment.Id);
+            payment.Should().NotBeNull();
+            payment.PaymentPeriod.Should().Be(11);
+            payment.PaymentYear.Should().Be(2021);
+        }
+
+    }
+}
