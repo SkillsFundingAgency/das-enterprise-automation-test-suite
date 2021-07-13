b1a6350eb9ad8babb1db796bb6f8e6a96a89234d
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2021-07-13 09:33:07 +0100

    added emails to excluded list

--- src/SFA.DAS.TestDataCleanup/Project/Helpers/TestDataCleanUpEmailsInUse.cs
+++ src/SFA.DAS.TestDataCleanup/Project/Helpers/TestDataCleanUpEmailsInUse.cs
@@ -4,7 +4,7 @@ namespace SFA.DAS.TestDataCleanup.Project.Helpers
 {
     internal static class TestDataCleanUpEmailsInUse
     {
-        private static readonly List<string> _emails = new List<string>() { "'1'" };
+        private static readonly List<string> _emails = new List<string>() { "'first111@test.com'", "'LE_Test_101_18Aug2020_18453894282@mailinator.com'" };
 
         internal static string GetInUseEmails() { lock (_emails) { return string.Join(",", _emails); } }
 
