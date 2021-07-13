f38f408fc8a89f421f878202bd41e74ad16d6c27
Siji Odunlami <siji.odunlami@gmail.com>
2021-07-13 14:16:44 +0100

    Revert "Revert "Added MAilinator steps for email verification""
    This reverts commit 186d515881c4dd59af3fef9ebd3f247d01b654ee.

---
+++ src/SFA.DAS.AggregatedEmployerDemand.UITests/Project/Tests/StepDefinitions/MailinatorSteps.cs
@@ -0,0 +1,32 @@
+﻿using SFA.DAS.AggregatedEmployerDemand.UITests.Project.Tests.Pages.MailinatorPages;
+using SFA.DAS.ConfigurationBuilder;
+using SFA.DAS.UI.Framework;
+using SFA.DAS.UI.FrameworkHelpers;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.AggregatedEmployerDemand.UITests.Project.Tests.StepDefinitions
+{
+    [Binding]
+    public class MailinatorSteps
+    {
+        private readonly ScenarioContext _context;
+        private readonly ObjectContext _objectContext;
+        private readonly TabHelper _tabHelper;
+
+        public MailinatorSteps(ScenarioContext context)
+        {
+            _context = context;
+            _objectContext = _context.Get<ObjectContext>();
+            _tabHelper = context.Get<TabHelper>();
+        }
+
+        [Then(@"confirm the user is able to verify the email '(.*)'")]
+        public void ThenConfirmTheUserIsAbleToVerifyTheEmail(string organisationEmailAddress)
+        {
+            _tabHelper.OpenInNewTab(UrlConfig.Mailinator_BaseUrl);
+            new MailinatorLandingPage(_context).EnterEmailAndClickOnGoButton(organisationEmailAddress)
+                .ClickOnEmail()
+                .VerifyEmailLink();
+        }
+    }
+}
