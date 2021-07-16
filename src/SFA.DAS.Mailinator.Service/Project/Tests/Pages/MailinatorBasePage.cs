1818e28a287702163690fcdc49d43dabd8f9e740
Siji Odunlami <sijiodunlami@gmail.com>
2021-07-16 12:00:47 +0100

    Employer / Provider Core journey (#802)
    * Initial Provider core journey including some employer side
    
    * Provider Able to register interest complete
    
    * Added Hooks & Updated Fat helpers
    
    * Updated specflow json for config details.
    
    * Updated warning messages
    
    * Added MAilinator steps for email verification
    
    * Revert "Added MAilinator steps for email verification"
    
    This reverts commit 769e98bf37c2196b4dd61beeb8f77c2e31f488f5.
    
    * Revert "Revert "Added MAilinator steps for email verification""
    
    This reverts commit 186d515881c4dd59af3fef9ebd3f247d01b654ee.
    
    * Resolved merge conflicts
    
    * Deleted files
    
    * code review changes
    
    * code review changes
    
    * updated project name
    
    * added SFA.DAS.Mailinator.Service
    
    * code reivew changes
    
    * updated test scenarios
    
    * Updated page element
    
    Co-authored-by: Siji Odunlami <siji.odunlami@gmail.com>
    Co-authored-by: Arvinth Seran <arvinth.seran@digital.education.gov.uk>

---
+++ src/SFA.DAS.Mailinator.Service/Project/Tests/Pages/MailinatorBasePage.cs
@@ -0,0 +1,22 @@
+﻿using SFA.DAS.UI.FrameworkHelpers;
+using TechTalk.SpecFlow;
+using SFA.DAS.UI.Framework.TestSupport;
+
+namespace SFA.DAS.Mailinator.Service.Project.Tests.Pages
+{
+    public abstract class MailinatorBasePage : BasePage
+    {
+        #region Helpers and Context
+        protected readonly IFrameHelper frameHelper;
+        protected readonly FormCompletionHelper formCompletionHelper;
+        protected readonly PageInteractionHelper pageInteractionHelper;
+        #endregion
+
+        protected MailinatorBasePage(ScenarioContext context) : base(context)
+        {
+            frameHelper = context.Get<IFrameHelper>();
+            formCompletionHelper = context.Get<FormCompletionHelper>();
+            pageInteractionHelper = context.Get<PageInteractionHelper>();
+        }
+    }
+}
