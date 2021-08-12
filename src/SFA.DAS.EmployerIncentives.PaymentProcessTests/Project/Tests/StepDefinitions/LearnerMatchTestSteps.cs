4ed64f4936d54906bf312983325c308fb4177e5a
ruslanss <ruslan.schelkunov@digital.education.gov.uk>
2021-08-12 15:35:54 +0100

    EI-1336 days in learning inc. academic year rollover (#834)
    * EI-1336 Learner Match: days in learning tests
    
    * Add learning stop/start scenarios, make phase setting explicit
    
    * Indicate academic year and period in the steps
    
    * Specify feature scope
    
    * update existing tests - supply academic year, increase no. of expected validation results

--- src/SFA.DAS.EmployerIncentives.PaymentProcessTests/Project/Tests/StepDefinitions/LearnerMatchTestSteps.cs
+++ src/SFA.DAS.EmployerIncentives.PaymentProcessTests/Project/Tests/StepDefinitions/LearnerMatchTestSteps.cs
@@ -37,6 +37,7 @@ namespace SFA.DAS.EmployerIncentives.PaymentProcessTests.Project.Tests.StepDefin
             await _helper.IncentiveApplicationHelper.Submit(testData.IncentiveApplication);
 
             var priceEpisode = new PriceEpisodeDtoBuilder()
+                .WithAcademicYear(2021)
                 .WithStartDate(testData.StartDate)
                 .WithEndDate("2022-10-15T00:00:00")
                 .WithPeriod(testData.ApprenticeshipId, 7)
@@ -77,6 +78,7 @@ namespace SFA.DAS.EmployerIncentives.PaymentProcessTests.Project.Tests.StepDefin
                 .WithStartDate(testData.StartDate)
                 .WithPriceEpisode(
                     new PriceEpisodeDtoBuilder()
+                    .WithAcademicYear(2021)
                     .WithStartDate(testData.StartDate)
                     .WithEndDate("2021-07-31T00:00:00")
                     .WithPeriod(testData.ApprenticeshipId, 11)
@@ -126,6 +128,7 @@ namespace SFA.DAS.EmployerIncentives.PaymentProcessTests.Project.Tests.StepDefin
                 .WithStartDate(testData.StartDate)
                 .WithPriceEpisode(
                     new PriceEpisodeDtoBuilder()
+                    .WithAcademicYear(2022)
                     .WithStartDate("2021-08-01T00:00:00")
                     .WithEndDate("2022-07-31T00:00:00")
                     .WithPeriod(testData.ApprenticeshipId, 1)
@@ -160,6 +163,7 @@ namespace SFA.DAS.EmployerIncentives.PaymentProcessTests.Project.Tests.StepDefin
                 .WithStartDate(testData.StartDate)
                 .WithPriceEpisode(
                     new PriceEpisodeDtoBuilder()
+                    .WithAcademicYear(2022)
                     .WithStartDate("2021-08-01T00:00:00")
                     .WithEndDate("2022-07-31T00:00:00")
                     .WithPeriod(testData.ApprenticeshipId + 1, 1) // not found ApprenticeshipId
