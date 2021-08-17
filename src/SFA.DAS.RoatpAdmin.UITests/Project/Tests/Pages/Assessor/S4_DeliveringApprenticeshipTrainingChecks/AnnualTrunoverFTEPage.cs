fb4e3164fc4c26e58d64a9524de62e1d47f08efb
44780 <44780@DESKTOP-5CF0C2V>
2021-08-17 16:56:19 +0100

    Added new page and updated the Assessor Moderator methods

---
+++ src/SFA.DAS.RoatpAdmin.UITests/Project/Tests/Pages/Assessor/S4_DeliveringApprenticeshipTrainingChecks/AnnualTrunoverFTEPage.cs
@@ -0,0 +1,11 @@
+﻿using TechTalk.SpecFlow;
+
+namespace SFA.DAS.RoatpAdmin.UITests.Project.Tests.Pages.Assessor.S4_DeliveringApprenticeshipTrainingChecks
+{
+    public class AnnualTurnOverFTEPage : AssessorBasePage
+    {
+        protected override string PageTitle => "Management hierarchy for apprenticeships";
+
+        public AnnualTurnOverFTEPage(ScenarioContext context) : base(context) { }
+    }
+}
---
+++ src/SFA.DAS.RoatpAdmin.UITests/Project/Tests/Pages/Moderator/S4_DeliveringApprenticeshipTrainingChecks/AnnualTrunoverFTEPage.cs
@@ -0,0 +1,11 @@
+﻿using TechTalk.SpecFlow;
+
+namespace SFA.DAS.RoatpAdmin.UITests.Project.Tests.Pages.Moderator.S4_DeliveringApprenticeshipTrainingChecks
+{
+    public class AnnualTurnOverFTEPage : ModeratorBasePage
+    {
+        protected override string PageTitle => "Management hierarchy for apprenticeships";
+
+        public AnnualTurnOverFTEPage(ScenarioContext context) : base(context) { }
+    }
+}
