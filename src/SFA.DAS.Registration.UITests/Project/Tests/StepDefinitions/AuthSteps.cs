359b9c45132ab6c97e31764c6a5f43c97dff979e
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2021-08-23 10:11:57 +0100

    added url exclude list

--- src/SFA.DAS.Registration.UITests/Project/Tests/StepDefinitions/AuthSteps.cs
+++ src/SFA.DAS.Registration.UITests/Project/Tests/StepDefinitions/AuthSteps.cs
@@ -52,8 +52,7 @@ namespace SFA.DAS.Registration.UITests.Project.Tests.StepDefinitions
             {
                 try
                 {
-                    if ((UrlExceptionListContains().Any(x => url.ContainsCompareCaseInsensitive(x)))
-                       || (UrlExceptionListEquals().Any(x => x.CompareToIgnoreCase(url)))) continue;
+                    if (UrlException(url) || (login && UrlExceptionForLogedInUser(url))) continue;
 
                     webDriver.Navigate().GoToUrl(url);
 
@@ -70,21 +69,43 @@ namespace SFA.DAS.Registration.UITests.Project.Tests.StepDefinitions
             if (exceptions.Count > 0) throw new Exception($"{ exceptions.ToString(Environment.NewLine)}{Environment.NewLine}{authurls.ToList().ToString(Environment.NewLine)}");
         }
 
-        private List<string> UrlExceptionListContains() => 
-            new List<string> { 
-                $"https://{EnvironmentConfig.EnvironmentName}-login.apprenticeships.education.gov.uk/account/register?clientId=easacc"
+        private List<string> ExcludeUrlContains() =>
+            new List<string> {
+                $"https://{EnvName}-login.apprenticeships.education.gov.uk/account/register?clientId=easacc"
             };
 
-        private List<string> UrlExceptionListEquals() =>
-            new List<string> {
+        private List<string> ExcludeUrlEquals() =>
+            new List<string>
+            {
                 $"{UriHelper.GetAbsoluteUri(UrlConfig.EmployerApprenticeshipService_BaseUrl,"service/index?")}",
                 $"{UrlConfig.EmployerApprenticeshipService_BaseUrl}",
                 $"{UriHelper.GetAbsoluteUri(UrlConfig.EmployerApprenticeshipService_BaseUrl, "service/accounts")}",
-                $"https://{EnvironmentConfig.EnvironmentName}-login.apprenticeships.education.gov.uk/account/changepassword?clientId=easacc{EnvironmentConfig.EnvironmentName}" +
-                $"&returnurl=https%3A%2F%2Faccounts.{EnvironmentConfig.EnvironmentName}-eas.apprenticeships.education.gov.uk%2F%2Fservice%2Fpassword%2Fchange",
-                $"https://{EnvironmentConfig.EnvironmentName}-login.apprenticeships.education.gov.uk/account/changeemail?clientId=easacc{EnvironmentConfig.EnvironmentName}" +
-                $"&returnurl=https%3A%2F%2Faccounts.{EnvironmentConfig.EnvironmentName}-eas.apprenticeships.education.gov.uk%2F%2Fservice%2Femail%2Fchange",
-                $"https://accounts.{EnvironmentConfig.EnvironmentName}-eas.apprenticeships.education.gov.uk/settings/notifications"
+                $"https://{EnvName}-login.apprenticeships.education.gov.uk/account/changepassword?clientId=easacc{EnvName}" +
+                $"&returnurl=https%3A%2F%2Faccounts.{EnvName}-eas.apprenticeships.education.gov.uk%2F%2Fservice%2Fpassword%2Fchange",
+                $"https://{EnvName}-login.apprenticeships.education.gov.uk/account/changeemail?clientId=easacc{EnvName}" +
+                $"&returnurl=https%3A%2F%2Faccounts.{EnvName}-eas.apprenticeships.education.gov.uk%2F%2Fservice%2Femail%2Fchange",
+                $"https://accounts.{EnvName}-eas.apprenticeships.education.gov.uk/settings/notifications"
             };
+
+        private List<string> ExcludeUrlEqualsForLogedInUser() =>
+            new List<string>
+            {
+                $"https://accounts.{EnvName}-eas.apprenticeships.education.gov.uk/accounts/getApprenticeshipFunding",
+                $"https://accounts.{EnvName}-eas.apprenticeships.education.gov.uk/accounts/gatewayInform",
+                $"https://accounts.{EnvName}-eas.apprenticeships.education.gov.uk/accounts/organisations/search",
+                $"https://accounts.{EnvName}-eas.apprenticeships.education.gov.uk/accounts/summary"
+            };
+
+        private List<string> ExcludeUrlContainsForLogedInUser() =>
+            new List<string>
+            {
+                $"https://accounts.{EnvName}-eas.apprenticeships.education.gov.uk/accounts/organisations/search/results?searchTerm=",
+            };
+
+        private bool UrlException(string url) => (ExcludeUrlContains().Any(x => url.ContainsCompareCaseInsensitive(x)) || ExcludeUrlEquals().Any(x => x.CompareToIgnoreCase(url)));
+
+        private bool UrlExceptionForLogedInUser(string url) => (ExcludeUrlContainsForLogedInUser().Any(x => url.ContainsCompareCaseInsensitive(x)) || ExcludeUrlEqualsForLogedInUser().Any(x => x.CompareToIgnoreCase(url)));
+
+        private string EnvName => EnvironmentConfig.EnvironmentName;
     }
 }
