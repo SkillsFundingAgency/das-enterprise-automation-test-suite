b5d1c779b3c142984024064efce319390eab73c9
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2021-09-03 13:01:19 +0100

    fix for EI tests

--- src/SFA.DAS.EmployerIncentives.UITests/Project/Tests/Pages/WhenDidApprenticeJoinTheOrgPage.cs
+++ src/SFA.DAS.EmployerIncentives.UITests/Project/Tests/Pages/WhenDidApprenticeJoinTheOrgPage.cs
@@ -11,6 +11,8 @@ namespace SFA.DAS.EmployerIncentives.UITests.Project.Tests.Pages
 
         #region Locators
         private readonly ScenarioContext _context;
+
+        private By DateGroup => By.CssSelector(".govuk-date-input");
         private By DayInputField => By.Name("EmploymentStartDateDays");
         private By MonthInputField => By.Name("EmploymentStartDateMonths");
         private By YearInputField => By.Name("EmploymentStartDateYears");
@@ -20,10 +22,17 @@ namespace SFA.DAS.EmployerIncentives.UITests.Project.Tests.Pages
 
         public ConfirmApprenticesPage EnterDOBAndContinue()
         {
-            formCompletionHelper.EnterText(DayInputField, "01");
-            formCompletionHelper.EnterText(MonthInputField, "04");
-            formCompletionHelper.EnterText(YearInputField, "2021");
+            var apprenties = pageInteractionHelper.FindElements(DateGroup);
+
+            foreach (var apprentie in apprenties)
+            {
+                formCompletionHelper.EnterText(apprentie.FindElement(DayInputField), eIDataHelper.JoiningDay);
+                formCompletionHelper.EnterText(apprentie.FindElement(MonthInputField), eIDataHelper.JoiningMonth);
+                formCompletionHelper.EnterText(apprentie.FindElement(YearInputField), "2021");
+            }
+
             Continue();
+
             return new ConfirmApprenticesPage(_context);
         }
     }
