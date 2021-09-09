59f37835d5e54721b72ac4f3cfcc22409e06f771
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2021-09-09 21:58:00 +0100

    updated button css selector

--- src/SFA.DAS.RAA-V2.Service/Project/Tests/Pages/ApprenticeshipTrainingPage.cs
+++ src/SFA.DAS.RAA-V2.Service/Project/Tests/Pages/ApprenticeshipTrainingPage.cs
@@ -19,6 +19,8 @@ namespace SFA.DAS.RAA_V2.Service.Project.Tests.Pages
 
         private By CancelLink => By.LinkText("Cancel");
 
+        protected override By ContinueButton => By.CssSelector(".govuk-button.save-button");
+
         public ApprenticeshipTrainingPage(ScenarioContext context) : base(context)
         {
             _context = context;
