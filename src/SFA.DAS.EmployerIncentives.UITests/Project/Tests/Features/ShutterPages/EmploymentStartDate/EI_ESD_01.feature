7c9933d158a1c31d27391def9359855e68cde35c
Michael Young <mick.young1981@googlemail.com>
2021-09-15 14:04:46 +0100

    WIP

---
+++ src/SFA.DAS.EmployerIncentives.UITests/Project/Tests/Features/ShutterPages/EmploymentStartDate/EI_ESD_01.feature
@@ -0,0 +1,27 @@
+﻿Feature: EI_ESD_01
+
+@regression
+@employerincentives
+@addLevyFunds
+Scenario: Employer enters valid employment start date for phase 2 and a phase 3 apprenticeships
+	Given an Employer creates a Levy Account and Signs the Agreement
+	And the Employer adds 2 apprentices Aged16to24 as of 01AUG2020 with start date as Month 10 and Year 2021
+	And the Provider approves the apprenticeship request
+	And the Employer selects those apprentices on an incentive application
+	And the Employer enters an employment start date of '30/09/2021' for the first learner
+    And the Employer enters an employment start date of '01/10/2021' for the second learner
+	When the Employer selects Continue
+	Then the Confirm Apprenticeships page is displayed
+
+@regression
+@employerincentives
+@addLevyFunds
+Scenario: Employer enters an invalid employment start date for phase 2 and a phase 3 apprenticeships
+	Given an Employer creates a Levy Account and Signs the Agreement
+	And the Employer adds 2 apprentices Aged16to24 as of 01AUG2020 with start date as Month 10 and Year 2021
+	And the Provider approves the apprenticeship request
+	And the Employer selects those apprentices on an incentive application
+	And the Employer enters an employment start date of '31/03/2021' for the first learner
+    And the Employer enters an employment start date of '01/02/2021' for the second learner
+	When the Employer selects Continue
+	Then the Ineligible Employment Start Date page is displayed
---
+++ src/SFA.DAS.EmployerIncentives.UITests/Project/Tests/StepDefinitions/EmploymentStartDateSteps.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using SFA.DAS.EmployerIncentives.UITests.Project.Helpers;
+using SFA.DAS.EmployerIncentives.UITests.Project.Tests.Pages;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.EmployerIncentives.UITests.Project.Tests.StepDefinitions
+{
+    [Binding]
+    public class EmploymentStartDateSteps
+    {
+        private readonly ScenarioContext _context;
+        private EINavigationHelper _eINavigationHelper;
+        private WhenDidApprenticeJoinTheOrgPage _whenDidApprenticeJoinPage;
+
+        public EmploymentStartDateSteps(ScenarioContext context)
+        {
+            _context = context;
+            _eINavigationHelper = new EINavigationHelper(_context);
+        }
+
+        [Given(@"the Employer selects those apprentices on an incentive application")]
+        public void GivenTheEmployerSelectsThoseApprenticesOnAnIncentiveApplication()
+        {
+            var qualificationQuestionPage = _eINavigationHelper.NavigateToEISelectApprenticesPage();
+            _whenDidApprenticeJoinPage = qualificationQuestionPage.SelectYesAndContinueForEligibleApprenticesScenario().SubmitApprentices();
+        }
+
+        [Given(@"the Employer enters an employment start date of '(.*)' for the first learner")]
+        public void GivenTheEmployerEntersAnEmploymentStartDateForTheFirstLearner(DateTime employmentStartDate)
+        {
+            _whenDidApprenticeJoinPage.EnterJoiningDate(0, employmentStartDate);
+        }
+
+        [Given(@"the Employer enters an employment start date of '(.*)' for the second learner")]
+        public void GivenTheEmployerEntersAnEmploymentStartDateForTheSecondLearner(DateTime employmentStartDate)
+        {
+            _whenDidApprenticeJoinPage.EnterJoiningDate(1, employmentStartDate);
+        }
+
+        [When(@"the Employer selects Continue")]
+        public void WhenTheEmployerSelectsContinue()
+        {
+            _whenDidApprenticeJoinPage.Continue();
+        }
+
+        [Then(@"Then the Confirm Apprenticeships page is displayed")]
+        public void ThenTheConfirmApprenticeshipsPageIsDisplayed()
+        {
+            // Apparently this will verify the correct page has been displayed.
+            var confirmApprenticesPage = new ConfirmApprenticesPage(_context);
+        }
+    }
+}
