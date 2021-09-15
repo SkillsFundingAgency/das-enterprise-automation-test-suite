4a3c36265ace3c2621d7023968fda8cfc8a10cf7
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2021-09-15 11:25:16 +0100

    updated qareview page

--- src/SFA.DAS.RAA-V2-QA.UITests/Project/Tests/Pages/Reviewer/ApproveVacancyBasePage.cs
+++ src/SFA.DAS.RAA-V2-QA.UITests/Project/Tests/Pages/Reviewer/ApproveVacancyBasePage.cs
@@ -52,11 +52,4 @@ namespace SFA.DAS.RAA_V2_QA.UITests.Project.Tests.Pages.Reviewer
 
         private List<IWebElement> ErrorsCheckboxElements() => pageInteractionHelper.FindElements(ErrorsCheckboxes).ToList();
     }
-
-    public class QAReviewsPage : VerifyDetailsBasePage
-    {
-        public QAReviewsPage(ScenarioContext context) : base(context, false) => pageInteractionHelper.WaitforURLToChange("reviews");
-
-        protected override string PageTitle { get; }
-    }
 }
