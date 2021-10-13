a88bc9de3dcfddb5db556a1da55daa1c75e72ec7
ruslan <ruslanss@gmail.com>
2021-10-13 17:42:04 +0100

    Add data snapshot file to test attachments

--- src/SFA.DAS.EmployerIncentives.PaymentProcessTests/Project/Helpers/EIDataSnapper.cs
+++ src/SFA.DAS.EmployerIncentives.PaymentProcessTests/Project/Helpers/EIDataSnapper.cs
@@ -1,8 +1,9 @@
 ﻿using Dapper.Contrib.Extensions;
+using NUnit.Framework;
 using SFA.DAS.ConfigurationBuilder;
 using SFA.DAS.EmployerIncentives.PaymentProcessTests.Models;
-using System;
 using System.Data.SqlClient;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace SFA.DAS.EmployerIncentives.PaymentProcessTests.Project.Helpers
@@ -10,6 +11,7 @@ namespace SFA.DAS.EmployerIncentives.PaymentProcessTests.Project.Helpers
     public class IEDataSnapper : IEIDataSnapper
     {
         private readonly string _connectionString;
+
         private IEDataSnapper(DbConfig config)
         {
             _connectionString = config.IncentivesDbConnectionString;
@@ -17,32 +19,37 @@ namespace SFA.DAS.EmployerIncentives.PaymentProcessTests.Project.Helpers
 
         public async Task TakeDataSnapshot()
         {
-            var fileName = $"c:/temp/ei_data_snapshot_{DateTime.Now:yyyy-MM-ddTHH-mm-ss}.xlsx";
+            var file = Path.Combine(Directory.GetCurrentDirectory(), "EI_data_snapshot_{DateTime.Now:yyyy-MM-ddTHH-mm-ss}.xlsx");
 
+            await WriteToExcel(file);
+
+            TestContext.AddTestAttachment(file);
+        }
+
+        private async Task WriteToExcel(string file)
+        {
             await using var dbConnection = new SqlConnection(_connectionString);
-            {
-                var excel = new ExcelDataWriter(fileName);
+            var excel = new ExcelDataWriter(file);
 
-                await excel.TakeDataSnapshot(dbConnection.GetAll<IncentiveApplicationApprenticeship>());
-                await excel.TakeDataSnapshot(dbConnection.GetAll<ApprenticeshipIncentive>());
-                await excel.TakeDataSnapshot(dbConnection.GetAll<PendingPayment>());
-                await excel.TakeDataSnapshot(dbConnection.GetAll<PendingPaymentValidationResult>());
-                await excel.TakeDataSnapshot(dbConnection.GetAll<Learner>());
-                await excel.TakeDataSnapshot(dbConnection.GetAll<LearningPeriod>());
-                await excel.TakeDataSnapshot(dbConnection.GetAll<ApprenticeshipDaysInLearning>());
-                await excel.TakeDataSnapshot(dbConnection.GetAll<Payment>());
-                await excel.TakeDataSnapshot(dbConnection.GetAll<ClawbackPayment>());
-                await excel.TakeDataSnapshot(dbConnection.GetAll<ChangeOfCircumstance>());
-                await excel.TakeDataSnapshot(dbConnection.GetAll<ApprenticeshipBreakInLearning>());
-                await excel.TakeDataSnapshot(dbConnection.GetAll<ArchivedPendingPayment>());
-                await excel.TakeDataSnapshot(dbConnection.GetAll<ArchivedPayment>());
-                await excel.TakeDataSnapshot(dbConnection.GetAll<ArchivedPendingPaymentValidationResult>());
-            }
+            await excel.TakeDataSnapshot(dbConnection.GetAll<IncentiveApplicationApprenticeship>());
+            await excel.TakeDataSnapshot(dbConnection.GetAll<ApprenticeshipIncentive>());
+            await excel.TakeDataSnapshot(dbConnection.GetAll<PendingPayment>());
+            await excel.TakeDataSnapshot(dbConnection.GetAll<PendingPaymentValidationResult>());
+            await excel.TakeDataSnapshot(dbConnection.GetAll<Learner>());
+            await excel.TakeDataSnapshot(dbConnection.GetAll<LearningPeriod>());
+            await excel.TakeDataSnapshot(dbConnection.GetAll<ApprenticeshipDaysInLearning>());
+            await excel.TakeDataSnapshot(dbConnection.GetAll<Payment>());
+            await excel.TakeDataSnapshot(dbConnection.GetAll<ClawbackPayment>());
+            await excel.TakeDataSnapshot(dbConnection.GetAll<ChangeOfCircumstance>());
+            await excel.TakeDataSnapshot(dbConnection.GetAll<ApprenticeshipBreakInLearning>());
+            await excel.TakeDataSnapshot(dbConnection.GetAll<ArchivedPendingPayment>());
+            await excel.TakeDataSnapshot(dbConnection.GetAll<ArchivedPayment>());
+            await excel.TakeDataSnapshot(dbConnection.GetAll<ArchivedPendingPaymentValidationResult>());
         }
 
         public static IEIDataSnapper Create(DbConfig dbConfig)
         {
-#if DEBUG
+#if !DEBUG
             return new LazyDataSnapper();
 #else
             return new IEDataSnapper(dbConfig);
