a40e3369fa9dde61b2d6fb6df4be02af81c0b0c0
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2021-10-13 14:34:01 +0100

    Fix for approvals tests

--- src/SFA.DAS.TestDataExport/Helper/ApprenticePPIDataHelper.cs
+++ src/SFA.DAS.TestDataExport/Helper/ApprenticePPIDataHelper.cs
@@ -6,21 +6,27 @@ namespace SFA.DAS.TestDataExport.Helper
 {
     public class ApprenticePPIDataHelper
     {
+        private readonly bool _isApprenticeCommitments;
+        private readonly string _apprenticeEmail;
+
         public ApprenticePPIDataHelper(RandomDataGenerator randomDataGenerator, string[] _tags)
         {
             bool isPerfTest = _tags.Contains("perftest");
-            bool isApprenticeCommitments = _tags.Contains("apprenticecommitments");
+            _isApprenticeCommitments = _tags.Contains("apprenticecommitments");
+
             var emailprefix = isPerfTest ? "Apprentice_PerfTest_" : "ApprenticeAccount_";
             var emaildomain = isPerfTest ? "email.com" : "mailinator.com";
+            
             ApprenticeFirstname = $"F_{randomDataGenerator.GenerateRandomAlphabeticString(10)}";
             ApprenticeLastname = $"L_{randomDataGenerator.GenerateRandomAlphabeticString(10)}";
             DateOfBirthDay = randomDataGenerator.GenerateRandomDateOfMonth();
             DateOfBirthMonth = randomDataGenerator.GenerateRandomMonth();
             DateOfBirthYear = randomDataGenerator.GenerateRandomDobYear();
-            ApprenticeEmail = isApprenticeCommitments ? $"{emailprefix}{DateTime.Now:ddMMMyy_HHmmss_fffff}@{emaildomain}" : $"{ApprenticeFirstname}_{ApprenticeLastname}_{DateTime.Now.ToSeconds()}_{DateTime.Now.ToNanoSeconds()}@email.com";
+            
+            _apprenticeEmail = $"{emailprefix}{DateTime.Now:ddMMMyy_HHmmss_fffff}@{emaildomain}";
         }
 
-        public string ApprenticeEmail { get; }
+        public string ApprenticeEmail => _isApprenticeCommitments ? _apprenticeEmail : $"{ApprenticeFirstname}_{ApprenticeLastname}_{DateTime.Now.ToSeconds()}_{DateTime.Now.ToNanoSeconds()}@email.com";
 
         public string ApprenticeFirstname { get; set; }
 
