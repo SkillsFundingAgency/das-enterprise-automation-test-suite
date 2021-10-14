4713cd77f36cb1099b72193f3d459d9fd7de9279
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2021-10-14 11:29:22 +0100

    added test to generate uln

---
+++ src/SFA.DAS.Approvals.UITests/Project/Tests/StepDefinitions/SupportSteps.cs
@@ -0,0 +1,23 @@
+﻿using TechTalk.SpecFlow;
+using SFA.DAS.Approvals.UITests.Project.Helpers.DataHelpers;
+using SFA.DAS.ConfigurationBuilder;
+
+namespace SFA.DAS.Approvals.UITests.Project.Tests.StepDefinitions
+{
+    [Binding]
+    public class SupportSteps
+    {
+        protected readonly ApprenticeDataHelper apprenticeDataHelper;
+
+        public SupportSteps(ScenarioContext context) => apprenticeDataHelper = context.GetValue<ApprenticeDataHelper>();
+
+        [Then(@"([1-9]) ULN can be generated")]
+        public void ThenARandomULNCanBeGenerated(int noOfUln)
+        {
+            for (int i = 0; i < noOfUln; i++)
+            {
+                apprenticeDataHelper.Uln();
+            }
+        }
+    }
+}
