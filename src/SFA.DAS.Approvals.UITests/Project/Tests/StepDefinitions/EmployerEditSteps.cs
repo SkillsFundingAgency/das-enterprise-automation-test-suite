e479c047603df059ab0bade2e61edb9dbba593e8
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2021-10-15 11:17:43 +0100

    added EmployerEditSteps

---
+++ src/SFA.DAS.Approvals.UITests/Project/Tests/StepDefinitions/EmployerEditSteps.cs
@@ -0,0 +1,23 @@
+﻿using SFA.DAS.Approvals.UITests.Project.Helpers.StepsHelper;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Approvals.UITests.Project.Tests.StepDefinitions
+{
+    [Binding]
+    public class EmployerEditSteps
+    {
+
+        private readonly EmployerStepsHelper _employerStepsHelper;
+
+        public EmployerEditSteps(ScenarioContext context)
+        {
+            _employerStepsHelper = new EmployerStepsHelper(context);
+        }
+
+        [Given(@"the employer update the email address")]
+        public void GivenTheEmployerUpdateTheEmailAddress()
+        {
+            _employerStepsHelper.EditApprenticeDetailsPagePostApproval().ContinueToAddValidEmailDetails().ApproveAndNotifyTrainingProvider();
+        }
+    }
+}
