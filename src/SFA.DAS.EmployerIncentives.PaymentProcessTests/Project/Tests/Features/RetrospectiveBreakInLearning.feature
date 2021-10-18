50c23757061cc16ae7f50b90395d0e0308ac9461
Michael Young <mick.young1981@googlemail.com>
2021-10-18 11:51:36 +0100

    WIP

---
+++ src/SFA.DAS.EmployerIncentives.PaymentProcessTests/Project/Tests/Features/RetrospectiveBreakInLearning.feature
@@ -0,0 +1,13 @@
+﻿@employerincentivesPaymentsProcess
+Feature: RetrospectiveBreakInLearning
+
+Scenario: Simple Retro BIL - Learner Stopped before First Payment due date and Resumed later
+	Given an existing apprenticeship incentive with learning starting on 30-Nov-2020 and ending on 31-Jul-2021
+	And a payment of £1000 is not sent in Period R07 2021
+	And Learner data is updated with a Break in Learning before the first payment due date
+	When the Learner Match is run in Period R02 2122
+	And the earnings are recalculated
+	Then the Learner Break in Learning is recorded in Period R02 2122
+	And a new first pending payment of £1000 is created for Period R08 2021
+	And a new second pending payment of £1000 is created for Period R05 2122
+	And the Learner is In Learning
---
+++ src/SFA.DAS.EmployerIncentives.PaymentProcessTests/Project/Tests/StepDefinitions/RetrospectiveBreakInLearningSteps.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using SFA.DAS.EmployerIncentives.PaymentProcessTests.Models;
+using SFA.DAS.EmployerIncentives.PaymentProcessTests.Project.Tests.Builders;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.EmployerIncentives.PaymentProcessTests.Project.Tests.StepDefinitions
+{
+    [Binding]
+    [Scope(Feature = "RetrospectiveBreakInLearning")]
+    public class RetrospectiveBreakInLearningSteps : StepsBase
+    {
+        private DateTime _initialStartDate;
+        private DateTime _initialEndDate;
+
+        protected RetrospectiveBreakInLearningSteps(ScenarioContext context) : base(context)
+        {
+        }
+
+        [Given(@"an existing apprenticeship incentive with learning starting on (.*) and ending on (.*)")]
+        public async Task GivenAnExistingApprenticeshipIncentiveWithLearningStartingIn_Oct(DateTime startDate, DateTime endDate)
+        {
+            _initialStartDate = startDate;
+            _initialEndDate = endDate;
+            await Helper.CollectionCalendarHelper.SetActiveCollectionPeriod(6, 2021);
+
+            TestData.IncentiveApplication = new IncentiveApplicationBuilder()
+                .WithAccount(TestData.Account)
+                .WithApprenticeship(TestData.ApprenticeshipId, TestData.ULN, TestData.UKPRN,
+                    startDate, startDate.AddYears(-20), Phase.Phase1)
+                .Create();
+
+            await Helper.IncentiveApplicationHelper.Submit(TestData.IncentiveApplication);
+        }
+
+        [Given(@"a payment of £1000 is not sent in Period R07 2021")]
+        public void GivenAPaymentIsNotSent()
+        {
+        }
+
+        [Given(@"Learner data is updated with a Break in Learning before the first payment due date")]
+        public void GivenABreakInLearningBeforeTheFirstPayment()
+        {
+            throw new NotImplementedException();
+        }
+
+        [When(@"the Learner Match is run in Period R(.*) (.*)")]
+        public async Task WhenTheLearnerMatchIsRunInPeriodR(byte period, short year)
+        {
+            await Helper.CollectionCalendarHelper.SetActiveCollectionPeriod(period, year);
+            await Helper.LearnerMatchOrchestratorHelper.Run();
+        }
+
+        [When(@"the earnings are recalculated")]
+        public void WhenTheEarningsAreRecalculated()
+        {
+        }
+    }
+}
