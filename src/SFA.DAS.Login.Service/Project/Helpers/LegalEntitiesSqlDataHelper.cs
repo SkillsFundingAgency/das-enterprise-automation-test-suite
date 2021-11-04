56ae7d4a85523c5de3e192cec8140e56468464db
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2021-11-04 16:31:08 +0000

    updated fetch org name query

--- src/SFA.DAS.Login.Service/Project/Helpers/LegalEntitiesSqlDataHelper.cs
+++ src/SFA.DAS.Login.Service/Project/Helpers/LegalEntitiesSqlDataHelper.cs
@@ -2,7 +2,7 @@
 using SFA.DAS.UI.FrameworkHelpers;
 using System.Collections.Generic;
 
-namespace SFA.DAS.Login.Service
+namespace SFA.DAS.Login.Service.Project.Helpers
 {
     internal class LegalEntitiesSqlDataHelper : SqlDbHelper
     {
@@ -10,7 +10,7 @@ namespace SFA.DAS.Login.Service
 
         internal List<string> GetAccountLegalEntities(string email)
         {
-            var legalEntities = GetMultipleData($"select [name] from employer_account.AccountLegalEntity where AccountId in (select AccountId from employer_account.Membership where UserId in ( select id from employer_account.[User] where Email = '{email}' )) order by Created asc", 1).ListOfArrayToList(0);
+            var legalEntities = GetMultipleData($"select [name] from employer_account.AccountLegalEntity where  deleted is null and AccountId in (select AccountId from employer_account.Membership where UserId in ( select id from employer_account.[User] where Email = '{email}' )) order by Created asc", 1).ListOfArrayToList(0);
 
             return IsNoDataFound(legalEntities) ? new List<string>() : legalEntities;
         }
