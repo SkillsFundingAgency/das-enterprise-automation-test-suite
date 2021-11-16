d9e0a2e4550e3766422673511eddc462d3a39290
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2021-11-16 17:28:27 +0000

    added more org

--- src/SFA.DAS.Registration.UITests/Project/RegistrationConfigurationSetup.cs
+++ src/SFA.DAS.Registration.UITests/Project/RegistrationConfigurationSetup.cs
@@ -46,6 +46,14 @@ namespace SFA.DAS.Registration.UITests.Project
             _context.SetMongoDbConfig(_configSection.GetConfigSection<MongoDbConfig>());
         }
 
-        private List<string> ListOfOrganisation() => new List<string>() { "ABCD BUILDING SERVICE LTD", "COVENTRY AIRPORT LIMITED", "LOAD ESTATES LIMITED" };
+        private List<string> ListOfOrganisation() => new List<string>() 
+        { 
+            "SURREY LIMITED", "CROYDON LIMITED", "TWO LTD", "TWO A ENTERPRISES LIMITED", 
+            "THREE LTD", "FOUR ACE LIMITED", "FOUR ACRE CHEM LTD", "FOUR ACRE FIELD LTD", 
+            "FOUR ACRE SITE LTD","FIVE LIMITED", "FIVE A DAY LTD", "FIVE ACRE KOI SUPPLIES LTD", 
+            "FIVE ACRES LIMITED", "SIX LIMITED", "SIX ADRIAN SQUARE LIMITED", "SIX AND OUT LTD",
+            "SEVEN LIMITED", "SEVEN ACRES FARM LTD", "SEVEN AIR LIMITED", "SEVEN AND FOUR LIMITED",
+            "EIGHT ACCOUNTING LTD", "EIGHT AND FOUR LTD", "EIGHT BRANDS LIMITED", "NINE ALMINGTON LIMITED",
+            "NINE BLESSINGS LIMITED", "NEXT IQ LTD", "TEN ALEXANDER LIMITED", "TEN BAT LTD"};
     }
 }
