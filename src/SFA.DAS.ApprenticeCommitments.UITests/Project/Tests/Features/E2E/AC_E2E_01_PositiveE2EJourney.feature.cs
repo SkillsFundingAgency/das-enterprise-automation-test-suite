b0c3ea790acedc215d7b5c3e9c81df0d21bedb57
DESKTOP-UQKM3U7\Sree <sreekanth.badigenchula@digital.education.gov.uk>
2021-12-07 16:10:44 +0000

    Help section changes

--- src/SFA.DAS.ApprenticeCommitments.UITests/Project/Tests/Features/E2E/AC_E2E_01_PositiveE2EJourney.feature.cs
+++ src/SFA.DAS.ApprenticeCommitments.UITests/Project/Tests/Features/E2E/AC_E2E_01_PositiveE2EJourney.feature.cs
@@ -116,8 +116,8 @@ this.ScenarioInitialize(scenarioInfo);
  testRunner.Then("the apprentice is able to confirm the Overall Apprenticeship status", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
 #line 10
- testRunner.And("the apprentice is able to navigate to the Help and Support from the link on Overv" +
-                        "iew page and Navigation menu link", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+ testRunner.And("the apprentice is able to navigate to the Help and Support from the Overview page" +
+                        "", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
 #line 11
  testRunner.And("the apprentice is able to navigate to Home page back and forth from Overview and " +
