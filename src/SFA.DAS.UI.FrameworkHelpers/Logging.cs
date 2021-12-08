1df9277c4a8122bee6497173921c97bac70373b4
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2021-12-08 19:32:07 +0000

    updated ScreenshotTimeout

--- src/SFA.DAS.UI.FrameworkHelpers/Logging.cs
+++ src/SFA.DAS.UI.FrameworkHelpers/Logging.cs
@@ -19,7 +19,7 @@ namespace SFA.DAS.UI.FrameworkHelpers
 
         internal static TimeSpan[] DefaultTimeout() => new TimeSpan[] { TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(2), TimeSpan.FromSeconds(3) };
 
-        internal static TimeSpan[] ScreenshotTimeout() => new TimeSpan[] { TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1) };
+        internal static TimeSpan[] ScreenshotTimeout() => new TimeSpan[] { TimeSpan.FromMilliseconds(500), TimeSpan.FromMilliseconds(500) , TimeSpan.FromMilliseconds(500) , TimeSpan.FromMilliseconds(500) };
 
         internal static TimeSpan[] Timeout() => new TimeSpan[] { TimeSpan.FromSeconds(1) };
     }
