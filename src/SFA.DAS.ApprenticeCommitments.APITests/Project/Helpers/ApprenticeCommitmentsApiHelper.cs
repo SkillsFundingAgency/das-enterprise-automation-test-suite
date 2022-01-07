243a63ebe14fc6bddfc0850dad1b1e4f334b62e1
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2022-01-07 14:38:35 +0000

    added comments

--- src/SFA.DAS.ApprenticeCommitments.APITests/Project/Helpers/ApprenticeCommitmentsApiHelper.cs
+++ src/SFA.DAS.ApprenticeCommitments.APITests/Project/Helpers/ApprenticeCommitmentsApiHelper.cs
@@ -6,6 +6,7 @@ namespace SFA.DAS.ApprenticeCommitments.APITests.Project.Helpers
     {
         public ApprenticeCommitmentsApiHelper(ScenarioContext context) : base(context) { }
 
+        // This call can be used in TEST and TEST2 but can't be used in PP.
         public void CreateApprenticeshipViaCommitmentsJobApiRequest()
         {
             _assertHelper.RetryOnNUnitException(() => 
