b021b9c91d284c77de64614b9efe16de3a55e4c2
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2022-01-13 11:28:14 +0000

    commnted unwanted hook

--- src/SFA.DAS.UI.Framework/Hooks/BeforeScenario/ResetWebDriverSetup.cs
+++ src/SFA.DAS.UI.Framework/Hooks/BeforeScenario/ResetWebDriverSetup.cs
@@ -1,30 +1,30 @@
-﻿using SFA.DAS.UI.Framework.TestSupport;
-using TechTalk.SpecFlow;
+﻿//using SFA.DAS.UI.Framework.TestSupport;
+//using TechTalk.SpecFlow;
 
-namespace SFA.DAS.UI.Framework.Hooks.BeforeScenario
-{
-    [Binding]
-    public class ResetWebDriverSetup : WebDriverSetupBase
-    {
-        public ResetWebDriverSetup(ScenarioContext context) : base(context) { }
+//namespace SFA.DAS.UI.Framework.Hooks.BeforeScenario
+//{
+//    [Binding]
+//    public class ResetWebDriverSetup : WebDriverSetupBase
+//    {
+//        public ResetWebDriverSetup(ScenarioContext context) : base(context) { }
 
-        [BeforeScenario(Order = 5)]
-        public void SetupWebDriver()
-        {          
-            if (!IsCloudExecution() && frameworkConfig.IsVstsExecution && IsUnsupportedChromeDriverVersion(objectContext.GetChromedriverVersion()))
-            {
-                objectContext.SetFireFoxDriverLocation(FindLocalDriverServiceLocation(FirefoxDriverServiceName));
+//        [BeforeScenario(Order = 5)]
+//        public void SetupWebDriver()
+//        {          
+//            if (!IsCloudExecution() && frameworkConfig.IsVstsExecution && IsUnsupportedChromeDriverVersion(objectContext.GetChromedriverVersion()))
+//            {
+//                objectContext.SetFireFoxDriverLocation(FindLocalDriverServiceLocation(FirefoxDriverServiceName));
 
-                objectContext.SetChromeDriverLocation(FindLocalDriverServiceLocation(ChromeDriverServiceName));
+//                objectContext.SetChromeDriverLocation(FindLocalDriverServiceLocation(ChromeDriverServiceName));
 
-                objectContext.SetIeDriverLocation(FindLocalDriverServiceLocation(InternetExplorerDriverServiceName));
+//                objectContext.SetIeDriverLocation(FindLocalDriverServiceLocation(InternetExplorerDriverServiceName));
 
-                var webDriver = new RestartWebDriverHelper(context).RestartWebDriver();
+//                var webDriver = new RestartWebDriverHelper(context).RestartWebDriver();
 
-                AddCapabilities(webDriver);
-            }
-        }
+//                AddCapabilities(webDriver);
+//            }
+//        }
 
-        private bool IsUnsupportedChromeDriverVersion(string chromedriverVersion) => chromedriverVersion.StartsWith("87.0.4280.20");
-    }
-}
+//        private bool IsUnsupportedChromeDriverVersion(string chromedriverVersion) => chromedriverVersion.StartsWith("87.0.4280.20");
+//    }
+//}
