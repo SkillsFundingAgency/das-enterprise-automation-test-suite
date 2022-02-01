15e7bb4e1553f49f67e697ceadecd21d2e323e15
RangaVidela <ranga.videla@digital.education.gov.uk>
2022-02-01 11:53:23 +0000

    changes pushing

--- src/SFA.DAS.UI.Framework/UrlConfig.cs
+++ src/SFA.DAS.UI.Framework/UrlConfig.cs
@@ -8,7 +8,9 @@ namespace SFA.DAS.UI.Framework
         public static string RoATPAssessor_BaseUrl => $"https://{EnvironmentConfig.EnvironmentName}-roatp-assessor.apprenticeships.education.gov.uk/";
         public static string Apply_BaseUrl => $"https://{EnvironmentConfig.EnvironmentName}-apply.apprenticeships.education.gov.uk/";
         public static string CA_BaseUrl => EnvironmentConfig.IsPPEnvironment ? "https://preprod.apprenticeships.gov.uk/" : $"https://{EnvironmentConfig.EnvironmentName}.apprenticeships.gov.uk/";
-        public static string EmpFrontDoor_BaseUrl => "https://preprod.apprenticeships.gov.uk/";
+
+        // public static string EmpFrontDoor_BaseUrl => "https://preprod.apprenticeships.gov.uk/";
+        public static string EmpFrontDoor_BaseUrl => "https://test2-find-employer-schemes.apprenticeships.education.gov.uk/";
         public static string EPAOAssessmentService_BaseUrl => $"https://{EnvironmentConfig.EnvironmentName}-assessors.apprenticeships.education.gov.uk";
         public static string EmployerApprenticeshipService_BaseUrl => $"https://accounts.{EnvironmentConfig.EnvironmentName}-eas.apprenticeships.education.gov.uk/";
         public static string FAA_BaseUrl => $"https://{(EnvironmentConfig.EnvironmentName).ToLower()}.findapprenticeship.service.gov.uk/";
