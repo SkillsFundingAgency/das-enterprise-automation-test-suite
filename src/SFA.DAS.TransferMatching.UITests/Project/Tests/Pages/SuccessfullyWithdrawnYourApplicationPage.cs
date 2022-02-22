741cf07bdec0df488aee5bbf140f2d2dda0ba626
Ehtishaam-Ansari <ehtishaam.ansari@digital.education.gov.uk>
2022-02-22 17:20:25 +0000

    new test

---
+++ src/SFA.DAS.TransferMatching.UITests/Project/Tests/Pages/SuccessfullyWithdrawnYourApplicationPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.TransferMatching.UITests.Project.Tests.Pages
+{
+    public class SuccessfullyWithdrawnYourApplicationPage : TransferMatchingBasePage
+    {
+        protected override string PageTitle => "You have successfully withdrawn your application";
+
+        public SuccessfullyWithdrawnYourApplicationPage(ScenarioContext context) : base(context) { }
+
+        public AccountHomePage ReturnToMyAccount()
+        {
+            formCompletionHelper.ClickLinkByText("Continue to my account");
+            return new AccountHomePage(context);
+        }
+    }
+}
