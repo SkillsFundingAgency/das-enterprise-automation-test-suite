b359b718aeef9ddfa9c11072d7adbe8f5525eb4e
ehtishaam-ansari <82220765+ehtishaam-ansari@users.noreply.github.com>
2022-03-09 13:10:45 +0000

    ltmatutomationtesting new test for bulk reject (#1094)
    * new test for bulk reject
    
    * code review changes
    
    Co-authored-by: Arvinth Seran <arvinth.seran@digital.education.gov.uk>

---
+++ src/SFA.DAS.TransferMatching.UITests/Project/Tests/Features/TM_23_SenderCanBulkReject.feature
@@ -0,0 +1,10 @@
+﻿Feature: TM_23_SenderCanBulkReject
+
+@regression
+@transfermatching
+Scenario: TM_23_Sender can bulk reject application made by receiver
+Given the levy employer logins using existing transfer matching account
+	Then the levy employer can create pledge using default criteria
+	And the levy employer can view pledges from verification page
+	When the non levy employer applies for the pledge
+	Then the levy employer can bulk reject application 
--- src/SFA.DAS.TransferMatching.UITests/Project/Tests/StepDefinitions/TransferMatchingSteps.cs
+++ src/SFA.DAS.TransferMatching.UITests/Project/Tests/StepDefinitions/TransferMatchingSteps.cs
@@ -83,7 +83,7 @@ namespace SFA.DAS.TransferMatching.UITests.Project.Tests.StepDefinitions
             => AssertErrorMessage(ApplyForAnInvalidPledge(_context.GetUser<NonLevyUser>()).EnterAmountMoreThanAvailableFunding(), "There is not enough funding to support this many apprentices");
 
         [Then(@"the levy employer can download excel file")]
-        public void ThenTheLevyEmployerCanDownloadExcelFilen() => GoToTransferPledgePage().DownloadExcel();
+        public void ThenTheLevyEmployerCanDownloadExcelFilen() => GoToTransferPledgePageAsReceiver().DownloadExcel();
 
         [Then(@"the levy employer can close the pledge")]
         public void ThenTheLevyEmployerCanCloseThePledge() => ClosePledge().ConfirmClose();
@@ -198,6 +198,9 @@ namespace SFA.DAS.TransferMatching.UITests.Project.Tests.StepDefinitions
         [Then(@"the levy employer is able to apply for transfer opportunities")]
         public void ThenTheLevyEmployerIsAbletoApplyForTransferOpportunities() => CanApplyForTransferOppurtunity(true);
 
+        [Then(@"the levy employer can bulk reject application")]
+        public void ThenTheLevyEmployerCanBulkRejectApplication() => BulkReject();
+
         private ApplicationsDetailsPage OpenApprovedPledgeApplication()
         {
             UpdateOrganisationName(_sender);
@@ -220,27 +223,24 @@ namespace SFA.DAS.TransferMatching.UITests.Project.Tests.StepDefinitions
 
         private ApprovingTheApprenticeshipDetailsPage GoToApprovingTheApprenticeshipDetailsPage() => GoToApproveAppliationPage().GoToApprovingTheApprenticeshipDetailsPage();
 
-        private ClosePledgePage ClosePledge()
-        {
-            SignOut(); 
-            
-            LoginAsSender(_context.GetUser<TransferMatchingUser>());
+        private ClosePledgePage ClosePledge() => GoToTransferPledgePageAsSender().ClosePledge();
 
-            return NavigateToTransferMatchingPage().GoToViewMyTransferPledgePage().GoToTransferPledgePage().ClosePledge();
-        }
+        private TransferPledgePage BulkReject() => GoToTransferPledgePageAsSender().SelectBulkReject().CancelBulkReject().SelectBulkReject().BulkReject();
 
-        private TransferPledgePage SortApplications()
+        private TransferPledgePage SortApplications() => GoToTransferPledgePageAsSender().SortByApplicant();
+
+        private TransferPledgePage GoToTransferPledgePageAsSender()
         {
             SignOut();
 
             LoginAsSender(_context.GetUser<TransferMatchingUser>());
 
-            return NavigateToTransferMatchingPage().GoToViewMyTransferPledgePage().GoToTransferPledgePage().SortByApplicant();
+            return NavigateToTransferMatchingPage().GoToViewMyTransferPledgePage().GoToTransferPledgePage();
         }
 
-        private ApproveAppliationPage GoToApproveAppliationPage() => GoToTransferPledgePage().GoToApproveAppliationPage();
+        private ApproveAppliationPage GoToApproveAppliationPage() => GoToTransferPledgePageAsReceiver().GoToApproveAppliationPage();
 
-        private TransferPledgePage GoToTransferPledgePage()
+        private TransferPledgePage GoToTransferPledgePageAsReceiver()
         {
             SignOut();
 
