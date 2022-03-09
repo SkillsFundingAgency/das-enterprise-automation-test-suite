71df956bf2f37cdecbf1b7aeaf77e9b1a97c9a9f
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2022-03-09 12:20:14 +0000

    added custom specflow plugin

---
+++ src/SFA.DAS.FrameworkHelpers/TestMethodTagDecorator.cs
@@ -0,0 +1,23 @@
+﻿
+using System.CodeDom;
+using TechTalk.SpecFlow.Generator;
+using TechTalk.SpecFlow.Generator.UnitTestConverter;
+
+namespace SFA.DAS.FrameworkHelpers
+{
+    public class TestMethodTagDecorator : ITestMethodTagDecorator
+    {
+        public static readonly string TAG_NAME = "donotexecuteinparallel";
+        private readonly ITagFilterMatcher _tagFilterMatcher;
+
+        public TestMethodTagDecorator(ITagFilterMatcher tagFilterMatcher) => _tagFilterMatcher = tagFilterMatcher;
+
+        public bool CanDecorateFrom(string tagName, TestClassGenerationContext generationContext, CodeMemberMethod testMethod) => _tagFilterMatcher.Match(TAG_NAME, tagName);
+
+        public void DecorateFrom(string tagName, TestClassGenerationContext generationContext, CodeMemberMethod testMethod) => testMethod.CustomAttributes.Add(new CodeAttributeDeclaration("NUnit.Framework.NonParallelizableAttribute"));
+
+        public int Priority { get; }
+        public bool RemoveProcessedTags { get; }
+        public bool ApplyOtherDecoratorsForProcessedTags { get; }
+    }
+}
---
+++ src/SFA.DAS.FrameworkHelpers/TestMethodTagGeneratorPlugin.cs
@@ -0,0 +1,22 @@
+﻿
+using SFA.DAS.FrameworkHelpers;
+using TechTalk.SpecFlow.Generator.Plugins;
+using TechTalk.SpecFlow.Generator.UnitTestConverter;
+using TechTalk.SpecFlow.Infrastructure;
+using TechTalk.SpecFlow.UnitTestProvider;
+
+[assembly: GeneratorPlugin(typeof(TestMethodTagGeneratorPlugin))]
+
+namespace SFA.DAS.FrameworkHelpers
+{
+    public class TestMethodTagGeneratorPlugin : IGeneratorPlugin
+    {
+        public void Initialize(GeneratorPluginEvents generatorPluginEvents, 
+            GeneratorPluginParameters generatorPluginParameters,
+            UnitTestProviderConfiguration unitTestProviderConfiguration) 
+            => generatorPluginEvents.RegisterDependencies += RegisterDependencies;
+
+        private void RegisterDependencies(object sender, RegisterDependenciesEventArgs eventArgs) 
+            => eventArgs.ObjectContainer.RegisterTypeAs<TestMethodTagDecorator, ITestMethodTagDecorator>(TestMethodTagDecorator.TAG_NAME);
+    }
+}
