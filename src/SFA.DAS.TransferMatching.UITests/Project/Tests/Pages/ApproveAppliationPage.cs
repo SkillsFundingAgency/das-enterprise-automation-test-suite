cfacc99d92aeff660d0d2b0313662bb29a88bb75
Ehtishaam-Ansari <ehtishaam.ansari@digital.education.gov.uk>
2022-03-10 02:31:23 +0000

    new test and increase coverage of old tests

--- src/SFA.DAS.TransferMatching.UITests/Project/Tests/Pages/ApproveAppliationPage.cs
+++ src/SFA.DAS.TransferMatching.UITests/Project/Tests/Pages/ApproveAppliationPage.cs
@@ -9,12 +9,16 @@ namespace SFA.DAS.TransferMatching.UITests.Project.Tests.Pages
         protected override string PageTitle => objectContext.GetOrganisationName();
 
         protected override By ContinueButton => By.CssSelector("#main-content .govuk-button");
+        private By EstimatedCostText => By.LinkText("The estimated total cost of training is based on the funding band maximum, the planned start date of training and the 20% completion payment.");
+        private By ApproveDisclaimerText => By.LinkText("You may wish to undertake your own in -house governance process before committing to fund a transfer.The receiving employer will provide you with their contact details as part of their application in order to do this. Before this is confirmed, you will need to choose how to approve the apprentice details on the next page.");
 
         public ApproveAppliationPage(ScenarioContext context) : base(context) { }
 
         public ApprovingTheApprenticeshipDetailsPage GoToApprovingTheApprenticeshipDetailsPage()
         {
+            pageInteractionHelper.IsElementDisplayed(EstimatedCostText);
             SelectRadioOptionByText("Approve the application");
+            pageInteractionHelper.IsElementDisplayed(ApproveDisclaimerText);
             Continue();
             return new ApprovingTheApprenticeshipDetailsPage(context);
         }
--- src/SFA.DAS.TransferMatching.UITests/Project/Tests/Pages/MyTransferPledgesPage.cs
+++ src/SFA.DAS.TransferMatching.UITests/Project/Tests/Pages/MyTransferPledgesPage.cs
@@ -10,7 +10,8 @@ namespace SFA.DAS.TransferMatching.UITests.Project.Tests.Pages
         private By CreatePledgesSelector => By.CssSelector("[href*='/pledges/create/inform']");
 
         private By PledgeSelector => By.CssSelector($"a[href='pledges/{GetPledgeId()}/applications']");
-
+        private By ActiveStatusSelector => By.CssSelector("#main-content > div > div > div.govuk-grid-column-two-thirds > table > tbody > tr > td:nth-child(5) > strong");
+        private By ClosedStatusSelector => By.CssSelector("#main-content > div > div:nth-child(2) > div.govuk-grid-column-two-thirds > table > tbody > tr:nth-child(2) > td:nth-child(5) > strong");
         public MyTransferPledgesPage(ScenarioContext context) : base(context) { }
 
         public TransferPledgePage GoToTransferPledgePage()
@@ -24,7 +25,18 @@ namespace SFA.DAS.TransferMatching.UITests.Project.Tests.Pages
             formCompletionHelper.Click(CreatePledgesSelector);
             return new PledgeAndTransferYourLevyFundsPage(context);
         }
+        public MyTransferPledgesPage ConfirmCloseStatus()
+        {
+            pageInteractionHelper.IsElementDisplayed(ClosedStatusSelector);
+            return new MyTransferPledgesPage(context);
+        }
+        public MyTransferPledgesPage ConfirmActiveStatus()
+        {
+            pageInteractionHelper.IsElementDisplayed(ActiveStatusSelector);
+            return new MyTransferPledgesPage(context);
+        }
 
         public void VerifyPledge() => VerifyElement(PledgeSelector);
+
     }
 }
