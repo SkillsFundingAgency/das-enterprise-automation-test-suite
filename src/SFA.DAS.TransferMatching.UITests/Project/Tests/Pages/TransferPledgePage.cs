1fdd59e6f0e952f48c00749f36ffa1a5f5addab3
Ehtishaam-Ansari <ehtishaam.ansari@digital.education.gov.uk>
2022-03-10 14:43:03 +0000

    change selector

--- src/SFA.DAS.TransferMatching.UITests/Project/Tests/Pages/TransferPledgePage.cs
+++ src/SFA.DAS.TransferMatching.UITests/Project/Tests/Pages/TransferPledgePage.cs
@@ -13,8 +13,7 @@ namespace SFA.DAS.TransferMatching.UITests.Project.Tests.Pages
 
         private By DownloadSelector => By.CssSelector("#main-content > div > div:nth-child(1) > div.govuk-grid-column-one-third > p > a");
 
-        private By ClosePLedgeSelector => By.CssSelector("#main-content > div > div:nth-child(2) > div > div > form:nth-child(2) > button");
-        private By ClosePledgeSelector2 => By.CssSelector("#main-content > div > div:nth-child(3) > div > div > form:nth-child(2) > button");
+        private By ClosePLedgeSelector => By.Id("close-pledge-button");
         private By RejectContinueSelector => By.CssSelector("#applications-action");
         private By CheckBoxSelector => By.ClassName("govuk-checkboxes__input");
         public TransferPledgePage(ScenarioContext context) : base(context) { }
