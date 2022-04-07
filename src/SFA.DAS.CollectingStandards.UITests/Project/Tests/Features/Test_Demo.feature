6f3c414affe9e09ef01c2f3461c75a9caeefbd59
44780 <44780@DESKTOP-5CF0C2V>
2022-04-07 17:44:49 +0100

    Created New Project for Collecting Standards and added a Demo TEST

---
+++ src/SFA.DAS.CollectingStandards.UITests/Project/Tests/Features/Test_Demo.feature
@@ -0,0 +1,6 @@
+﻿Feature: CS_Testing_Demo_Feature
+
+@CollectingStandards01
+
+Scenario: CS_Test Demo
+	Given the provider land on Provider portal page
---
+++ src/SFA.DAS.CollectingStandards.UITests/Project/Tests/StepDefinitions/CS_DemoSteps.cs
@@ -0,0 +1,31 @@
+﻿using SFA.DAS.ConfigurationBuilder;
+using SFA.DAS.UI.Framework;
+using SFA.DAS.UI.FrameworkHelpers;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.CollectingStandards.UITests.Project.Tests.StepDefinitions
+{
+    [Binding]
+    public class CS_DemoSteps
+    {
+        private readonly ScenarioContext _context;
+        private readonly ObjectContext _objectContext;
+        private readonly TabHelper _tabHelper;
+
+
+        public CS_DemoSteps(ScenarioContext context)
+        {
+            _context = context;
+            _objectContext = context.Get<ObjectContext>();
+            _tabHelper = context.Get<TabHelper>();
+        }
+    
+        [Given(@"the provider land on Provider portal page")]
+        public void GivenTheProviderLandOnProviderPortalPage()
+        {
+            GoToProviderHomePage();
+        }
+        private void GoToProviderHomePage() => _tabHelper.GoToUrl(UrlConfig.Provider_BaseUrl);
+    }
+}
