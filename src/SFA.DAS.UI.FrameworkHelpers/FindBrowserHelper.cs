eb86d8a886a5ecc31912ea1b62038ba12740876a
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2022-04-08 10:58:34 +0100

    updated local to be chrome driver

--- src/SFA.DAS.UI.FrameworkHelpers/FindBrowserHelper.cs
+++ src/SFA.DAS.UI.FrameworkHelpers/FindBrowserHelper.cs
@@ -8,14 +8,14 @@ namespace SFA.DAS.UI.FrameworkHelpers
 
         public static bool IsZap(this string browser) => browser.CompareToIgnoreCase("zapProxyChrome");
 
-        public static bool IsEdge(this string browser) => browser.CompareToIgnoreCase("edge") || browser.CompareToIgnoreCase("microsoftedge") ;
+        public static bool IsEdge(this string browser) => browser.CompareToIgnoreCase("edge") || browser.CompareToIgnoreCase("microsoftedge");
 
         public static bool IsIe(this string browser) => browser.CompareToIgnoreCase("ie") || browser.CompareToIgnoreCase("internetexplorer");
 
-        public static bool IsFirefox(this string browser) => browser.CompareToIgnoreCase("firefox") || browser.CompareToIgnoreCase("mozillafirefox") ;
+        public static bool IsFirefox(this string browser) => browser.CompareToIgnoreCase("firefox") || browser.CompareToIgnoreCase("mozillafirefox");
 
-        public static bool IsChrome(this string browser) => browser.CompareToIgnoreCase("chrome") || browser.CompareToIgnoreCase("googlechrome") ;
+        public static bool IsChrome(this string browser) => browser.CompareToIgnoreCase("chrome") || browser.CompareToIgnoreCase("googlechrome") || browser.CompareToIgnoreCase("local");
 
-        public static bool IsChromeHeadless(this string browser) => browser.CompareToIgnoreCase("chromeheadless") || browser.CompareToIgnoreCase("headlessbrowser") || browser.CompareToIgnoreCase("headless") || browser.CompareToIgnoreCase("local");
+        public static bool IsChromeHeadless(this string browser) => browser.CompareToIgnoreCase("chromeheadless") || browser.CompareToIgnoreCase("headlessbrowser") || browser.CompareToIgnoreCase("headless");
     }
 }
