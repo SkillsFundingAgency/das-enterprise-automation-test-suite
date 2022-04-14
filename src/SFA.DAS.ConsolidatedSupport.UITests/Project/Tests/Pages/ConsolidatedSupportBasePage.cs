797a09fce282b5e186c9e567847508d9a2540baf
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2022-04-14 15:36:12 +0100

    removed unused using

--- src/SFA.DAS.ConsolidatedSupport.UITests/Project/Tests/Pages/ConsolidatedSupportBasePage.cs
+++ src/SFA.DAS.ConsolidatedSupport.UITests/Project/Tests/Pages/ConsolidatedSupportBasePage.cs
@@ -2,7 +2,6 @@
 using SFA.DAS.ConsolidatedSupport.UITests.Project.Helpers;
 using SFA.DAS.UI.Framework;
 using SFA.DAS.UI.Framework.TestSupport;
-using System;
 using System.Linq;
 using TechTalk.SpecFlow;
 
