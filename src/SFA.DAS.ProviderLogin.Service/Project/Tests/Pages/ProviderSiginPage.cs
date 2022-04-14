0070888d456341d0e2e625e81fccfd34b4f81de2
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2022-04-14 11:13:20 +0100

    updated provider login to resue idams

--- src/SFA.DAS.ProviderLogin.Service/Project/Tests/Pages/ProviderSiginPage.cs
+++ src/SFA.DAS.ProviderLogin.Service/Project/Tests/Pages/ProviderSiginPage.cs
@@ -1,43 +1,18 @@
-﻿using OpenQA.Selenium;
+﻿using SFA.DAS.IdamsLogin.Service.Project.Tests.Pages;
 using SFA.DAS.ProviderLogin.Service.Project.Helpers;
 using TechTalk.SpecFlow;
 
 namespace SFA.DAS.ProviderLogin.Service.Pages
 {
-    public class ProviderSiginPage : ProviderLoginBasePage
+    public class ProviderSiginPage : SignInBasePage
     {
-        protected override string PageTitle => "Sign in";
-
-        protected override bool TakeFullScreenShot => false;
-
-        protected override By PageHeader => By.CssSelector(".pageTitle");
-
         public ProviderSiginPage(ScenarioContext context) : base(context) { }
-
-        private By UsernameField => By.Id("username");
-        private By PasswordField => By.Id("password");
-        private By SignInButton => By.XPath("//button[@value='Log in']");
 
         public ProviderHomePage SubmitValidLoginDetails(ProviderLoginUser login)
         {
-            EnterEmailAddress(login.UserId)
-                    .EnterPassword(login.Password)
-                    .SignIn();
+            SubmitValidLoginDetails(login.UserId, login.Password);
+
             return new ProviderHomePage(context);
         }  
-
-        private ProviderSiginPage EnterEmailAddress(string username)
-        {
-            formCompletionHelper.EnterText(UsernameField, username);
-            return this;
-        }
-
-        private ProviderSiginPage EnterPassword(string password)
-        {
-            formCompletionHelper.EnterText(PasswordField, password);
-            return this;
-        }
-
-        private void SignIn() => formCompletionHelper.ClickElement(SignInButton);
     }
 }
