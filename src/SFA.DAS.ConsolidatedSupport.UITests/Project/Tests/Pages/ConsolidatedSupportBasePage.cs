1947e1d1ec7e7cc2c1e641067c24b7e731393427
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2022-04-25 11:30:37 +0100

    updated url - fix for CS_NU_01_New test

--- src/SFA.DAS.ConsolidatedSupport.UITests/Project/Tests/Pages/ConsolidatedSupportBasePage.cs
+++ src/SFA.DAS.ConsolidatedSupport.UITests/Project/Tests/Pages/ConsolidatedSupportBasePage.cs
@@ -20,7 +20,7 @@ namespace SFA.DAS.ConsolidatedSupport.UITests.Project.Tests.Pages
             dataHelper = context.Get<ConsolidateSupportDataHelper>();
         }
 
-        protected void NavigateTo(string url) => tabHelper.GoToUrl(UrlConfig.ConsolidatedSupport_WebBaseUrl, $"users/{objectContext.GetUserId()}/{url}");
+        protected void NavigateTo(string url) => tabHelper.GoToUrl($"{UrlConfig.ConsolidatedSupport_WebBaseUrl}/users/{objectContext.GetUserId()}/{url}");
 
         protected void CloseAllTickets()
         {
