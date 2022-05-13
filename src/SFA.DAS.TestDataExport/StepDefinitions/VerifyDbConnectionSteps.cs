8565bbdeb7111fedbb41d4b9214b59b8f4e5b1f4
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2022-05-13 19:19:13 +0100

    Assert Multiple implementation

--- src/SFA.DAS.TestDataExport/StepDefinitions/VerifyDbConnectionSteps.cs
+++ src/SFA.DAS.TestDataExport/StepDefinitions/VerifyDbConnectionSteps.cs
@@ -12,13 +12,25 @@ namespace SFA.DAS.TestDataExport.StepDefinitions
 
         public VerifyDbConnectionSteps(ScenarioContext context) => _dbConfig = context.Get<DbConfig>();
         
-
         [Then(@"the db connection are verified")]
         public void ThenTheDbConnectionAreVerified()
         {
-            AssertDbConnection(new TestDataCleanUpEasLtmcSqlDataHelper(_dbConfig).GetTableCatalog());
+            Assert.Multiple(() => 
+            {
+                AssertDbConnection(new EasAccDbSqlDataHelper(_dbConfig));
+                AssertDbConnection(new TestDataCleanupAComtSqlDataHelper(_dbConfig));
+                AssertDbConnection(new TestDataCleanupComtSqlDataHelper(_dbConfig));
+                AssertDbConnection(new TestDataCleanUpEasLtmcSqlDataHelper(_dbConfig));
+                AssertDbConnection(new TestDataCleanUpEmpFcastSqlDataHelper(_dbConfig));
+                AssertDbConnection(new TestDataCleanUpEmpFinSqlDataHelper(_dbConfig));
+                AssertDbConnection(new TestDataCleanUpEmpIncSqlDataHelper(_dbConfig));
+                AssertDbConnection(new TestDataCleanUpPfbeDbSqlDataHelper(_dbConfig));
+                AssertDbConnection(new TestDataCleanUpPrelDbSqlDataHelper(_dbConfig));
+                AssertDbConnection(new TestDataCleanUpPsrDbSqlDataHelper(_dbConfig));
+                AssertDbConnection(new TestDataCleanUpRsvrSqlDataHelper(_dbConfig));
+            });
         }
 
-        private void AssertDbConnection(string actual) => StringAssert.StartsWith("das-", actual);
+        private void AssertDbConnection(ProjectSqlDbHelper helper) => StringAssert.StartsWith("das-", helper.GetTableCatalog());
     }
 }
