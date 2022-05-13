4e38fb9f9fcfe5f80d4f5252e9130d133a37f357
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2022-05-13 19:03:06 +0100

    updated csproj to discover step bindings

--- src/SFA.DAS.EPAO.UITests/Project/Tests/Features/E2E/EPAO_E2E_01.feature.cs
+++ src/SFA.DAS.EPAO.UITests/Project/Tests/Features/E2E/EPAO_E2E_01.feature.cs
@@ -35,7 +35,7 @@ namespace SFA.DAS.EPAO.UITests.Project.Tests.Features.E2E
         public virtual void FeatureSetup()
         {
             testRunner = TechTalk.SpecFlow.TestRunnerManager.GetTestRunner();
-            TechTalk.SpecFlow.FeatureInfo featureInfo = new TechTalk.SpecFlow.FeatureInfo(new System.Globalization.CultureInfo("en-US"), "Project/Tests/Features/E2E", "EPAO_E2E_01", null, ProgrammingLanguage.CSharp, featureTags);
+            TechTalk.SpecFlow.FeatureInfo featureInfo = new TechTalk.SpecFlow.FeatureInfo(new System.Globalization.CultureInfo("en-GB"), "Project/Tests/Features/E2E", "EPAO_E2E_01", null, ProgrammingLanguage.CSharp, featureTags);
             testRunner.OnFeatureStart(featureInfo);
         }
         
--- src/SFA.DAS.EPAO.UITests/Project/Tests/Features/TestDataPreparation/VerifyDbConnection.feature.cs
+++ src/SFA.DAS.EPAO.UITests/Project/Tests/Features/TestDataPreparation/VerifyDbConnection.feature.cs
@@ -35,7 +35,7 @@ namespace SFA.DAS.EPAO.UITests.Project.Tests.Features.TestDataPreparation
         public virtual void FeatureSetup()
         {
             testRunner = TechTalk.SpecFlow.TestRunnerManager.GetTestRunner();
-            TechTalk.SpecFlow.FeatureInfo featureInfo = new TechTalk.SpecFlow.FeatureInfo(new System.Globalization.CultureInfo("en-US"), "Project/Tests/Features/TestDataPreparation", "VerifyDbConnection", null, ProgrammingLanguage.CSharp, featureTags);
+            TechTalk.SpecFlow.FeatureInfo featureInfo = new TechTalk.SpecFlow.FeatureInfo(new System.Globalization.CultureInfo("en-GB"), "Project/Tests/Features/TestDataPreparation", "VerifyDbConnection", null, ProgrammingLanguage.CSharp, featureTags);
             testRunner.OnFeatureStart(featureInfo);
         }
         
