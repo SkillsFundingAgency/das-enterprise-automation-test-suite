d88f2e9b2b4262553e8bf671317f9932c3a71590
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2022-06-01 15:49:17 +0100

    Framework : dotnet6 migration (#1160)
    * updated projects to .Net 6
    
    * updated environment.json file
    
    * added ErrorOnDuplicatePublishOutputFiles to the project group
    
    * migrated AggregatedEmployerDemand project
    
    * removed runtime config file - employer finance
    
    * removed all runtime config json files

--- src/SFA.DAS.AggregatedEmployerDemand.UITests/Project/Tests/Pages/ProviderPages/EditProvidersContactDetailsPage.cs
+++ src/SFA.DAS.AggregatedEmployerDemand.UITests/Project/Tests/Pages/ProviderPages/EditProvidersContactDetailsPage.cs
@@ -1,55 +1,54 @@
 ﻿using OpenQA.Selenium;
 using TechTalk.SpecFlow;
 
-namespace SFA.DAS.AggregatedEmployerDemand.UITests.Project.Tests.Pages.ProviderPages
+namespace SFA.DAS.AggregatedEmployerDemand.UITests.Project.Tests.Pages.ProviderPages;
+
+public class EditProvidersContactDetailsPage : AedBasePage
 {
-    public class EditProvidersContactDetailsPage : AedBasePage
+    protected override string PageTitle => "Edit";
+
+    protected override bool TakeFullScreenShot => false;
+
+    public EditProvidersContactDetailsPage(ScenarioContext context) : base(context)  { }
+
+    #region Locators
+    private static By EmailAddressTextBox => By.CssSelector("#EmailAddress");
+    private static By TelephoneNumberTextBox => By.CssSelector("#PhoneNumber");
+    private static By WebsiteTextBox => By.CssSelector("#Website");
+    #endregion
+
+    private EditProvidersContactDetailsPage EnterProvidersContactDetails(string email, string telephone, string website)
     {
-        protected override string PageTitle => "Edit";
+        formCompletionHelper.EnterText(EmailAddressTextBox, email);
+        formCompletionHelper.EnterText(TelephoneNumberTextBox, telephone);
+        formCompletionHelper.EnterText(WebsiteTextBox, website);
+        return this;
+    }
 
-        protected override bool TakeFullScreenShot => false;
+    public EditProvidersContactDetailsPage DoNotEnterDetails() => StayOnEditProvidersContactDetailsPage();
 
-        public EditProvidersContactDetailsPage(ScenarioContext context) : base(context)  { }
+    public EditProvidersContactDetailsPage EnterInvalidDetails()
+    {
+        EnterProvidersContactDetails($"{dataHelper.RandomEmail}@gmail.com", $"{dataHelper.TelephoneNumber}ABC", dataHelper.RandomWebsiteAddress);
+        return StayOnEditProvidersContactDetailsPage();
+    }
 
-        #region Locators
-        private By EmailAddressTextBox => By.CssSelector("#EmailAddress");
-        private By TelephoneNumberTextBox => By.CssSelector("#PhoneNumber");
-        private By WebsiteTextBox => By.CssSelector("#Website");
-        #endregion
+    public ConfirmProvidersContactDetailsPage EnterValidDetails()
+    {
+        EnterProvidersContactDetails(dataHelper.RandomEmail, dataHelper.TelephoneNumber, dataHelper.RandomWebsiteAddress);
+        Continue();
+        return new ConfirmProvidersContactDetailsPage(context);
+    }
 
-        private EditProvidersContactDetailsPage EnterProvidersContactDetails(string email, string telephone, string website)
-        {
-            formCompletionHelper.EnterText(EmailAddressTextBox, email);
-            formCompletionHelper.EnterText(TelephoneNumberTextBox, telephone);
-            formCompletionHelper.EnterText(WebsiteTextBox, website);
-            return this;
-        }
+    public WhichEmployersAreYouInterestedInPage BackToWhichEmployersAreYouInterestedInPage()
+    {
+        formCompletionHelper.Click(BackLink);
+        return new WhichEmployersAreYouInterestedInPage(context);
+    }
 
-        public EditProvidersContactDetailsPage DoNotEnterDetails() => StayOnEditProvidersContactDetailsPage();
-
-        public EditProvidersContactDetailsPage EnterInvalidDetails()
-        {
-            EnterProvidersContactDetails($"{dataHelper.RandomEmail}@gmail.com", $"{dataHelper.TelephoneNumber}ABC", dataHelper.RandomWebsiteAddress);
-            return StayOnEditProvidersContactDetailsPage();
-        }
-
-        public ConfirmProvidersContactDetailsPage EnterValidDetails()
-        {
-            EnterProvidersContactDetails(dataHelper.RandomEmail, dataHelper.TelephoneNumber, dataHelper.RandomWebsiteAddress);
-            Continue();
-            return new ConfirmProvidersContactDetailsPage(context);
-        }
-
-        public WhichEmployersAreYouInterestedInPage BackToWhichEmployersAreYouInterestedInPage()
-        {
-            formCompletionHelper.Click(BackLink);
-            return new WhichEmployersAreYouInterestedInPage(context);
-        }
-
-        private EditProvidersContactDetailsPage StayOnEditProvidersContactDetailsPage()
-        {
-            Continue();
-            return new EditProvidersContactDetailsPage(context);
-        }
+    private EditProvidersContactDetailsPage StayOnEditProvidersContactDetailsPage()
+    {
+        Continue();
+        return new EditProvidersContactDetailsPage(context);
     }
 }
