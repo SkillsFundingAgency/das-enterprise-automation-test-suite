34b77da13ee1a11da4fd5de050ab7ea94d454f18
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2022-06-01 10:50:34 +0100

    migrated AggregatedEmployerDemand project

--- src/SFA.DAS.AggregatedEmployerDemand.UITests/Project/Tests/Pages/EmployerPages/AEDIndexPage.cs
+++ src/SFA.DAS.AggregatedEmployerDemand.UITests/Project/Tests/Pages/EmployerPages/AEDIndexPage.cs
@@ -1,29 +1,27 @@
 ﻿using OpenQA.Selenium;
-using SFA.DAS.AggregatedEmployerDemand.UITests.Project.Tests.Pages.EmployerPages;
 using TechTalk.SpecFlow;
 
-namespace SFA.DAS.AggregatedEmployerDemand.UITests.Project.Tests.Pages
+namespace SFA.DAS.AggregatedEmployerDemand.UITests.Project.Tests.Pages.EmployerPages;
+
+public class AedIndexPage : AedBasePage
 {
-    public class AedIndexPage : AedBasePage
+    protected override string PageTitle => "";
+
+    public AedIndexPage(ScenarioContext context) : base(context) { }
+
+    #region Locators
+    private static By GetHelpWithFindingATrainingProviderLink => By.LinkText("Share your interest");
+    private static By ShareInterestButton => By.LinkText("Share interest");
+
+    #endregion
+
+    public ShareYourInterestWithTrainingProvidersPage ClickGetHelpWithFindingATrainingProviderLink() => GotoShareYourInterestWithTrainingProvidersPage(GetHelpWithFindingATrainingProviderLink);
+
+    public ShareYourInterestWithTrainingProvidersPage ClickShareInterestButton() => GotoShareYourInterestWithTrainingProvidersPage(ShareInterestButton);
+
+    private ShareYourInterestWithTrainingProvidersPage GotoShareYourInterestWithTrainingProvidersPage(By by)
     {
-        protected override string PageTitle => "";
-
-        public AedIndexPage(ScenarioContext context) : base(context) { }
-
-        #region Locators
-        private By GetHelpWithFindingATrainingProviderLink => By.LinkText("Share your interest");
-        private By ShareInterestButton => By.LinkText("Share interest");
-
-        #endregion
-
-        public ShareYourInterestWithTrainingProvidersPage ClickGetHelpWithFindingATrainingProviderLink() => GotoShareYourInterestWithTrainingProvidersPage(GetHelpWithFindingATrainingProviderLink);
-        
-        public ShareYourInterestWithTrainingProvidersPage ClickShareInterestButton() => GotoShareYourInterestWithTrainingProvidersPage(ShareInterestButton);
-        
-        private ShareYourInterestWithTrainingProvidersPage GotoShareYourInterestWithTrainingProvidersPage(By by)
-        {
-            formCompletionHelper.Click(by);
-            return new ShareYourInterestWithTrainingProvidersPage(context);
-        }
+        formCompletionHelper.Click(by);
+        return new ShareYourInterestWithTrainingProvidersPage(context);
     }
 }
