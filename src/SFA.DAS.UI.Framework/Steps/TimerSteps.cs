4f32328cd8628b1a2c820f4de6184d0d8ad871f2
Scott Wakefield <scott@expertcc.co.uk>
2022-06-10 18:01:40 +0100

    FAI-417 add shared step and use in employer feature.

---
+++ src/SFA.DAS.UI.Framework/Steps/TimerSteps.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.UI.Framework.Steps;
+
+[Binding]
+public sealed class TimerSteps
+{
+    private const int MilliSecondsPerSecond = 1000;
+    private readonly ScenarioContext _scenarioContext;
+
+    public TimerSteps(ScenarioContext scenarioContext)
+    {
+        _scenarioContext = scenarioContext;
+    }
+
+    [Given("the user waits for (.*) seconds")]
+    public void GivenAUserWaitsForSeconds(int waitInSeconds)
+    {
+        Thread.Sleep(waitInSeconds*MilliSecondsPerSecond);
+    }
+}
