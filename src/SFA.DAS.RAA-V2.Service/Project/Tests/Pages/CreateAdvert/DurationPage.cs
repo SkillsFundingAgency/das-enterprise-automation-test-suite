c1383a64e47c03e34df4ee9bd3ca55ed4a4e3739
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2022-06-29 20:23:10 +0100

    FAI-422 - Add conditional statements for page titles for traineeships

--- src/SFA.DAS.RAA-V2.Service/Project/Tests/Pages/CreateAdvert/DurationPage.cs
+++ src/SFA.DAS.RAA-V2.Service/Project/Tests/Pages/CreateAdvert/DurationPage.cs
@@ -5,7 +5,7 @@ namespace SFA.DAS.RAA_V2.Service.Project.Tests.Pages.CreateAdvert
 {
     public class DurationPage : Raav2BasePage
     {
-        protected override string PageTitle => "Duration and working hours";
+        protected override string PageTitle => IsTraineeship ? "Duration and weekly hours" : "Duration and working hours";
 
         private By Duration => By.Id("Duration");
 
