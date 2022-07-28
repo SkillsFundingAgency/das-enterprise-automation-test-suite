2d73d2f78b03d0ea67e9807ae23fca7ed3a724aa
Najam Muhammad <58732568+Najamuddin-Muhammad@users.noreply.github.com>
2022-07-28 15:48:56 +0100

    Supporttools/map 32 (#1204)
    * added new test as per MAP-32
    
    * aded missing steps
    
    * fixed support console tests
    
    * added status check
    
    * removed reference to approvals project
    
    * code reivew changes
    
    Co-authored-by: Arvinth Seran <arvinth.seran@digital.education.gov.uk>

---
+++ src/SFA.DAS.SupportConsole.UITests/Project/Tests/Pages/SuspendUsersPage.cs
@@ -0,0 +1,24 @@
+﻿
+namespace SFA.DAS.SupportConsole.UITests.Project.Tests.Pages
+{
+    public class SuspendUsersPage : ToolSupportBasePage
+    {
+        protected override string PageTitle => "Suspend users";
+
+        #region Locators
+        private static By SuspendUsersbtn => By.Id("okButton");
+        private static By StatusColumn => By.CssSelector("#usersResultsTable tr td:nth-child(3)");
+        #endregion
+
+        public SuspendUsersPage(ScenarioContext context) : base(context) { }
+
+        public SuspendUsersPage ClicSuspendUsersbtn()
+        {
+            formCompletionHelper.Click(SuspendUsersbtn);
+            return this;
+        }
+
+        public List<IWebElement> GetStatusColumn() => pageInteractionHelper.FindElements(StatusColumn);
+    }
+
+}
