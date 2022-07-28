45c5de7304a5245746986ce1cbeb24da389843f9
Sudhakar Chinoor <45876897+sudhakarchinoor@users.noreply.github.com>
2022-07-28 16:23:20 +0100

    Managing Standards Tests added (#1203)
    * Added Methods and pages.
    
    * Pages, Methods and Tests added
    
    * Database connection and Clear down script added.
    
    * Changed the Environment url to PP
    
    * Updtaed the Data helper
    
    * Merged Feature 1 and 2 and action review comments
    
    * updated Managing standards to .net core 6
    
    * pushed data to datahelpers
    
    * updated page title
    
    * updated url
    
    * Devops Page removed
    
    * Removed unused methods
    
    * Scenario Title updated
    
    * updated query string
    
    * updated sql query
    
    * fix for failing managing standards tests
    
    Co-authored-by: OlaniyiOshinusi <90407109+OlaniyiOshinusi@users.noreply.github.com>
    Co-authored-by: 44780 <44780@DESKTOP-5CF0C2V>
    Co-authored-by: Arvinth Seran <arvinth.seran@digital.education.gov.uk>

---
+++ src/SFA.DAS.ManagingStandards.UITests/Project/Tests/Pages/EditCourseContactDetailsPage.cs
@@ -0,0 +1,26 @@
+﻿namespace SFA.DAS.ManagingStandards.UITests.Project.Tests.Pages;
+
+public class YourContactInformationForThisStandardPage : ManagingStandardsBasePage
+{
+    protected override string PageTitle => "Your contact information for this standard";
+
+    private static By EmailAddressTextBox => By.Id("ContactUsEmail");
+    private static By TelephoneNumberTextBox => By.Id("ContactUsPhoneNumber");
+    private static By ContactPageTextBox => By.Id("ContactUsPageUrl");
+    private static By YourWebsitePageTextBox => By.Id("StandardInfoUrl");
+    private static By SaveAndContinueButton => By.Id("submit");
+
+    public YourContactInformationForThisStandardPage(ScenarioContext context) : base(context)
+    {
+    }
+
+    public ManageAStandard_TeacherPage UpdateContactInformation()
+    {
+        formCompletionHelper.EnterText(EmailAddressTextBox, managingStandardsDataHelpers.EmailAddress);
+        formCompletionHelper.EnterText(TelephoneNumberTextBox, managingStandardsDataHelpers.ContactNumber);
+        formCompletionHelper.EnterText(ContactPageTextBox, managingStandardsDataHelpers.ContactWebsite);
+        formCompletionHelper.EnterText(YourWebsitePageTextBox, managingStandardsDataHelpers.Website);
+        formCompletionHelper.Click(SaveAndContinueButton);
+        return new ManageAStandard_TeacherPage(context);
+    }
+}
---
+++ src/SFA.DAS.ManagingStandards.UITests/Project/Tests/Pages/ManageAStandard_TeacherPage.cs
@@ -0,0 +1,31 @@
+﻿namespace SFA.DAS.ManagingStandards.UITests.Project.Tests.Pages;
+
+public class ManageAStandard_TeacherPage : ManagingStandardsBasePage
+{
+    protected override string PageTitle => managingStandardsDataHelpers.StandardsTestData.StandardName;
+
+    public ManageAStandard_TeacherPage(ScenarioContext context) : base(context) { }
+
+    public RegulatedStandardPage AccessApprovedByRegulationOrNot()
+    {
+        formCompletionHelper.ClickLinkByText("Change");
+        return new RegulatedStandardPage(context);
+    }
+
+    public WhereWillThisStandardBeDeliveredPage AccessWhereYouWillDeliverThisStandard()
+    {
+        formCompletionHelper.ClickLinkByText("where you deliver this standard");
+        return new WhereWillThisStandardBeDeliveredPage(context);
+    }
+
+    public WhereCanYouDeliverTrainingPage AccessEditTheseRegions()
+    {
+        formCompletionHelper.ClickLinkByText("Edit these regions");
+        return new WhereCanYouDeliverTrainingPage(context);
+    }
+    public YourContactInformationForThisStandardPage UpdateTheseContactDetails()
+    {
+        formCompletionHelper.ClickLinkByText("Update these contact details");
+        return new YourContactInformationForThisStandardPage(context);
+    }
+}
