599b0f558103c4b4042113f1478efa8b00a66859
DESKTOP-UQKM3U7\Sree <sreekanth.badigenchula@digital.education.gov.uk>
2022-08-01 00:30:40 +0100

    RPL Bulkupload change

--- src/SFA.DAS.Approvals.UITests/Project/Tests/StepDefinitions/ProviderSteps.cs
+++ src/SFA.DAS.Approvals.UITests/Project/Tests/StepDefinitions/ProviderSteps.cs
@@ -140,6 +140,9 @@ namespace SFA.DAS.Approvals.UITests.Project.Tests.StepDefinitions
             providerApproveApprenticeDetailsPage.SelectAddAnApprentice().ProviderSelectsAStandard().SubmitValidApprenticeDetails().SubmitApprove();
         }
 
+        [When(@"approves the cohort from the saved draft section")]
+        public void WhenApprovesTheCohortFromTheSavedDraftSection() => _providerStepsHelper.EditApprentice().SubmitApprove();
+
         private int? GetProvidersDraftAndReadyForReviewCohortsCount() => _commitmentsSqlDataHelper.GetProvidersDraftAndReadyForReviewCohortsCount(_providerConfig.Ukprn);
     }
 }
