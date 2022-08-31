e1243cf8b694757077ddf3a522383d81b76abd4b
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2022-08-31 15:58:02 +0100

    updated employement check test data

--- src/SFA.DAS.EmplomentChecks.APITests/Project/SetupScenarioTestData.cs
+++ src/SFA.DAS.EmplomentChecks.APITests/Project/SetupScenarioTestData.cs
@@ -47,7 +47,7 @@ namespace SFA.DAS.EmploymentChecks.APITests.Project
 
                 { new Tuple<int, string>(9, "TEST"), new TestData() { ULN = 9000001500, AccountId = 28284, NationalInsuranceNumber = "AA890123C", PayeScheme = "001/SB034,001/SB035" } },
                 { new Tuple<int, string>(9, "TEST2"), new TestData() { ULN = 9000001500, AccountId = 2680, NationalInsuranceNumber = "AA890123C", PayeScheme = "100/GDS00010,100/GDS00011" } },
-                { new Tuple<int, string>(9, "PP"), new TestData() { ULN = 2771115661, AccountId = 206775, NationalInsuranceNumber = "LJ000000A", PayeScheme = "101/LE13812,102/LE76077" } },
+                { new Tuple<int, string>(9, "PP"), new TestData() { ULN = 2771115661, AccountId = 12926, NationalInsuranceNumber = "LJ000000A", PayeScheme = "100/GDS00001,001/AC09013" } },
 
                 { new Tuple<int, string>(10, "TEST"), new TestData() { AccountId = 8369 } },
                 { new Tuple<int, string>(10, "TEST2"), new TestData() { AccountId = 1124 } },
