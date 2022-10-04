53546559f42264fffa6a31a7cd2de2eebe387b9c
DESKTOP-UQKM3U7\Sree <sreekanth.badigenchula@digital.education.gov.uk>
2022-10-04 13:59:24 +0100

    loctors change

--- src/SFA.DAS.ApprenticeCommitments.UITests/Project/Tests/Page/ApprenticeHomePage.cs
+++ src/SFA.DAS.ApprenticeCommitments.UITests/Project/Tests/Page/ApprenticeHomePage.cs
@@ -15,11 +15,10 @@ namespace SFA.DAS.ApprenticeCommitments.UITests.Project.Tests.Page
         private static By CmadDashboardLinkForConfimredApprenticeshipDetails => By.XPath($"//ul[@class='dashboard-nav dashboard-li']/li/h2/a[contains(text(),'{HomePageHelper.ConfirmedCmadSectionTitle}')]");
         private static By ConfimredApprenticeshipDetailsSubText => By.XPath($"//ul[@class='dashboard-nav dashboard-li']/li/h2/a[contains(text(),'{HomePageHelper.ConfirmedCmadSectionTitle}')]/..//following-sibling::p");
         private static By HelpAndSupportDashboardLink => By.XPath("//ul[@class='dashboard-nav dashboard-li']/li/h2/a[contains(text(),'Help and support')]");
-        private static By CmadDashboardLinkAfterFullyConfirmed => By.XPath("//a[contains(text(),'My apprenticeship details')]");
+        private static By CmadDashboardLinkAfterFullyConfirmed => By.XPath("//ul[@class='dashboard-nav dashboard-li']/li/h2/a[contains(text(),'My apprenticeship details')]");
         private static By CmadDashboardText => By.XPath("(//ul[@class='dashboard-nav dashboard-li']/li/h2/following-sibling::p)[1]");
         private static By HelpAndSupportDashboardText => By.XPath("(//ul[@class='dashboard-nav dashboard-li']/li/h2/following-sibling::p)[2]");
         private static By CurrentApprenticeshipStatusSelector => By.CssSelector("#dashboard-section strong.govuk-tag--yellow");
-        private static By MyApprenticeshipDetailsLink => By.XPath("//a[contains(text(),'My apprenticeship details')]");
 
         public ApprenticeHomePage(ScenarioContext context, bool verifyConfirmMyApprenticeLink = true) : base(context)
         {
@@ -50,7 +49,7 @@ namespace SFA.DAS.ApprenticeCommitments.UITests.Project.Tests.Page
 
         public FullyConfirmedOverviewPage NavigateToFullyConfirmedOverviewPageWithMyApprenticeshipDetailsLinkOnTheHomePage()
         {
-            formCompletionHelper.Click(MyApprenticeshipDetailsLink);
+            formCompletionHelper.Click(CmadDashboardLinkAfterFullyConfirmed);
             return new FullyConfirmedOverviewPage(context);
         }
 
