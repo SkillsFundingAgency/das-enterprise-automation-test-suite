97060792903885f45a978825a59b44a8816fb7ba
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2022-10-26 16:37:03 +0100

    minor fix to the sql query

--- src/SFA.DAS.Approvals.UITests/Project/Helpers/SqlHelpers/RoatpV2SqlDataHelper.cs
+++ src/SFA.DAS.Approvals.UITests/Project/Helpers/SqlHelpers/RoatpV2SqlDataHelper.cs
@@ -9,5 +9,5 @@ public class RoatpV2SqlDataHelper : SqlDbHelper
     private readonly string _ukprn;
     public RoatpV2SqlDataHelper(DbConfig dbConfig, string ukprn) : base(dbConfig.ManagingStandardsDbConnectionString) => _ukprn = ukprn;
 
-    internal List<string> GetPortableFlexiJobLarsCode() => GetData($"select top 1 pc.LarsCode from ProviderCourse pc Join [Provider] p on pc.ProviderId = p.id where HasPortableFlexiJobOption = 1 and ukprn = {_ukprn} order by NEWID();");
+    internal List<string> GetPortableFlexiJobLarsCode() => GetData($"select pc.LarsCode from ProviderCourse pc Join [Provider] p on pc.ProviderId = p.id where HasPortableFlexiJobOption = 1 and ukprn = {_ukprn} order by NEWID();");
 }
