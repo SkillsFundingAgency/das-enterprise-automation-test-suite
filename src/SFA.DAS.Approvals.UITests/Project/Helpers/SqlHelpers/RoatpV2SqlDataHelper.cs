d32a77c164680e951a069a1682d418c9df960a93
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2022-10-27 14:08:25 +0100

    potential fix for apprentice commitments

--- src/SFA.DAS.Approvals.UITests/Project/Helpers/SqlHelpers/RoatpV2SqlDataHelper.cs
+++ src/SFA.DAS.Approvals.UITests/Project/Helpers/SqlHelpers/RoatpV2SqlDataHelper.cs
@@ -7,7 +7,8 @@ namespace SFA.DAS.Approvals.UITests.Project.Helpers.SqlHelpers;
 public class RoatpV2SqlDataHelper : SqlDbHelper
 {
     private readonly string _ukprn;
+
     public RoatpV2SqlDataHelper(DbConfig dbConfig, string ukprn) : base(dbConfig.ManagingStandardsDbConnectionString) => _ukprn = ukprn;
 
-    internal List<string> GetPortableFlexiJobLarsCode() => GetData($"select pc.LarsCode from ProviderCourse pc Join [Provider] p on pc.ProviderId = p.id where HasPortableFlexiJobOption = 1 and ukprn = '{_ukprn}' order by NEWID();");
+    internal List<string> GetPortableFlexiJobLarsCode() => string.IsNullOrEmpty(_ukprn) ? new List<string>() {""} :  GetData($"select pc.LarsCode from ProviderCourse pc Join [Provider] p on pc.ProviderId = p.id where HasPortableFlexiJobOption = 1 and ukprn = '{_ukprn}' order by NEWID();");
 }
