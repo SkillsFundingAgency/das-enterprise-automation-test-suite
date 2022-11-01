6b5ce844654a00f0778f14cde291d87051dcd26b
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2022-11-01 10:17:30 +0000

    updated chrome driver

--- src/SFA.DAS.TransferMatching.UITests/Project/Tests/Features/TM_11_TransferUsersWithFunds.feature.cs
+++ src/SFA.DAS.TransferMatching.UITests/Project/Tests/Features/TM_11_TransferUsersWithFunds.feature.cs
@@ -35,7 +35,7 @@ namespace SFA.DAS.TransferMatching.UITests.Project.Tests.Features
         public virtual void FeatureSetup()
         {
             testRunner = TechTalk.SpecFlow.TestRunnerManager.GetTestRunner();
-            TechTalk.SpecFlow.FeatureInfo featureInfo = new TechTalk.SpecFlow.FeatureInfo(new System.Globalization.CultureInfo("en-GB"), "Project/Tests/Features", "TM_11_TransferUsersWithFunds", "//The scenario is not valid anymore", ProgrammingLanguage.CSharp, featureTags);
+            TechTalk.SpecFlow.FeatureInfo featureInfo = new TechTalk.SpecFlow.FeatureInfo(new System.Globalization.CultureInfo("en-GB"), "Project/Tests/Features", "TM_11_TransferUsersWithFunds", null, ProgrammingLanguage.CSharp, featureTags);
             testRunner.OnFeatureStart(featureInfo);
         }
         
@@ -74,16 +74,18 @@ namespace SFA.DAS.TransferMatching.UITests.Project.Tests.Features
         }
         
         [NUnit.Framework.TestAttribute()]
-        [NUnit.Framework.DescriptionAttribute("TM_11_Do not display the ‘Apply for transfers funding’ section to levy-paying emp" +
-            "loyers that are currently sending transfer funds")]
+        [NUnit.Framework.DescriptionAttribute("TM_11_Display the ‘Apply for transfers funding’ section to levy-paying employers " +
+            "that are currently sending transfer funds")]
+        [NUnit.Framework.CategoryAttribute("regression")]
         [NUnit.Framework.CategoryAttribute("transfermatching")]
-        public void TM_11_DoNotDisplayTheApplyForTransfersFundingSectionToLevy_PayingEmployersThatAreCurrentlySendingTransferFunds()
+        public void TM_11_DisplayTheApplyForTransfersFundingSectionToLevy_PayingEmployersThatAreCurrentlySendingTransferFunds()
         {
             string[] tagsOfScenario = new string[] {
+                    "regression",
                     "transfermatching"};
             System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
-            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("TM_11_Do not display the ‘Apply for transfers funding’ section to levy-paying emp" +
-                    "loyers that are currently sending transfer funds", null, tagsOfScenario, argumentsOfScenario, featureTags);
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("TM_11_Display the ‘Apply for transfers funding’ section to levy-paying employers " +
+                    "that are currently sending transfer funds", null, tagsOfScenario, argumentsOfScenario, featureTags);
 #line 4
 this.ScenarioInitialize(scenarioInfo);
 #line hidden
