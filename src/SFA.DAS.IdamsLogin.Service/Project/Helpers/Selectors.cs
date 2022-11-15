ac066d8e336cefdcb7509da3a03cb4fd7e767002
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2022-11-15 14:12:08 +0000

    minor changes to aviod duplication

---
+++ src/SFA.DAS.IdamsLogin.Service/Project/Helpers/Selectors.cs
@@ -0,0 +1,8 @@
+﻿using OpenQA.Selenium;
+
+namespace SFA.DAS.IdamsLogin.Service.Project.Helpers;
+
+public static class Selectors
+{
+    public static By PireanPreprod => By.XPath("//span[contains(text(),'Pirean Preprod')]");
+}
--- src/SFA.DAS.IdamsLogin.Service/Project/Tests/Pages/SignInBasePage.cs
+++ src/SFA.DAS.IdamsLogin.Service/Project/Tests/Pages/SignInBasePage.cs
@@ -1,29 +1,28 @@
 ﻿using OpenQA.Selenium;
 using TechTalk.SpecFlow;
 
-namespace SFA.DAS.IdamsLogin.Service.Project.Tests.Pages
+namespace SFA.DAS.IdamsLogin.Service.Project.Tests.Pages;
+
+public abstract class SignInBasePage : IdamsLoginBasePage
 {
-    public abstract class SignInBasePage : IdamsLoginBasePage
+    protected override By PageHeader => By.CssSelector(".pageTitle");
+    
+    protected override bool TakeFullScreenShot => false;
+    
+    protected override string PageTitle => "Sign in";
+
+    #region Locators
+    protected By UsernameField => By.Id("username");
+    protected By PasswordField => By.Id("password");
+    protected By SignInButton => By.XPath("//button[@value='Log in']");
+    #endregion
+
+    protected SignInBasePage(ScenarioContext context) : base(context) { }
+
+    public void SubmitValidLoginDetails(string username, string password)
     {
-        protected override By PageHeader => By.CssSelector(".pageTitle");
-        
-        protected override bool TakeFullScreenShot => false;
-        
-        protected override string PageTitle => "Sign in";
-
-        #region Locators
-        protected By UsernameField => By.Id("username");
-        protected By PasswordField => By.Id("password");
-        protected By SignInButton => By.XPath("//button[@value='Log in']");
-        #endregion
-
-        protected SignInBasePage(ScenarioContext context) : base(context) { }
-
-        public void SubmitValidLoginDetails(string username, string password)
-        {
-            formCompletionHelper.EnterText(UsernameField, username);
-            formCompletionHelper.EnterText(PasswordField, password);
-            formCompletionHelper.ClickElement(SignInButton);
-        }
+        formCompletionHelper.EnterText(UsernameField, username);
+        formCompletionHelper.EnterText(PasswordField, password);
+        formCompletionHelper.ClickElement(SignInButton);
     }
 }
