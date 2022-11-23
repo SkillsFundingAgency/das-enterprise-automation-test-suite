730fa28738584f1ff2d95990affa6f89d9299ef6
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2022-11-23 15:05:57 +0000

    Approvals/map 152 (#1283) (#1285)
    * checking initial changes
    
    * test added for OLTD
    
    * OLTD
    
    * adding the second test
    
    * OLTD Journey
    
    * OLTD Changes
    
    * test changes
    
    * refactor
    
    * further fixes
    
    * renamed methods
    
    * OLTD TEST
    
    * OLTD
    
    * test environment reverted back to PP
    
    * updates on back of code review comments
    
    * Update OverlappingTrainingDateRequestSteps.cs
    
    * fixed merge conflicts
    
    * updated tests
    
    * removed lines from csproj
    
    * code review changes
    
    * renamed tests
    
    * update csproj
    
    * updated css selector
    
    * code review changes
    
    * updated css selector
    
    Co-authored-by: Shahzad <shahzad.khan@hkf-technologies.co.uk>
    Co-authored-by: kola <samuel.dev.dba@gmail.com>
    Co-authored-by: Kola samuel <kola.samuel@digital.education.gov.uk>
    Co-authored-by: Arvinth Seran <arvinth.seran@digital.education.gov.uk>
    
    Co-authored-by: Najam Muhammad <58732568+Najamuddin-Muhammad@users.noreply.github.com>
    Co-authored-by: Shahzad <shahzad.khan@hkf-technologies.co.uk>
    Co-authored-by: kola <samuel.dev.dba@gmail.com>
    Co-authored-by: Kola samuel <kola.samuel@digital.education.gov.uk>

---
+++ src/SFA.DAS.Approvals.UITests/Project/Helpers/DataHelpers/OltdDataHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Approvals.UITests.Project.Helpers.DataHelpers
+{
+    public class OltdApprenticeDetails
+    {
+        public int NewStartDate { get; set; }
+        public int NewEndDate { get; set; }
+        public bool DisplayOverlapErrorOnStartDate { get; set; }
+        public bool DisplayOverlapErrorOnEndDate { get; set; }
+    }
+}
