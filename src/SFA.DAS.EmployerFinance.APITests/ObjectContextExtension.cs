34dc058ad0e312f3dd4dd617507da20d0b3cf346
Vasantha Kasirajan <56582101+VasanthaKasirajan3008@users.noreply.github.com>
2022-11-25 10:45:59 +0000

    Mac 100 employer finance api (#1262)
    * MAC-100
    
    * MAC-100-Added test to Employer Finance Inner API  endpoints
    
    * MAC - 100 - resolved merge conflicts
    
    * MAC-100
    
    * Delete SFA - Backup.DAS.EmployerFinance.APITests.csproj
    
    * Approvals/map 152 (#1283) (#1285)
    
    * checking initial changes
    
    * test added for OLTD
    
    * OLTD
    
    * adding the second test
    
    * OLTD Journey
    
    * OLTD Changes
    
    * test changes
    
    * refactor
    
    * further fixes
    
    * renamed methods
    
    * OLTD TEST
    
    * OLTD
    
    * test environment reverted back to PP
    
    * updates on back of code review comments
    
    * Update OverlappingTrainingDateRequestSteps.cs
    
    * fixed merge conflicts
    
    * updated tests
    
    * removed lines from csproj
    
    * code review changes
    
    * renamed tests
    
    * update csproj
    
    * updated css selector
    
    * code review changes
    
    * updated css selector
    
    Co-authored-by: Shahzad <shahzad.khan@hkf-technologies.co.uk>
    Co-authored-by: kola <samuel.dev.dba@gmail.com>
    Co-authored-by: Kola samuel <kola.samuel@digital.education.gov.uk>
    Co-authored-by: Arvinth Seran <arvinth.seran@digital.education.gov.uk>
    
    Co-authored-by: Najam Muhammad <58732568+Najamuddin-Muhammad@users.noreply.github.com>
    Co-authored-by: Shahzad <shahzad.khan@hkf-technologies.co.uk>
    Co-authored-by: kola <samuel.dev.dba@gmail.com>
    Co-authored-by: Kola samuel <kola.samuel@digital.education.gov.uk>
    
    * Approvals/map 152 (#1283) (#1286)
    
    * checking initial changes
    
    * test added for OLTD
    
    * OLTD
    
    * adding the second test
    
    * OLTD Journey
    
    * OLTD Changes
    
    * test changes
    
    * refactor
    
    * further fixes
    
    * renamed methods
    
    * OLTD TEST
    
    * OLTD
    
    * test environment reverted back to PP
    
    * updates on back of code review comments
    
    * Update OverlappingTrainingDateRequestSteps.cs
    
    * fixed merge conflicts
    
    * updated tests
    
    * removed lines from csproj
    
    * code review changes
    
    * renamed tests
    
    * update csproj
    
    * updated css selector
    
    * code review changes
    
    * updated css selector
    
    Co-authored-by: Shahzad <shahzad.khan@hkf-technologies.co.uk>
    Co-authored-by: kola <samuel.dev.dba@gmail.com>
    Co-authored-by: Kola samuel <kola.samuel@digital.education.gov.uk>
    Co-authored-by: Arvinth Seran <arvinth.seran@digital.education.gov.uk>
    
    Co-authored-by: Najam Muhammad <58732568+Najamuddin-Muhammad@users.noreply.github.com>
    Co-authored-by: Shahzad <shahzad.khan@hkf-technologies.co.uk>
    Co-authored-by: kola <samuel.dev.dba@gmail.com>
    Co-authored-by: Kola samuel <kola.samuel@digital.education.gov.uk>
    
    Co-authored-by: Raj Balguri <37080619+reachash@users.noreply.github.com>
    Co-authored-by: Raj <reachash1@hotmail.com>
    Co-authored-by: Arvinth Seran <arvinth.seran@digital.education.gov.uk>
    Co-authored-by: Najam Muhammad <58732568+Najamuddin-Muhammad@users.noreply.github.com>
    Co-authored-by: Shahzad <shahzad.khan@hkf-technologies.co.uk>
    Co-authored-by: kola <samuel.dev.dba@gmail.com>
    Co-authored-by: Kola samuel <kola.samuel@digital.education.gov.uk>

---
+++ src/SFA.DAS.EmployerFinance.APITests/ObjectContextExtension.cs
@@ -0,0 +1,16 @@
+﻿using SFA.DAS.ConfigurationBuilder;
+
+namespace SFA.DAS.EmployerFinance.APITests
+{
+    public static class ObjectContextExtension
+    {
+        private const string AccountIdKey = "accountid";
+        private const string HashedAccountIdKey = "hashedaccountid";
+
+        internal static void SetAccountId(this ObjectContext objectContext, string value) => objectContext.Replace(AccountIdKey, value);
+        internal static void SetHashedAccountId(this ObjectContext objectContext, string value) => objectContext.Replace(HashedAccountIdKey, value);
+
+        internal static string GetAccountId(this ObjectContext objectContext) => objectContext.Get(AccountIdKey);
+        internal static string GetHashedAccountId(this ObjectContext objectContext) => objectContext.Get(HashedAccountIdKey);
+    }
+}
