7de00c8ccad2ca76ba09f82b0db48b2d0cbc597b
Najam Muhammad <najamuddin.muhammad@digital.education.gov.uk>
2023-01-03 18:53:19 +0000

    dummy

--- src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Employer/SelectDeliveryModelPage.cs
+++ src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Employer/SelectDeliveryModelPage.cs
@@ -75,7 +75,7 @@ namespace SFA.DAS.Approvals.UITests.Project.Tests.Pages.Employer
 
         public ProviderEditApprenticeDetailsPage ProviderEditsDeliveryModelToRegularAndSubmits()
         {
-            formCompletionHelper.Click(EditRegularRadioButton);
+            formCompletionHelper.Click(EditRegularRadioButton); 
             Continue();
             return new ProviderEditApprenticeDetailsPage(context);
         }
