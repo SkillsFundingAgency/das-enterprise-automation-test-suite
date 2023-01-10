a85f25e1818d6f495f0b4e88e3072229ce634fd9
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2023-01-10 17:08:31 +0000

    minor changes

--- src/SFA.DAS.EmployerFinance.APITests/Project/Helpers/SqlDbHelpers/EmployerFinanceSqlHelper.cs
+++ src/SFA.DAS.EmployerFinance.APITests/Project/Helpers/SqlDbHelpers/EmployerFinanceSqlHelper.cs
@@ -1,27 +1,8 @@
 ﻿using SFA.DAS.ConfigurationBuilder;
 using SFA.DAS.FrameworkHelpers;
-using TechTalk.SpecFlow;
 
 namespace SFA.DAS.EmployerFinance.APITests.Project.Helpers.SqlDbHelpers
 {
-    public class EmployerAccountsSqlHelper : SqlDbHelper
-    {
-        private readonly ObjectContext _objectContext;
-
-        public EmployerAccountsSqlHelper(DbConfig dbConfig, ObjectContext objectContext) : base(dbConfig.AccountsDbConnectionString)
-        {
-
-        }
-
-        public string SetHashedAccountId(string accountId)
-        {
-            var id = GetDataAsString($"SELECT HashedId FROM [employer_account].[Account] WHERE id = '{accountId}' ");
-            _objectContext.SetHashedAccountId(id);
-            return id;
-        }
-
-
-    }
 
     public class EmployerFinanceSqlHelper : SqlDbHelper
     {
