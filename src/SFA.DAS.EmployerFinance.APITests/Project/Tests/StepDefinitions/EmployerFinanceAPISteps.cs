a8fdea57c5ba455a700bb42217e750e5e854d112
Raj Balguri <37080619+reachash@users.noreply.github.com>
2023-01-11 10:10:49 +0000

    Fix for finance accounts api tests (#1306)
    * Using same account for each test
    
    * Allways use account with PAYE scheme for Employer Accounts API tests
    
    * code review changes
    
    * replaced encoding package
    
    * minor changes
    
    * updated csproj file
    
    Co-authored-by: Chris Woodcock <chris@humesoftware.com>
    Co-authored-by: Arvinth Seran <arvinth.seran@digital.education.gov.uk>
    Co-authored-by: Gabriel PT <98819464+GPeterThomas@users.noreply.github.com>

--- src/SFA.DAS.EmployerFinance.APITests/Project/Tests/StepDefinitions/EmployerFinanceAPISteps.cs
+++ src/SFA.DAS.EmployerFinance.APITests/Project/Tests/StepDefinitions/EmployerFinanceAPISteps.cs
@@ -16,6 +16,7 @@ namespace SFA.DAS.EmployerFinance.APITests.Project.Tests.StepDefinitions
         private readonly Inner_EmployerFinanceApiRestClient _innerApiRestClient;
         private readonly Outer_EmployerFinanceApiHelper _employerFinanceOuterApiHelper;
         private readonly EmployerFinanceSqlHelper _employerFinanceSqlDbHelper;
+        private readonly EmployerAccountsSqlHelper _employerAccountsSqlDbHelper;
         private readonly ObjectContext _objectContext;
 
         public EmployerFinanceAPISteps(ScenarioContext context)
@@ -23,9 +24,10 @@ namespace SFA.DAS.EmployerFinance.APITests.Project.Tests.StepDefinitions
             _innerApiRestClient = context.GetRestClient<Inner_EmployerFinanceApiRestClient>();
             _employerFinanceOuterApiHelper = new Outer_EmployerFinanceApiHelper(context);
             _employerFinanceSqlDbHelper = context.Get<EmployerFinanceSqlHelper>();
+            _employerAccountsSqlDbHelper = context.Get<EmployerAccountsSqlHelper>();
             _objectContext = context.Get<ObjectContext>();
-            _employerFinanceSqlDbHelper.SetHashedAccountId();
-            _employerFinanceSqlDbHelper.SetAccountId();
+            var accountid = _employerFinanceSqlDbHelper.SetAccountId();
+            _employerAccountsSqlDbHelper.SetHashedAccountId(accountid);
             _employerFinanceSqlDbHelper.SetEmpRef();
         }
 
@@ -35,21 +37,21 @@ namespace SFA.DAS.EmployerFinance.APITests.Project.Tests.StepDefinitions
         [Then(@"endpoint /Accounts/\{accountId}/minimum-signed-agreement-version can be accessed")]
         public void ThenEndpointAccountsAccountIdMinimum_Signed_Agreement_VersionCanBeAccessed()
         {
-            var accountId = _objectContext.GetAccountId();
+            var accountId = GetAccountId();
             _employerFinanceOuterApiHelper.GetAccountMinimumSignedAgreementVersion(long.Parse(accountId));
         }
 
         [Then(@"endpoint /Accounts/\{accountId}/users/which-receive-notifications can be accessed")]
         public void ThenEndpointAccountsAccountIdUsersWhich_Receive_NotificationsCanBeAccessed()
         {
-            var accountId = _objectContext.GetAccountId();
+            var accountId = GetAccountId();
             _employerFinanceOuterApiHelper.GetAccountUserWhichCanReceiveNotifications(long.Parse(accountId));
         }
 
         [Then(@"endpoint /Pledges\?accountId=\{accountId} can be accessed")]
         public void ThenEndpointPledgesAccountIdAccountIdCanBeAccessed()
         {
-            var accountId = _objectContext.GetAccountId();
+            var accountId = GetAccountId();
             _employerFinanceOuterApiHelper.GetPledges(long.Parse(accountId));
         }
 
@@ -57,7 +59,7 @@ namespace SFA.DAS.EmployerFinance.APITests.Project.Tests.StepDefinitions
         [Then(@"endpoint /Projections/\{accountId} can be accessed")]
         public void ThenEndpointProjectionsAccountIdCanBeAccessed()
         {
-            var accountId = _objectContext.GetAccountId();
+            var accountId = GetAccountId();
             _employerFinanceOuterApiHelper.GetProjections(long.Parse(accountId));
         }
 
@@ -88,14 +90,14 @@ namespace SFA.DAS.EmployerFinance.APITests.Project.Tests.StepDefinitions
         [Then(@"endpoint /Transfers/\{accountId}/counts can be accessed")]
         public void ThenEndpointTransfersAccountIdCountsCanBeAccessed()
         {
-            var accountId = _objectContext.GetAccountId();
+            var accountId = GetAccountId();
             _employerFinanceOuterApiHelper.GetTransfersCounts(long.Parse(accountId));
         }
 
         [Then(@"endpoint /Transfers/\{accountId}/financial-breakdown can be accessed")]
         public void ThenEndpointTransfersAccountIdFinancial_BreakdownCanBeAccessed()
         {
-            var accountId = _objectContext.GetAccountId();
+            var accountId = GetAccountId();
             _employerFinanceOuterApiHelper.GetTransfersFinancialBreakdown(long.Parse(accountId));
         }
 
@@ -108,14 +110,14 @@ namespace SFA.DAS.EmployerFinance.APITests.Project.Tests.StepDefinitions
         [Then(@"endpoint api/accounts/\{hashedAccountId}/levy can be accessed")]
         public void ThenEndpointApiAccountsHashedAccountIdLevyEndpointCanBeAccessed()
         {
-            var hashedAccountId = _objectContext.GetHashedAccountId();
+            var hashedAccountId = GetHashedAccountId();
             _innerApiRestClient.ExecuteEndpoint($"/api/accounts/{hashedAccountId}/levy", HttpStatusCode.OK);
         }
 
         [Then(@"endpoint api/accounts/\{hashedAccountId}/levy/GetLevyForPeriod can be accessed")]
         public void ThenEndpointApiAccountsHashedAccountIdLevyGetLevyForPeriodEndpointCanBeAccessed()
         {
-            var hashedAccountId = _objectContext.GetHashedAccountId();
+            var hashedAccountId = GetHashedAccountId();
             var response = _innerApiRestClient.ExecuteEndpoint($"/api/accounts/{hashedAccountId}/levy");
             var result = JsonConvert.DeserializeObject<ICollection<LevyDeclaration>>(response.Content);
             _innerApiRestClient.ExecuteEndpoint($"/api/accounts/{hashedAccountId}/levy/{result.FirstOrDefault().PayrollYear}/{result.FirstOrDefault().PayrollMonth}", HttpStatusCode.OK);
@@ -125,14 +127,14 @@ namespace SFA.DAS.EmployerFinance.APITests.Project.Tests.StepDefinitions
         [Then(@"endpoint api/accounts/\{accountId}/transactions can be accessed")]
         public void ThenEndpointApiAccountsAccountIdTransactionsEndpointCanBeAccessed()
         {
-            var hashedAccountId = _objectContext.GetHashedAccountId();
+            var hashedAccountId = GetHashedAccountId();
             _innerApiRestClient.ExecuteEndpoint($"/api/accounts/{hashedAccountId}/transactions", HttpStatusCode.OK);
         }
 
         [Then(@"endpoint api/accounts/\{hashedAccountId}/transactions/GetTransactions can be accessed")]
         public void ThenEndpointApiAccountsHashedAccountIdTransactionsGetTransactionsEndpointCanBeAccessed()
         {
-            var hashedAccountId = _objectContext.GetHashedAccountId();
+            var hashedAccountId = GetHashedAccountId();
             var response = _innerApiRestClient.ExecuteEndpoint($"/api/accounts/{hashedAccountId}/transactions");
             var result = JsonConvert.DeserializeObject<ICollection<TransactionSummary>>(response.Content);
             _innerApiRestClient.ExecuteEndpoint($"/api/accounts/{hashedAccountId}/transactions/{result.FirstOrDefault().Year}/{result.FirstOrDefault().Month}", HttpStatusCode.OK);
@@ -147,21 +149,21 @@ namespace SFA.DAS.EmployerFinance.APITests.Project.Tests.StepDefinitions
         [Then(@"endpoint api/accounts/\{hashedAccountId}/transferAllowance can be accessed")]
         public void ThenEndpointApiAccountsHashedAccountIdTransferAllowanceCanBeAccessed()
         {
-            var hashedAccountId = _objectContext.GetHashedAccountId();
+            var hashedAccountId = GetHashedAccountId();
             _innerApiRestClient.ExecuteEndpoint($"/api/accounts/{hashedAccountId}/transferAllowance", HttpStatusCode.OK);
         }
 
         [Then(@"endpoint api/accounts/\{hashedAccountId}/levy/english-fraction-current can be accessed")]
         public void ThenEndpointApiAccountsHashedAccountIdLevyEnglishFractionCurrentCanBeAccessed()
         {
-            var hashedAccountId = _objectContext.GetHashedAccountId();
+            var hashedAccountId = GetHashedAccountId();
             _innerApiRestClient.ExecuteEndpoint($"/api/accounts/{hashedAccountId}/levy/english-fraction-current", HttpStatusCode.OK);
         }
 
         [Then(@"endpoint api/accounts/\{hashedAccountId}/levy/english-fraction-history can be accessed")]
         public void ThenEndpointApiAccountsHashedAccountIdLevyEnglishFractionHistoryCanBeAccessed()
         {
-            var hashedAccountId = _objectContext.GetHashedAccountId();
+            var hashedAccountId = GetHashedAccountId();
             var empRef = _objectContext.GetEmpRef();
             _innerApiRestClient.ExecuteEndpoint($"/api/accounts/{hashedAccountId}/levy/english-fraction-history?empRef={empRef}", HttpStatusCode.OK);
         }
@@ -169,15 +171,19 @@ namespace SFA.DAS.EmployerFinance.APITests.Project.Tests.StepDefinitions
         [Then(@"endpoint /api/accounts/\{hashedAccountId}/transfers/connections can be accessed")]
         public void ThenEndpointApiAccountsHashedAccountIdTransfersConnectionsCanBeAccessed()
         {
-            var hashedAccountId = _objectContext.GetHashedAccountId();
+            var hashedAccountId = GetHashedAccountId();
             _innerApiRestClient.ExecuteEndpoint($"/api/accounts/{hashedAccountId}/transfers/connections", HttpStatusCode.OK);
         }
 
         [Then(@"endpoint /api/accounts/internal/\{accountId}/transfers/connections can be accessed")]
         public void ThenEndpointApiAccountsInternalAccountIdTransfersConnectionsCanBeAccessed()
         {
-            var accountId = _objectContext.GetAccountId();
+            var accountId = GetAccountId();
             _innerApiRestClient.ExecuteEndpoint($"/api/accounts/internal/{accountId}/transfers/connections", HttpStatusCode.OK);
         }
+
+        private string GetHashedAccountId() => _objectContext.GetHashedAccountId();
+
+        private string GetAccountId() => _objectContext.GetAccountId();
     }
 }
