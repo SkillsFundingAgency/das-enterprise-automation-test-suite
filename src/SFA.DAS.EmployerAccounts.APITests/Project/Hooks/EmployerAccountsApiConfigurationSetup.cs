621db32c55638325fd2c691b5fae80f622ec1417
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2023-01-20 10:58:22 +0000

    refactored hashing service (#1315)

---
+++ src/SFA.DAS.EmployerAccounts.APITests/Project/Hooks/EmployerAccountsApiConfigurationSetup.cs
@@ -0,0 +1,21 @@
+﻿using SFA.DAS.ConfigurationBuilder;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.EmployerAccounts.APITests.Project.Hooks
+{
+    [Binding]
+    public class EmployerAccountsApiConfigurationSetup
+    {
+        private readonly ScenarioContext _context;
+        private readonly IConfigSection _configSection;
+
+        public EmployerAccountsApiConfigurationSetup(ScenarioContext context)
+        {
+            _context = context;
+            _configSection = context.Get<IConfigSection>();
+        }
+
+        [BeforeScenario(Order = 2)]
+        public void SetUpEmployerAccountsApiConfig() => _context.SetEmployerAccountsApiConfig(_configSection.GetConfigSection<EmployerAccountsApiConfig>());
+    }
+}
