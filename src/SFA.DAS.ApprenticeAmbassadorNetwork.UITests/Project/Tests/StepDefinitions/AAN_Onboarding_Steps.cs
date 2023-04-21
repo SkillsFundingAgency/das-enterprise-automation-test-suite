1a253a2105ace68807a715ff314342fae2b1b02a
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2023-04-21 12:15:14 +0100

    upadted steps

--- src/SFA.DAS.ApprenticeAmbassadorNetwork.UITests/Project/Tests/StepDefinitions/AAN_Onboarding_Steps.cs
+++ src/SFA.DAS.ApprenticeAmbassadorNetwork.UITests/Project/Tests/StepDefinitions/AAN_Onboarding_Steps.cs
@@ -1,4 +1,5 @@
-﻿using SFA.DAS.ApprenticeAmbassadorNetwork.UITests.Project.Tests.Pages.RoatpApply;
+﻿using Polly;
+using SFA.DAS.ApprenticeAmbassadorNetwork.UITests.Project.Tests.Pages.RoatpApply;
 using SFA.DAS.UI.Framework;
 
 namespace SFA.DAS.ApprenticeAmbassadorNetwork.UITests.Project.Tests.StepDefinitions
@@ -6,22 +7,20 @@ namespace SFA.DAS.ApprenticeAmbassadorNetwork.UITests.Project.Tests.StepDefiniti
     [Binding]
     public class AAN_Onboarding_Steps
     {
-        private ScenarioContext _context;
-        private SignInPage _signInPage;
-        private readonly AANConfig _config;
+        private readonly ScenarioContext context;
+        private readonly AANConfig config;
 
         public AAN_Onboarding_Steps(ScenarioContext context)
         {
-            _context = context;
-            _signInPage = new SignInPage(context);
-            _config = context.GetAANConfig<AANConfig>();
+            this.context = context;
+            config = context.GetAANConfig<AANConfig>();
         }
 
         [Given(@"the provider logs into AAN portal")]
         public void GivenTheProviderLogsIntoAANPortal()
         {
-            _context.Get<TabHelper>().GoToUrl(UrlConfig.AAN_BaseUrl);
-            _signInPage.SubmitValidUserDetails(_config.AANUserName,_config.AANPassword);
+            context.Get<TabHelper>().GoToUrl(UrlConfig.AAN_BaseUrl);
+            new SignInPage(context).SubmitValidUserDetails(config.AANUserName,config.AANPassword);
 
         }
     }
