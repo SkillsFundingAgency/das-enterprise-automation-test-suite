7fb463540ea87d9f12c56e40adbd799542964561
Sudhakar Chinoor <45876897+sudhakarchinoor@users.noreply.github.com>
2023-04-21 15:48:36 +0100

    Apprentice onboarding journey  (#1370)
    * AAN initial test added
    
    * Appsettings changes
    
    * updated project
    
    * upadted steps
    
    * Updated Project Name
    
    * Updated the config set up
    
    * updated appsettings
    
    ---------
    
    Co-authored-by: Arvinth Seran <arvinth.seran@digital.education.gov.uk>

---
+++ src/SFA.DAS.ApprenticeAmbassadorNetwork.UITests/Project/AANConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace SFA.DAS.ApprenticeAmbassadorNetwork.UITests.Project;
+
+public class AANConfig
+{
+    public string AANPassword { get; set; }
+    public string AANUserName { get; set; }
+}
---
+++ src/SFA.DAS.ApprenticeAmbassadorNetwork.UITests/Project/Helpers/StepsHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace SFA.DAS.ApprenticeAmbassadorNetwork.UITests.Project.Helpers;
+
+public class StepsHelper
+{
+    private readonly ScenarioContext _context;
+    private readonly FormCompletionHelper formCompletionHelper;
+
+    public StepsHelper(ScenarioContext context)
+    {
+        _context = context;
+        formCompletionHelper = _context.Get<FormCompletionHelper>();
+    }
+
+    public void NavigateToReviewYourDetails()
+    {
+        formCompletionHelper.ClickLinkByText("Your standards and training venues");
+    }
+}
