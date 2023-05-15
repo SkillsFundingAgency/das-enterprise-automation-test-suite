7f961a0a8a1d824ba9d53c83cca2f59c706ccb56
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2023-05-15 09:25:17 +0100

    firx for MS_05_Accessibility_Test

--- src/SFA.DAS.ManagingStandards.UITests/Project/Tests/Pages/ManageAStandard_ActuaryPage.cs
+++ src/SFA.DAS.ManagingStandards.UITests/Project/Tests/Pages/ManageAStandard_ActuaryPage.cs
@@ -2,7 +2,7 @@
 
 public class ManageAStandard_ActuaryPage : ManagingStandardsBasePage
 {
-    protected override string PageTitle => managingStandardsDataHelpers.Standard_ActuaryLevel7;
+    protected override string PageTitle => managingStandardsDataHelpers?.Standard_ActuaryLevel7;
 
     public ManageAStandard_ActuaryPage(ScenarioContext context) : base(context) { }
 
