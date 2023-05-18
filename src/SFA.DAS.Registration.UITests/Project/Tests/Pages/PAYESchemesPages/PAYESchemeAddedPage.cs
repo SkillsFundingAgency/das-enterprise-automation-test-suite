85985deaa48371005a021cc4fccf169a7a354057
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2023-05-18 16:37:30 +0100

    updated css sslector

--- src/SFA.DAS.Registration.UITests/Project/Tests/Pages/PAYESchemesPages/PAYESchemeAddedPage.cs
+++ src/SFA.DAS.Registration.UITests/Project/Tests/Pages/PAYESchemesPages/PAYESchemeAddedPage.cs
@@ -7,9 +7,9 @@ namespace SFA.DAS.Registration.UITests.Project.Tests.Pages.PAYESchemesPages
     public class PAYESchemeAddedPage : RegistrationBasePage
     {
         protected override string PageTitle => $"{objectContext.GetGatewayPaye(1)} has been added";
-        
+
         #region Locators
-        protected override By PageHeader => By.CssSelector(".bold-large");
+        protected override By PageHeader => By.CssSelector(".bold-large, .das-notification__heading");
         protected override By ContinueButton => By.CssSelector("button#accept");
         private By ContinueAccountSetupRadioButton => By.Id("choice3");
         #endregion
