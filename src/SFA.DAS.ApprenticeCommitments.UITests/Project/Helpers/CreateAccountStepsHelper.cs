7821392ac68ccddcef0b584594e58e7513a26ec3
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2023-06-08 17:40:40 +0100

    updated RetryGetRegistrationId

--- src/SFA.DAS.ApprenticeCommitments.UITests/Project/Helpers/CreateAccountStepsHelper.cs
+++ src/SFA.DAS.ApprenticeCommitments.UITests/Project/Helpers/CreateAccountStepsHelper.cs
@@ -102,7 +102,7 @@ namespace SFA.DAS.ApprenticeCommitments.UITests.Project.Helpers
             return new StartPage(_context);
         }
 
-        private void RetryGetRegistrationId(Action action) => _assertHelper.RetryOnNUnitExceptionWithLongerTimeOut(action);
+        private void RetryGetRegistrationId(Action action) => _assertHelper.RetryOnNUnitException(action, RetryTimeOut.GetTimeSpan(new int[] { 10, 20, 30, 60, 120, 180 }));
 
         private string GetApprenticeEmail() => _objectContext.GetApprenticeEmail();
     }
