1f78d1ec7502f79feb1568d080844a1f58a6a0c9
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2023-06-14 17:35:27 +0100

    minor changes

--- src/SFA.DAS.UI.FrameworkHelpers/FormCompletionHelper.cs
+++ src/SFA.DAS.UI.FrameworkHelpers/FormCompletionHelper.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using SFA.DAS.TestDataExport;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace SFA.DAS.UI.FrameworkHelpers
 {
