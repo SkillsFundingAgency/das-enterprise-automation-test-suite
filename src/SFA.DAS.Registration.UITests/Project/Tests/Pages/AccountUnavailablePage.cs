b6d4ce793ae24c61db4d6be41a690b3d0d1e8793
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2023-07-04 22:21:55 +0100

    updated AccountUnavailablePage

--- src/SFA.DAS.Registration.UITests/Project/Tests/Pages/AccountUnavailablePage.cs
+++ src/SFA.DAS.Registration.UITests/Project/Tests/Pages/AccountUnavailablePage.cs
@@ -3,13 +3,10 @@ using TechTalk.SpecFlow;
 
 namespace SFA.DAS.Registration.UITests.Project.Tests.Pages
 {
-    public class AccountUnavailablePage : VerifyBasePage
+    public class AccountUnavailablePage : RegistrationBasePage
     {
         protected override string PageTitle => "This account is unavailable";
 
-        public AccountUnavailablePage(ScenarioContext context) : base(context)
-        {
-
-        }
+        public AccountUnavailablePage(ScenarioContext context) : base(context) => VerifyPage();
     }
 }
