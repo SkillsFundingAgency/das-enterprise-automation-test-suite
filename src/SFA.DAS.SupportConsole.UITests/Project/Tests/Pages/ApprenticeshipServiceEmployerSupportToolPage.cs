8c058c9c9126c3269be5fd4ba37a6589bf0a86de
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2023-07-06 09:29:33 +0100

    updated ApprenticeshipServiceEmployerSupportToolPage

--- src/SFA.DAS.SupportConsole.UITests/Project/Tests/Pages/ApprenticeshipServiceEmployerSupportToolPage.cs
+++ src/SFA.DAS.SupportConsole.UITests/Project/Tests/Pages/ApprenticeshipServiceEmployerSupportToolPage.cs
@@ -4,17 +4,13 @@ namespace SFA.DAS.SupportConsole.UITests.Project.Tests.Pages
     internal class ApprenticeshipServiceEmployerSupportToolPage : SupportConsoleBasePage
     {
         protected override string PageTitle => "Apprenticeship service employer support tool";
-        private By StartNowButton => By.CssSelector(".govuk-button--start");
+        private static By StartNowButton => By.CssSelector(".govuk-button--start");
 
-
-
-        public ApprenticeshipServiceEmployerSupportToolPage(ScenarioContext context) : base(context) { }
+        public ApprenticeshipServiceEmployerSupportToolPage(ScenarioContext context) : base(context) => VerifyPage();
         
-
         public IdamsPage ClickStartNowButton() 
         {
-            if(pageInteractionHelper.IsElementDisplayed(StartNowButton))
-                formCompletionHelper.Click(StartNowButton);
+           formCompletionHelper.Click(StartNowButton);
 
             return new IdamsPage(context);
         }
