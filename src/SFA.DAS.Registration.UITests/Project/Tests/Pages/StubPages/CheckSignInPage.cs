189baff4787e281f24dd7506dd09d3e0d9f04abb
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2023-09-06 13:32:00 +0100

    more updates

--- src/SFA.DAS.Registration.UITests/Project/Tests/Pages/StubPages/CheckSignInPage.cs
+++ src/SFA.DAS.Registration.UITests/Project/Tests/Pages/StubPages/CheckSignInPage.cs
@@ -7,6 +7,7 @@ namespace SFA.DAS.Registration.UITests.Project.Tests.Pages.StubPages
     public class CheckStubSignInPage : CheckPage
     {
         protected override string PageTitle { get; }
+
         protected override By Identifier => By.CssSelector(".govuk-input[id='Email']");
 
         public CheckStubSignInPage(ScenarioContext context) : base(context) { }
