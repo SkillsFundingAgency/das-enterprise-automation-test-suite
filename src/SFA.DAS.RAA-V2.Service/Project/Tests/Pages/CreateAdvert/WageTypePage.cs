ddd8be1d38d117a27fa3afd2c1f31778893568e0
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2023-10-19 11:18:12 +0100

    fix build

--- src/SFA.DAS.RAA-V2.Service/Project/Tests/Pages/CreateAdvert/WageTypePage.cs
+++ src/SFA.DAS.RAA-V2.Service/Project/Tests/Pages/CreateAdvert/WageTypePage.cs
@@ -29,17 +29,38 @@ namespace SFA.DAS.RAA_V2.Service.Project.Tests.Pages.CreateAdvert
 
         private void ChooseWage(string wageType)
         {
-            if (wageType == RAAV2Const.NationalMinWages) SelectNationalMinimumWage();
+            if (wageType == RAAV2Const.NationalMinWages) SelectRadioOptionByForAttribute("wage-type-national-minimum-wage");
 
-            else if (wageType == RAAV2Const.FixedWageType) SelectFixedWageType();
+            else if (wageType == RAAV2Const.FixedWageType) SelectRadioOptionByForAttribute("wage-type-fixed");
 
-            else SelectNationalMinimumWageForApprentices();
+            else SelectRadioOptionByForAttribute("wage-type-national-minimum-wage-for-apprentices");
         }
 
-        private void SelectNationalMinimumWage() => SelectRadioOptionByForAttribute("wage-type-national-minimum-wage");
+        public PreviewYourVacancyPage SelectNationalMinimumWage()
+        {
+            SelectRadioOptionByForAttribute("wage-type-national-minimum-wage");
+            return ContinueToPreviewYourVacancyPage();
+        }
 
-        private void SelectFixedWageType() => SelectRadioOptionByForAttribute("wage-type-fixed");
+        public PreviewYourVacancyPage SelectNationalMinimumWageForApprentices()
+        {
+            SelectRadioOptionByForAttribute("wage-type-national-minimum-wage-for-apprentices");
+            return ContinueToPreviewYourVacancyPage();
+        }
 
-        private void SelectNationalMinimumWageForApprentices() => SelectRadioOptionByForAttribute("wage-type-national-minimum-wage-for-apprentices");
+        public PreviewYourVacancyPage SelectFixedWageType()
+        {
+            SelectRadioOptionByForAttribute("wage-type-fixed");
+            formCompletionHelper.EnterText(FixedWageYearlyAmount, rAAV2DataHelper.FixedWageYearlyAmount);
+            return ContinueToPreviewYourVacancyPage();
+        }
+
+        private PreviewYourVacancyPage ContinueToPreviewYourVacancyPage()
+        {
+            formCompletionHelper.EnterText(WageAdditionalInformation, rAAV2DataHelper.OptionalMessage);
+            Continue();
+            pageInteractionHelper.WaitforURLToChange("part1-complete");
+            return new PreviewYourVacancyPage(context);
+        }
     }
 }
