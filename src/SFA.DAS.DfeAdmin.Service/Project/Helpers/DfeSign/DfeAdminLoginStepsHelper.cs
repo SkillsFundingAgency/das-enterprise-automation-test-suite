bcb89ce2778dc38d59b2016c2a5a00fc82166821
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2023-11-10 14:50:12 +0000

    added comments

--- src/SFA.DAS.DfeAdmin.Service/Project/Helpers/DfeSign/DfeAdminLoginStepsHelper.cs
+++ src/SFA.DAS.DfeAdmin.Service/Project/Helpers/DfeSign/DfeAdminLoginStepsHelper.cs
@@ -1,7 +1,6 @@
 ﻿using SFA.DAS.DfeAdmin.Service.Project.Helpers.DfeSign.User;
 using SFA.DAS.DfeAdmin.Service.Project.Tests.Pages.LandingPage;
 using SFA.DAS.Login.Service;
-using SFA.DAS.Login.Service.Project.Helpers;
 using SFA.DAS.UI.Framework;
 
 namespace SFA.DAS.DfeAdmin.Service.Project.Helpers.DfeSign;
@@ -12,6 +11,7 @@ public class DfeAdminLoginStepsHelper
 
     public DfeAdminLoginStepsHelper(ScenarioContext context) => _context = context;
 
+    #region Login
     public void NavigateAndLoginToASAdmin()
     {
         _context.Get<TabHelper>().GoToUrl(UrlConfig.Admin_BaseUrl);
@@ -25,17 +25,22 @@ public class DfeAdminLoginStepsHelper
 
     public void LoginToAsAdmin() => SubmitValidAsLoginDetails(GetAsAdminUser());
 
-    public void CheckAndLoginToAsAdmin() => CheckAndLoginTo(new CheckASAdminLandingPage(_context), new ASAdminLandingPage(_context), GetAsAdminUser());
-
     public void LoginToSupportConsole(DfeAdminUser dfeAdminUser) => SubmitValidLoginDetails(new ASEmpSupportConsoleLandingPage(_context), dfeAdminUser);
 
     public void LoginToSupportTool(DfeAdminUser dfeAdminUser) => SubmitValidLoginDetails(new ASEmpSupportToolLandingPage(_context), dfeAdminUser);
 
-    public void CheckAndLoginToSupportTool(DfeAdminUser dfeAdminUser) => CheckAndLoginTo(new CheckASEmpSupportToolLandingPage(_context), new ASEmpSupportToolLandingPage(_context), dfeAdminUser);
-
     public void SubmitValidAsLoginDetails(ASLandingBasePage landingPage) => SubmitValidLoginDetails(landingPage, GetAsAdminUser());
 
+    #endregion
+
+    #region CheckAndLogin
+    public void CheckAndLoginToAsAdmin() => CheckAndLoginTo(new CheckASAdminLandingPage(_context), new ASAdminLandingPage(_context), GetAsAdminUser());
+
+    public void CheckAndLoginToSupportTool(DfeAdminUser dfeAdminUser) => CheckAndLoginTo(new CheckASEmpSupportToolLandingPage(_context), new ASEmpSupportToolLandingPage(_context), dfeAdminUser);
+
     public void CheckAndLoginToASVacancyQa() => CheckAndLoginTo(new CheckASVacancyQaLandingPage(_context), new ASVacancyQaLandingPage(_context), _context.GetUser<VacancyQaUser>());
+
+    #endregion
 
     private void SubmitValidAsLoginDetails(DfeAdminUser dfeAdminUser) => SubmitValidLoginDetails(new ASAdminLandingPage(_context), dfeAdminUser);
 
@@ -49,7 +54,7 @@ public class DfeAdminLoginStepsHelper
     private void SubmitValidLoginDetails(ASLandingBasePage landingPage, DfeAdminUser dfeAdminUser)
     {
         landingPage.ClickStartNowButton();
-        
+
         SubmitValidLoginDetails(dfeAdminUser);
     }
 
