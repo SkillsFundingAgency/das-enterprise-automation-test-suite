2a70dd1ed3a6ad70a586c7008eff22f8bc858484
Najamuddin-Muhammad <najamuddin.muhammad@education.gov.uk>
2023-12-01 11:40:52 +0000

    initial commit

---
+++ src/SFA.DAS.Registration.UITests/Project/Helpers/EmployerAccountRegistrationHelper.cs
@@ -0,0 +1,70 @@
+﻿using SFA.DAS.FrameworkHelpers;
+using SFA.DAS.Registration.UITests.Project.Tests.Pages;
+using SFA.DAS.UI.Framework.TestSupport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using static SFA.DAS.Registration.UITests.Project.Helpers.EnumHelper;
+
+namespace SFA.DAS.Registration.UITests.Project.Helpers
+{
+    public class EmployerAccountRegistrationHelper : AccountCreationStepsHelper
+    {
+        CreateYourEmployerAccountPage _createYourEmployerAccountPage;
+
+        public EmployerAccountRegistrationHelper(ScenarioContext context) : base(context)
+        {
+           
+        }
+
+        public void CreateNewEmployerAccount()
+        {
+            // Navigate to home page
+            // click on register
+            // enter first and last name
+            // enter PAYE details & add org
+            // confirm arg name
+            // sign legal agreement
+            // give provider permissions
+        }
+
+        public void AddAdditionalAccount()
+        { 
+        
+        }
+
+
+        internal CreateYourEmployerAccountPage RegisterStubUserAccount2(CreateAnAccountToManageApprenticeshipsPage indexPage, string email)
+        {
+            return _createYourEmployerAccountPage = indexPage
+                .ClickOnCreateAccountLink()
+                .RegisterUser()
+                .ContinueToCreateYourEmployerAccountPage();
+        }
+
+        internal void AddPayeAndOrgDetails(int payeIndex, OrgType orgType = OrgType.Default)
+        {
+            _createYourEmployerAccountPage
+                .GoToAddPayeLink()
+                .SelectOptionLessThan3Million()
+                .AddPaye()
+                .ContinueToGGSignIn()
+                .SignInTo(payeIndex)
+                .SearchForAnOrganisation(orgType)
+                .SelectYourOrganisation(orgType)
+                .ClickYesContinueButton();
+        }
+
+
+            
+
+
+
+
+
+
+    }
+}
--- src/SFA.DAS.Registration.UITests/Project/Tests/StepDefinitions/CreateAccountSteps.cs
+++ src/SFA.DAS.Registration.UITests/Project/Tests/StepDefinitions/CreateAccountSteps.cs
@@ -18,6 +18,8 @@ namespace SFA.DAS.Registration.UITests.Project.Tests.StepDefinitions
         private readonly RegistrationSqlDataHelper _registrationSqlDataHelper;
         private readonly TprSqlDataHelper _tprSqlDataHelper;
         private readonly AccountCreationStepsHelper _accountCreationStepsHelper;
+        //private readonly EmployerAccountRegistrationHelper _employerAccountRegistrationHelper;
+
         private HomePage _homePage;
         private AddAPAYESchemePage _addAPAYESchemePage;
         private GgSignInPage _gGSignInPage;
@@ -30,6 +32,7 @@ namespace SFA.DAS.Registration.UITests.Project.Tests.StepDefinitions
         private UsingYourGovtGatewayDetailsPage _usingYourGovtGatewayDetailsPage;
         private CreateAnAccountToManageApprenticeshipsPage _indexPage;
         private AddPayeSchemeUsingGGDetailsPage _addPayeSchemeUsingGGDetailsPage;
+        private DoYouAcceptTheEmployerAgreementOnBehalfOfPage _doYouAcceptTheEmployerAgreementOnBehalfOfPage;
 
         public CreateAccountSteps(ScenarioContext context)
         {
@@ -39,6 +42,7 @@ namespace SFA.DAS.Registration.UITests.Project.Tests.StepDefinitions
             _registrationSqlDataHelper = context.Get<RegistrationSqlDataHelper>();
             _tprSqlDataHelper = context.Get<TprSqlDataHelper>();
             _accountCreationStepsHelper = new AccountCreationStepsHelper(context);
+            //_employerAccountRegistrationHelper = new EmployerAccountRegistrationHelper(context);
         }
 
         [Given(@"a User Account is created")]
@@ -70,7 +74,7 @@ namespace SFA.DAS.Registration.UITests.Project.Tests.StepDefinitions
                     .SelectFirstOrganisationAndContinue();
             }
 
-            _signAgreementPage = _accountCreationStepsHelper.GoToSignAgreementPage(_checkYourDetailsPage);
+            _doYouAcceptTheEmployerAgreementOnBehalfOfPage = _accountCreationStepsHelper.GoToSignAgreementPage(_checkYourDetailsPage);
         }
 
         [When(@"the User adds Invalid PAYE details")]
@@ -86,8 +90,12 @@ namespace SFA.DAS.Registration.UITests.Project.Tests.StepDefinitions
         public void AddOrganisationDetails() => AddOrganisationTypeDetails(OrgType.Default);
 
         [When(@"adds (Company|PublicSector|Charity) Type Organisation details")]
-        public void AddOrganisationTypeDetails(OrgType orgType) =>
-            _signAgreementPage = _accountCreationStepsHelper.GoToSignAgreementPage(_searchForYourOrganisationPage.SearchForAnOrganisation(orgType).SelectYourOrganisation(orgType));
+        public void AddOrganisationTypeDetails(OrgType orgType)
+        {
+            //=> _signAgreementPage = _accountCreationStepsHelper.GoToSignAgreementPage(_searchForYourOrganisationPage.SearchForAnOrganisation(orgType).SelectYourOrganisation(orgType));
+            _doYouAcceptTheEmployerAgreementOnBehalfOfPage = _accountCreationStepsHelper.GoToSignAgreementPage(_searchForYourOrganisationPage.SearchForAnOrganisation(orgType).SelectYourOrganisation(orgType));
+        }
+        
 
         [When(@"enters an Invalid Company number for Org search")]
         public SelectYourOrganisationPage WhenAnEmployerEntersAnInvalidCompanyNumberForOrgSearchInOrganisationSearchPage()
@@ -106,9 +114,12 @@ namespace SFA.DAS.Registration.UITests.Project.Tests.StepDefinitions
         [Then(@"the Employer Signs the Agreement")]
         public void SignTheAgreement()
         {
-            _homePage = _signAgreementPage
+            _homePage = _doYouAcceptTheEmployerAgreementOnBehalfOfPage
                 .SignAgreement()
-                .ClickOnViewYourAccountButton();
+                .GoToTrainingProviderLink()
+                .AddTrainingProviderLater()
+                .SelectGoToYourEmployerAccountHomepage();
+                //.ClickOnViewYourAccountButton();
 
             SetAgreementId(_homePage);
         }
