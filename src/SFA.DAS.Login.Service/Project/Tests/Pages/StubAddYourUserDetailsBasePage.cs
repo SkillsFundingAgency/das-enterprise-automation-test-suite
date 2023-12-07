0a89ad64372a9c08ba228da16bc5d1451ace82a1
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2023-12-07 12:00:32 +0000

    updated epao create account test selector

--- src/SFA.DAS.Login.Service/Project/Tests/Pages/StubAddYourUserDetailsBasePage.cs
+++ src/SFA.DAS.Login.Service/Project/Tests/Pages/StubAddYourUserDetailsBasePage.cs
@@ -8,8 +8,8 @@ public abstract class StubAddYourUserDetailsBasePage : VerifyBasePage
 {
     protected override string PageTitle => "Add your user details";
 
-    private static By FirstNameInput => By.CssSelector($"#FirstName");
-    private static By LastNameInput => By.CssSelector($"#LastName");
+    private static By FirstNameInput => By.CssSelector($"#FirstName, #GivenName");
+    private static By LastNameInput => By.CssSelector($"#LastName, #FamilyName");
     protected override By ContinueButton => By.CssSelector("[id='main-content'] button.govuk-button");
 
     public StubAddYourUserDetailsBasePage(ScenarioContext context) : base(context) => VerifyPage();
