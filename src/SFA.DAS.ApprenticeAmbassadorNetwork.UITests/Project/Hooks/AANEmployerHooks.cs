1685b0053ac29e914b3c0217a4617607f391e15d
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2023-12-19 16:29:36 +0000

    updated aan employer login

--- src/SFA.DAS.ApprenticeAmbassadorNetwork.UITests/Project/Hooks/AANEmployerHooks.cs
+++ src/SFA.DAS.ApprenticeAmbassadorNetwork.UITests/Project/Hooks/AANEmployerHooks.cs
@@ -4,14 +4,5 @@
 public class AANEmployerHooks(ScenarioContext context) : AANBaseHooks(context)
 {
     [BeforeScenario(Order = 31)]
-    public void Navigate_Employer()
-    {
-        var account = "mpbd6m";
-
-        if (context.ScenarioInfo.Tags.Contains("aanemployeronboardingreset")) account = "n7kry6";
-
-        UrlConfig.AAN_Employer_BaseUrl = $"https://employer-aan.pp-eas.apprenticeships.education.gov.uk/accounts/{account}";
-
-        tabHelper.GoToUrl(UrlConfig.AAN_Employer_BaseUrl);
-    }
+    public void Navigate_Employer() => tabHelper.GoToUrl(UrlConfig.AAN_Employer_BaseUrl);
 }
