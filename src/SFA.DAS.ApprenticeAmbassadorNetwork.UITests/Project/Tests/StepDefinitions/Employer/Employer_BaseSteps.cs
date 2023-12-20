8add0322fe874f8043ab766fef247b67843ce610
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2023-12-20 09:26:51 +0000

    updated aan employer login (#1505)

--- src/SFA.DAS.ApprenticeAmbassadorNetwork.UITests/Project/Tests/StepDefinitions/Employer/Employer_BaseSteps.cs
+++ src/SFA.DAS.ApprenticeAmbassadorNetwork.UITests/Project/Tests/StepDefinitions/Employer/Employer_BaseSteps.cs
@@ -1,6 +1,8 @@
 ﻿using SFA.DAS.ApprenticeAmbassadorNetwork.UITests.Project.Tests.Pages.Employer;
 using SFA.DAS.Login.Service.Project.Helpers;
-using SFA.DAS.Registration.UITests.Project.Tests.Pages.StubPages;
+using SFA.DAS.Registration.UITests.Project.Helpers;
+using SFA.DAS.Registration.UITests.Project.Tests.Pages;
+using System;
 
 namespace SFA.DAS.ApprenticeAmbassadorNetwork.UITests.Project.Tests.StepDefinitions.Employer;
 
@@ -8,5 +10,9 @@ public abstract class Employer_BaseSteps(ScenarioContext context) : AppEmp_BaseS
 {
     protected Employer_NetworkHubPage networkHubPage;
 
-    protected void EmployerSign(EasAccountUser user) => new StubSignInEmployerPage(context).Login(user).Continue();
+    protected void EmployerSign(EasAccountUser user) => EmployerSign(() => new EmployerPortalLoginHelper(context).Login(user, true));
+
+    protected void EmployerSign(MultipleEasAccountUser user) => EmployerSign(() => new MultipleAccountsLoginHelper(context, user).Login(user, true));
+
+    private static void EmployerSign(Func<HomePage> func) => func().GoToAanHomePage();
 }
--- src/SFA.DAS.UI.Framework/UrlConfig.cs
+++ src/SFA.DAS.UI.Framework/UrlConfig.cs
@@ -5,7 +5,7 @@ namespace SFA.DAS.UI.Framework;
 
 public static class UrlConfig
 {
-    public static string AAN_Employer_BaseUrl { get; set; }
+    public static string AAN_Employer_BaseUrl => EmployerApprenticeshipService_BaseUrl;
     public static string AAN_Apprentice_BaseUrl => $"https://aan.{EnvironmentConfig.EnvironmentName}-aas.apprenticeships.education.gov.uk/onboarding/before-you-start";
     public static string AAN_Admin_BaseUrl => $"https://{EnvironmentConfig.EnvironmentName}-adminaan.apprenticeships.education.gov.uk/";
     public static string Admin_BaseUrl => $"https://{EnvironmentConfig.EnvironmentName}-admin.apprenticeships.education.gov.uk/";
