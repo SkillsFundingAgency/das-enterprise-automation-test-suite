9ca45f37b586ed00c245b772f225a951ee322012
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2024-02-26 14:54:03 +0000

    Framework : Updated email to mailasour (#1536)
    * faa random email update
    
    * registration another email fix
    
    * updated Faa email address
    
    * code cleanup

--- src/SFA.DAS.FrameworkHelpers/RandomDataGenerator.cs
+++ src/SFA.DAS.FrameworkHelpers/RandomDataGenerator.cs
@@ -44,9 +44,7 @@
             return randomString;
         }
 
-        public static string GenerateRandomEmail() => GenerateRandomAlphanumericString(10) + DateTime.Now.Millisecond + "@example.com";
-
-        public static string GenerateRandomEmail(string email) { var emailsplit = email.Split("@"); return $"{emailsplit[0]}@{DateTime.Now.ToNanoSeconds()}{emailsplit[1]}"; }
+        public static string GenerateRandomEmail(string suffix, string domain) => $"{suffix}_{GenerateRandomAlphanumericString(5)}_{DateTime.Now.ToSeconds()}_{DateTime.Now.ToNanoSeconds()}@{domain}";
 
         public static int GenerateRandomDateOfMonth() => GenerateRandomNumberBetweenTwoValues(1, 28);
 
