2eb60dc5e6e8a6b4f702f91c925e6cd76eca901c
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2024-04-18 11:53:03 +0100

    updated project config

--- src/SFA.DAS.UI.FrameworkHelpers/FormCompletionHelper.cs
+++ src/SFA.DAS.UI.FrameworkHelpers/FormCompletionHelper.cs
@@ -3,7 +3,6 @@ using SFA.DAS.FrameworkHelpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 
 namespace SFA.DAS.UI.FrameworkHelpers;
 
