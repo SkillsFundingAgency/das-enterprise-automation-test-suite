77cca1341def7428eac0a926bb053a871d19d329
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2024-04-24 17:52:05 +0100

    Live navigation smoke test (#1570)
    * minor changes
    
    * added sketon project
    
    * added steps
    
    * end of day commit
    
    * added steps
    
    * updated project config
    
    * updated Live_EAS_01
    
    * updated live smoke test
    
    * minor changes
    
    * added EnterPpiAndContinue

---
+++ src/SFA.DAS.Live.SmokeTests/Project/Helpers/LiveEasUser.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.Live.SmokeTests.Project.Helpers;
+
+public class LiveEasUser : LoginUser
+{
+    public string Password { get; set; }
+
+    public string OrganisationName { get; set; }
+}
