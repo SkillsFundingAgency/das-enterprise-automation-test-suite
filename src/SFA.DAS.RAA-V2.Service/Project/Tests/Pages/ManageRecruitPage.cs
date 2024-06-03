02482904c3612a9e97828ef8edc4890bce287462
ratheeshri1984 <ratheeshri1984@gmail.com>
2024-06-03 14:51:37 +0100

    EC-464-Manage recruit page changes

--- src/SFA.DAS.RAA-V2.Service/Project/Tests/Pages/ManageRecruitPage.cs
+++ src/SFA.DAS.RAA-V2.Service/Project/Tests/Pages/ManageRecruitPage.cs
@@ -1,10 +1,14 @@
-﻿using TechTalk.SpecFlow;
+﻿using OpenQA.Selenium;
+using TechTalk.SpecFlow;
 
 namespace SFA.DAS.RAA_V2.Service.Project.Tests.Pages
 {
-    public class ManageRecruitPage(ScenarioContext context) : Raav2BasePage(context)
+    public class ManageRecruitPage(ScenarioContext context) : Raav2BasePage(context, false)
     {
         protected override string PageTitle => isRaaV2Employer ? "Manage Advert" : "Manage vacancy";
+
+        protected static By EditAdvertActionSelector => By.XPath("//a[@class='govuk-link' and contains(@href, 'edit-dates') and text()='Change']");
+        protected static By CloseAdvertActionSelector => By.XPath("//a[@class='govuk-link' and contains(@href, 'close') and text()='Change']");
 
         public CloneVacancyDatesPage CloneAdvert()
         {
@@ -14,13 +18,13 @@ namespace SFA.DAS.RAA_V2.Service.Project.Tests.Pages
 
         public EditVacancyDatesPage EditAdvert()
         {
-            formCompletionHelper.ClickLinkByText("Edit advert");
+            formCompletionHelper.ClickElement(EditAdvertActionSelector);
             return new EditVacancyDatesPage(context);
         }
 
         public CloseVacancyPage CloseAdvert()
         {
-            formCompletionHelper.ClickLinkByText("Close advert");
+            formCompletionHelper.ClickElement(CloseAdvertActionSelector);
             return new CloseVacancyPage(context);
         }
 
