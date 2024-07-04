344c2e8e9f3c1800437782616a83a30afa5bc80c
Raj <reachash1@hotmail.com>
2024-07-04 13:59:10 +0100

    RAT_NewProject_Setup

---
+++ src/SFA.DAS.RequestApprenticeshipTraining.UITests/Project/Hooks/BeforeScenarioHooks.cs
@@ -0,0 +1,20 @@
+﻿using SFA.DAS.FrameworkHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.RequestApprenticeshipTraining.UITests.Project.Hooks
+{
+    [Binding]
+    public class BeforeScenarioHooks(ScenarioContext context)
+    {
+        [BeforeScenario(Order = 46)]
+        public void SetUpHelpers()
+        {
+            
+        }
+    }
+}
---
+++ src/SFA.DAS.RequestApprenticeshipTraining.UITests/Project/Tests/Features/RAT_Emp_01_EmployerSingleLocationJourney.feature
@@ -0,0 +1,6 @@
+﻿Feature: RAT_Emp_01_EmployerSingleLocationJourney
+
+@regression
+@mailosaur
+Scenario: RAT_Emp_01_EmployerSingleLocatoinJourney
+	Given the User searches a course then navigates to the provider list
