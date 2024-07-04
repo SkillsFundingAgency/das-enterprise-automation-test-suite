04a9fee8a3db08ab207cd066e3c182245cb1d47a
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2024-07-04 14:58:57 +0100

    renamed FAAV2 to FAA

---
+++ src/SFA.DAS.FAA.UITests/Project/Tests/Pages/StubPages/StubSignInFAAPage.cs
@@ -0,0 +1,28 @@
+﻿namespace SFA.DAS.FAA.UITests.Project.Tests.Pages.StubPages;
+
+public class StubSignInFAAPage(ScenarioContext context) : StubSignInBasePage(context)
+{
+    protected override string PageTitle => StubSignInFAAPageTitle;
+
+    internal static string StubSignInFAAPageTitle => "Stub Authentication - Enter sign in details";
+
+    private static By MobilePhoneInput => By.CssSelector(".govuk-input[id='MobilePhone']");
+
+    public StubYouHaveSignedInFAAPage SubmitValidUserDetails(FAAPortalUser loginUser)
+    {
+        return GoToStubYouHaveSignedInFAAPage(loginUser.Username, loginUser.IdOrUserRef, loginUser.MobilePhone, false);
+    }
+
+    public StubYouHaveSignedInFAAPage CreateAccount(string email) => GoToStubYouHaveSignedInFAAPage(email, email, "0", true);
+
+    private StubYouHaveSignedInFAAPage GoToStubYouHaveSignedInFAAPage(string email, string idOrUserRef, string mobilePhone, bool newUser)
+    {
+        EnterLoginDetails(email, idOrUserRef);
+
+        formCompletionHelper.EnterText(MobilePhoneInput, mobilePhone);
+
+        ClickSignIn();
+
+        return new StubYouHaveSignedInFAAPage(context, email, idOrUserRef, newUser);
+    }
+}
