51b51fdfd381b58eb5746bfc3207e1d93ab6730c
ogrwill <will.ogrady@digital.education.gov.uk>
2024-07-11 14:50:59 +0100

    extending AP_CoC_05 to check option and version

--- src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Common/SelectAStandardVersionPage.cs
+++ src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Common/SelectAStandardVersionPage.cs
@@ -1,34 +1,25 @@
 ﻿using OpenQA.Selenium;
 using SFA.DAS.Approvals.UITests.Project.Tests.Pages.Employer;
 using SFA.DAS.Approvals.UITests.Project.Tests.Pages.Provider;
-using SFA.DAS.FrameworkHelpers;
 using TechTalk.SpecFlow;
 
 namespace SFA.DAS.Approvals.UITests.Project.Tests.Pages.Common
 {
-    public class SelectAStandardOptionpage(ScenarioContext context, bool verifyPage = true) : ApprovalsBasePage(context, verifyPage)
+    public class SelectAStandardVersionPage(ScenarioContext context, bool verifyPage = true) : ApprovalsBasePage(context, verifyPage)
     {
-        protected override string PageTitle => "Select a standard option";
+        protected override string PageTitle => "Select a standard version";
 
         protected override By ContinueButton => By.CssSelector("#main-content .govuk-button");
 
-        public void SelectAStandardOption()
-        {
-            formCompletionHelper.ClickElement(() => RandomDataGenerator.GetRandomElementFromListOfElements(pageInteractionHelper.FindElements(RadioLabels)));
-
-            Continue();
-        }
-
-        public void ContinueWithAlreadySelectedStandardOption() => Continue();
-
         public ApprenticeDetailsPage GoBackToApprenticeDetailsPage()
         {
-            Back();
+            tabHelper.NavigateBrowserBack();
             return new ApprenticeDetailsPage(context);
         }
+
         public ProviderApprenticeDetailsPage GoBackToProviderApprenticeDetailsPage()
         {
-            Back();
+            tabHelper.NavigateBrowserBack();
             return new ProviderApprenticeDetailsPage(context);
         }
     }
