d273fcb06bd808b213080f85260dcb7eb383ce87
Najamuddin-Muhammad <najamuddin.muhammad@education.gov.uk>
2024-07-17 18:26:09 +0100

    incorporated changes for 1043

--- src/SFA.DAS.Approvals.UITests/Project/Helpers/StepsHelper/Employer/EmployerCreateCohortStepsHelper.cs
+++ src/SFA.DAS.Approvals.UITests/Project/Helpers/StepsHelper/Employer/EmployerCreateCohortStepsHelper.cs
@@ -1,4 +1,5 @@
-﻿using SFA.DAS.Approvals.UITests.Project.Helpers.StepsHelper.Provider;
+﻿using SFA.DAS.Approvals.UITests.Project.Helpers.DataHelpers;
+using SFA.DAS.Approvals.UITests.Project.Helpers.StepsHelper.Provider;
 using SFA.DAS.Approvals.UITests.Project.Tests.Pages.Employer;
 using SFA.DAS.FrameworkHelpers;
 using System;
@@ -46,8 +47,10 @@ namespace SFA.DAS.Approvals.UITests.Project.Helpers.StepsHelper.Employer
 
         private CohortSentYourTrainingProviderPage EmployerCreateCohort(bool isTransferReceiverEmployer)
         {
-            return _confirmProviderDetailsHelper.ConfirmProviderDetailsAreCorrect(isTransferReceiverEmployer, AddTrainingProviderDetailsFunc())
-               .EmployerSendsToProviderToAddApprentices();
+            return _confirmProviderDetailsHelper
+               .ConfirmProviderDetailsAreCorrect(isTransferReceiverEmployer, AddTrainingProviderDetailsFunc())
+               .EmployerSendsToProviderToAddApprentices()
+               .VerifyMessageForTrainingProvider(context.GetValue<ApprenticeDataHelper>().MessageToProvider);
         }
 
         protected virtual Func<AddAnApprenitcePage, AddTrainingProviderDetailsPage> AddTrainingProviderDetailsFunc() => AddTrainingProviderStepsHelper.AddTrainingProviderDetailsFunc();
