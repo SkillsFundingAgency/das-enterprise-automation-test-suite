257827034d9f74fa08691c7c8001864969f72379
Najam Muhammad <58732568+Najamuddin-Muhammad@users.noreply.github.com>
2024-07-24 20:03:43 +0100

    added logic to handle error page (#1610)

--- src/SFA.DAS.ManagingStandards.UITests/Project/Tests/StepDefinitions/ProviderAddStandardSteps.cs
+++ src/SFA.DAS.ManagingStandards.UITests/Project/Tests/StepDefinitions/ProviderAddStandardSteps.cs
@@ -10,7 +10,7 @@ public class ProviderAddStandardSteps(ScenarioContext context)
     {
         var _providerManageTheStandardsYouDeliverPage = new ManagingStandardsProviderHomePage(context).NavigateToYourStandardsAndTrainingVenuesPage().AccessStandards();
 
-        for (int i = 0; i < 1; i++)
+        for (int i = 0; i < 500; i++)
         {
             _providerManageTheStandardsYouDeliverPage = AddStandard(_providerManageTheStandardsYouDeliverPage);
         }
