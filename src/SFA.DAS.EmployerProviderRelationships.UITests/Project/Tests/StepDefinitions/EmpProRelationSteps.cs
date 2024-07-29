1af6d84860c8c5c17ce5f9ac86d4803ab40c7bc8
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2024-07-29 16:53:45 +0100

    added sample test

---
+++ src/SFA.DAS.EmployerProviderRelationships.UITests/Project/Tests/StepDefinitions/EmpProRelationSteps.cs
@@ -0,0 +1,26 @@
+﻿using SFA.DAS.Login.Service;
+using SFA.DAS.Login.Service.Project.Helpers;
+using SFA.DAS.Registration.UITests.Project.Helpers;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.EmployerProviderRelationships.UITests.Project.Tests.StepDefinitions
+{
+    [Binding]
+    public class EmpProRelationSteps(ScenarioContext context)
+    {
+        private readonly EmployerPortalLoginHelper _employerLoginHelper = new(context);
+
+        [Given(@"Levy employer grant create cohort permission to a provider")]
+        public void LevyEmployerGrantCreateCohortPermissionToAProvider()
+        {
+            var homePage = _employerLoginHelper.Login(context.GetUser<EPRLevyUser>(), true);
+        }
+
+        [Then(@"the provider should be added with the correct permissions")]
+        public void TheProviderShouldBeAddedWithTheCorrectPermissions()
+        {
+
+        }
+
+    }
+}
