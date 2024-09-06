a75773f5047a7667209c0ba53f555b050cb0c8b4
Najamuddin-Muhammad <najamuddin.muhammad@education.gov.uk>
2024-09-06 17:07:26 +0100

    updated step defination for newly added OLTD tests

--- src/SFA.DAS.Approvals.UITests/Project/Tests/StepDefinitions/OverlappingTrainingDateRequestSteps.cs
+++ src/SFA.DAS.Approvals.UITests/Project/Tests/StepDefinitions/OverlappingTrainingDateRequestSteps.cs
@@ -244,8 +244,8 @@ namespace SFA.DAS.Approvals.UITests.Project.Tests.StepDefinitions
             _commitmentsSqlDataHelper.SetCreatedOnForOverlappingTrainingDate(uln, date);
         }
 
-        [Then(@"Automatically stop the record with stopDate = NewStartDate")]
-        public void ThenAutomaticallyStopPreviousApprenticeshipAndSetStopDate()
+        [Then(@"System automatically stops the record with stopDate = NewStartDate")]
+        public void ThenSystemAutomaticallyStopsTheRecordWithStopDateNewStartDate()
         {
             var uln = GetUlnForOLTD();
             var hasBeenAutoStopped = _commitmentsSqlDataHelper.CheckIfPreviousApprenticeshipHasBeenStoppedWithStopDateAsNewStartDate(uln);
