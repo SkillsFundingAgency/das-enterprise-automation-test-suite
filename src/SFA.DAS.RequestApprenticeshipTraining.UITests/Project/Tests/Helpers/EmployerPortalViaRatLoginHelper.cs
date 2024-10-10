1cb31a3fb7fdbff5bd120898996179ef7dae274e
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2024-10-10 09:21:35 +0100

    updated EmployerPortalLoginHelper

--- src/SFA.DAS.RequestApprenticeshipTraining.UITests/Project/Tests/Helpers/EmployerPortalViaRatLoginHelper.cs
+++ src/SFA.DAS.RequestApprenticeshipTraining.UITests/Project/Tests/Helpers/EmployerPortalViaRatLoginHelper.cs
@@ -4,19 +4,17 @@ using SFA.DAS.Registration.UITests.Project.Tests.Pages.StubPages;
 using SFA.DAS.RequestApprenticeshipTraining.UITests.Project.Tests.Pages.RATEmployerPages;
 using TechTalk.SpecFlow;
 
-
 namespace SFA.DAS.RequestApprenticeshipTraining.UITests.Project.Tests.Helpers
 {
     public class EmployerPortalViaRatLoginHelper(ScenarioContext context) : EmployerPortalLoginHelper(context)
     {
         public AskIfTrainingProvidersCanRunThisCoursePage LoginViaRat(RATOwnerUser loginUser)
         {
-            new StubSignInEmployerPage(context).Login(loginUser).Continue();
-
             SetCredentials(loginUser, true);
+
+            new StubSignInEmployerPage(context).Login(loginUser).Continue();
 
             return new(context);
         }
-
     }
 }
