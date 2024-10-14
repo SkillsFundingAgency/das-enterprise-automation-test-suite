183738ff5c70312e2030fedff168ef00b3f12d0f
David Rimar <david.rimar@digital.education.gov.uk>
2024-10-14 15:59:57 +0100

    EC-739 uncommenthelper class

--- src/SFA.DAS.EarlyConnectForms.UITests/Project/Helpers/RetriveEmailOTPCodeHelper.cs
+++ src/SFA.DAS.EarlyConnectForms.UITests/Project/Helpers/RetriveEmailOTPCodeHelper.cs
@@ -1,31 +1,31 @@
-﻿//using Mailosaur;
-//using Mailosaur.Models;
-//using NUnit.Framework.Constraints;
-//using SFA.DAS.FrameworkHelpers;
-//using System;
+﻿using Mailosaur;
+using Mailosaur.Models;
+using NUnit.Framework.Constraints;
+using SFA.DAS.FrameworkHelpers;
+using System;
 
-//namespace SFA.DAS.EarlyConnectForms.UITests.Project.Helpers
-//{
-//    public class RetriveEmailOTPCodeHelper
-//    {
-//        public RetriveEmailOTPCodeHelper(string email)
-//        {
-//            var apiKey = "yvODYD5LfqCcF7hG6hjsS3999aYqC0L5";
-//            var serverId = "szs3qaml";
+namespace SFA.DAS.EarlyConnectForms.UITests.Project.Helpers
+{
+    public class RetriveEmailOTPCodeHelper
+    {
+        public RetriveEmailOTPCodeHelper(string email)
+        {
+            var apiKey = "yvODYD5LfqCcF7hG6hjsS3999aYqC0L5";
+            var serverId = "szs3qaml";
 
-//            var mailosaur = new MailosaurClient(apiKey);
+            var mailosaur = new MailosaurClient(apiKey);
 
-//            var criteria = new SearchCriteria()
-//            {
-//                Subject = "Confirm your email address – Department for education"
-               
-//            };
-//            var emailMessage = mailosaur.Messages.Get(serverId, criteria);
-//            Console.WriteLine(emailMessage.Html.Codes[0].Value);
-//            AuthCodes = emailMessage.Html.Codes[0].Value;
-//        }
+            var criteria = new SearchCriteria()
+            {
+                Subject = "Confirm your email address – Department for education"
 
-//        public string AuthCodes { get; set; }
+            };
+            var emailMessage = mailosaur.Messages.Get(serverId, criteria);
+            Console.WriteLine(emailMessage.Html.Codes[0].Value);
+            AuthCodes = emailMessage.Html.Codes[0].Value;
+        }
 
-//    }
-//}
+        public string AuthCodes { get; set; }
+
+    }
+}
