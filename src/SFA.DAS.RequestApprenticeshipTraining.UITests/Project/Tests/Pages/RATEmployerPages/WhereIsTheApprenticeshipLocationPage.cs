e12a7b6d74212841cc18e3aec18924423b47d8b9
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2024-10-16 14:54:59 +0100

    updated folder structure

--- src/SFA.DAS.RequestApprenticeshipTraining.UITests/Project/Tests/Pages/RATEmployerPages/WhereIsTheApprenticeshipLocationPage.cs
+++ src/SFA.DAS.RequestApprenticeshipTraining.UITests/Project/Tests/Pages/RATEmployerPages/WhereIsTheApprenticeshipLocationPage.cs
@@ -3,68 +3,67 @@ using SFA.DAS.FrameworkHelpers;
 using SFA.DAS.UI.Framework.TestSupport;
 using TechTalk.SpecFlow;
 
-namespace SFA.DAS.RequestApprenticeshipTraining.UITests.Project.Tests.Pages.RATEmployerPages
+namespace SFA.DAS.RequestApprenticeshipTraining.UITests.Project.Tests.Pages.RATEmployerPages;
+
+public class WhereIsTheApprenticeshipLocationPage : RatProjectBasePage
 {
-    public class WhereIsTheApprenticeshipLocationPage : RatProjectBasePage
+    protected override By PageHeader => dynamicPageHeader;
+
+    private readonly By dynamicPageHeader;
+
+    public WhereIsTheApprenticeshipLocationPage(ScenarioContext context, bool IsSingleLocation) : base(context, false)
     {
-        protected override By PageHeader => dynamicPageHeader;
+        dynamicPageHeader = IsSingleLocation ? PageHeaderForSingleLocation : PageHeaderForMultipleLocation;
 
-        private readonly By dynamicPageHeader;
-
-        public WhereIsTheApprenticeshipLocationPage(ScenarioContext context, bool IsSingleLocation) : base(context, false)
-        {
-            dynamicPageHeader = IsSingleLocation ? PageHeaderForSingleLocation : PageHeaderForMultipleLocation;
-
-            VerifyPage();
-        }
-
-        protected override string PageTitle => "Where is the apprenticeship location?";
-
-        private static By PageHeaderForMultipleLocation => By.CssSelector(".govuk-fieldset__heading");
-
-        private static By PageHeaderForSingleLocation => By.CssSelector("label[for='SingleLocation']");
-
-        #region Locators
-        private static By MultipleLocation => By.CssSelector("label.govuk-checkboxes__label");
-        #endregion
-
-        public SelectTrainingOptionsPage GoToTrainingOptionsPage(bool enterLocation)
-        {
-            if (enterLocation) new ApprenticeshipLocationAutoCompleteHelper(context).SelectFromAutoCompleteList(RandomDataGenerator.RandomTown());
-
-            Continue();
-
-            return new SelectTrainingOptionsPage(context);
-        }
-
-        public SelectTrainingOptionsPage ChooseRegion()
-        {
-            var listOfRegions = pageInteractionHelper.FindElements(MultipleLocation);
-
-            var firstRegion = RandomDataGenerator.GetRandomElementFromListOfElements(listOfRegions);
-
-            listOfRegions.Remove(firstRegion);
-
-            var secondRegion = RandomDataGenerator.GetRandomElementFromListOfElements(listOfRegions);
-
-            formCompletionHelper.ClickElement(firstRegion);
-
-            formCompletionHelper.ClickElement(secondRegion);
-
-            Continue();
-
-            return new SelectTrainingOptionsPage(context);
-        }
+        VerifyPage();
     }
 
-    public class ApprenticeshipLocationAutoCompleteHelper(ScenarioContext context) : AutoCompleteHelper(context)
+    protected override string PageTitle => "Where is the apprenticeship location?";
+
+    private static By PageHeaderForMultipleLocation => By.CssSelector(".govuk-fieldset__heading");
+
+    private static By PageHeaderForSingleLocation => By.CssSelector("label[for='SingleLocation']");
+
+    #region Locators
+    private static By MultipleLocation => By.CssSelector("label.govuk-checkboxes__label");
+    #endregion
+
+    public SelectTrainingOptionsPage GoToTrainingOptionsPage(bool enterLocation)
     {
-        protected override string SearchPage => "Apprenticeship Location";
+        if (enterLocation) new ApprenticeshipLocationAutoCompleteHelper(context).SelectFromAutoCompleteList(RandomDataGenerator.RandomTown());
 
-        protected override By SearchTextInput => By.CssSelector("input[id='SingleLocation']");
+        Continue();
 
-        protected override By AutoCompleteMenu => By.CssSelector("[id='SingleLocation__listbox']");
-
-        protected override By NthOption(int i) => By.CssSelector($"[id='SingleLocation__option--{i}']");
+        return new SelectTrainingOptionsPage(context);
     }
+
+    public SelectTrainingOptionsPage ChooseRegion()
+    {
+        var listOfRegions = pageInteractionHelper.FindElements(MultipleLocation);
+
+        var firstRegion = RandomDataGenerator.GetRandomElementFromListOfElements(listOfRegions);
+
+        listOfRegions.Remove(firstRegion);
+
+        var secondRegion = RandomDataGenerator.GetRandomElementFromListOfElements(listOfRegions);
+
+        formCompletionHelper.ClickElement(firstRegion);
+
+        formCompletionHelper.ClickElement(secondRegion);
+
+        Continue();
+
+        return new SelectTrainingOptionsPage(context);
+    }
+}
+
+public class ApprenticeshipLocationAutoCompleteHelper(ScenarioContext context) : AutoCompleteHelper(context)
+{
+    protected override string SearchPage => "Apprenticeship Location";
+
+    protected override By SearchTextInput => By.CssSelector("input[id='SingleLocation']");
+
+    protected override By AutoCompleteMenu => By.CssSelector("[id='SingleLocation__listbox']");
+
+    protected override By NthOption(int i) => By.CssSelector($"[id='SingleLocation__option--{i}']");
 }
--- src/SFA.DAS.RequestApprenticeshipTraining.UITests/Project/Tests/StepDefinitions/RATEmployerSteps.cs
+++ src/SFA.DAS.RequestApprenticeshipTraining.UITests/Project/Tests/StepDefinitions/RATEmployerSteps.cs
@@ -9,67 +9,66 @@ using SFA.DAS.RequestApprenticeshipTraining.UITests.Project.Tests.Pages.RATEmplo
 using SFA.DAS.UI.Framework.TestSupport;
 using TechTalk.SpecFlow;
 
-namespace SFA.DAS.RequestApprenticeshipTraining.UITests.Project.Tests.StepDefinitions
+namespace SFA.DAS.RequestApprenticeshipTraining.UITests.Project.Tests.StepDefinitions;
+
+[Binding]
+public class RATEmployerSteps(ScenarioContext context)
 {
-    [Binding]
-    public class RATEmployerSteps(ScenarioContext context)
+    private readonly FATStepsHelper _fATV2StepsHelper = new(context);
+
+    private AskIfTrainingProvidersCanRunThisCoursePage landingPage;
+
+    private WeHaveSharedThisWithTrainingProvidersPage submitConfirmationPage;
+
+    [Given(@"an employer requests apprenticeship training")]
+    public void AnEmployerRequestsApprenticeshipTraining() => RequestTrainingProvider(false);
+
+    [Given(@"an employer adds location to requests apprenticeship training")]
+    public void AnEmployerAddsLocationToRequestsApprenticeshipTraining() => RequestTrainingProvider(true);
+
+    [When(@"the employer logs in to rat employer account")]
+    public void WhenTheEmployerLogsInToRatEmployerAccount() => LoginViaRat(context.GetUser<RatEmployerUser>());
+
+    [When(@"the employer logs in to rat cancel employer account")]
+    public void TheEmployerLogsInToRatCancelEmployerAccount() => LoginViaRat(context.GetUser<RatCancelEmployerUser>());
+
+
+    [Then(@"the employer submits the request for single location")]
+    public void TheEmployerSubmitsTheRequestForSingleLocation()
     {
-        private readonly FATStepsHelper _fATV2StepsHelper = new(context);
-
-        private AskIfTrainingProvidersCanRunThisCoursePage landingPage;
-
-        private WeHaveveSharedThisWithTrainingProvidersPage submitConfirmationPage;
-
-        [Given(@"an employer requests apprenticeship training")]
-        public void AnEmployerRequestsApprenticeshipTraining() => RequestTrainingProvider(false);
-
-        [Given(@"an employer adds location to requests apprenticeship training")]
-        public void AnEmployerAddsLocationToRequestsApprenticeshipTraining() => RequestTrainingProvider(true);
-
-        [When(@"the employer logs in to rat employer account")]
-        public void WhenTheEmployerLogsInToRatEmployerAccount() => LoginViaRat(context.GetUser<RatEmployerUser>());
-
-        [When(@"the employer logs in to rat cancel employer account")]
-        public void TheEmployerLogsInToRatCancelEmployerAccount() => LoginViaRat(context.GetUser<RatCancelEmployerUser>());
-
-
-        [Then(@"the employer submits the request for single location")]
-        public void TheEmployerSubmitsTheRequestForSingleLocation()
-        {
-            SubmitAnswers(landingPage.ClickStarNow().EnterMoreThan1Apprentices().ClickYesForASingleLocation().GoToTrainingOptionsPage(true));
-        }
-
-        [Then(@"the employer submits the request for multiple location")]
-        public void TheEmployerSubmitsTheRequest()
-        {
-            SubmitAnswers(landingPage.ClickStarNow().EnterMoreThan1Apprentices().ClickNoForAMultipleLocation().ChooseRegion());
-        }
-
-        [Then(@"the employer submits the request for one apprentice")]
-        public void TheEmployerSubmitsTheRequestForOneApprentice()
-        {
-            SubmitAnswers(landingPage.ClickStarNow().Enter1Apprentices().GoToTrainingOptionsPage(false));
-        }
-
-        [Then(@"the employer can cancel the request")]
-        public void TheEmployerCanCancelTheRequest()
-        {
-            submitConfirmationPage.ReturnToRequestPage().SelectActiveRequest().CancelYourRequest().SubmitCancelRequest();
-        }
-
-        private void LoginViaRat(RatEmployerBaseUser loginUser) => landingPage = new EmployerPortalViaRatLoginHelper(context).LoginViaRat(loginUser);
-
-        private void SubmitAnswers(SelectTrainingOptionsPage selectTrainingOptionsPage) => submitConfirmationPage = selectTrainingOptionsPage.SelectTrainingOptions().SubmitAnswers();
-
-
-        private void RequestTrainingProvider(bool filterLocation)
-        {
-            var courseTitles = context.Get<RoatpV2SqlDataHelper>().GetCourseTitlesthatProviderDeosNotOffer(context.GetProviderConfig<ProviderConfig>()?.Ukprn);
-
-            var randomCourse = RandomDataGenerator.GetRandomElementFromListOfElements(courseTitles);
-
-            _fATV2StepsHelper.SearchForTrainingCourse(randomCourse).SelectFirstTrainingResult().ViewProvidersForThisCourse(filterLocation, string.Empty).RequestTrainingProvider();
-        }
-
+        SubmitAnswers(landingPage.ClickStarNow().EnterMoreThan1Apprentices().ClickYesForASingleLocation().GoToTrainingOptionsPage(true));
     }
+
+    [Then(@"the employer submits the request for multiple location")]
+    public void TheEmployerSubmitsTheRequest()
+    {
+        SubmitAnswers(landingPage.ClickStarNow().EnterMoreThan1Apprentices().ClickNoForAMultipleLocation().ChooseRegion());
+    }
+
+    [Then(@"the employer submits the request for one apprentice")]
+    public void TheEmployerSubmitsTheRequestForOneApprentice()
+    {
+        SubmitAnswers(landingPage.ClickStarNow().Enter1Apprentices().GoToTrainingOptionsPage(false));
+    }
+
+    [Then(@"the employer can cancel the request")]
+    public void TheEmployerCanCancelTheRequest()
+    {
+        submitConfirmationPage.ReturnToRequestPage().SelectActiveRequest().CancelYourRequest().SubmitCancelRequest();
+    }
+
+    private void LoginViaRat(RatEmployerBaseUser loginUser) => landingPage = new EmployerPortalViaRatLoginHelper(context).LoginViaRat(loginUser);
+
+    private void SubmitAnswers(SelectTrainingOptionsPage selectTrainingOptionsPage) => submitConfirmationPage = selectTrainingOptionsPage.SelectTrainingOptions().SubmitAnswers();
+
+
+    private void RequestTrainingProvider(bool filterLocation)
+    {
+        var courseTitles = context.Get<RoatpV2SqlDataHelper>().GetCourseTitlesthatProviderDeosNotOffer(context.GetProviderConfig<ProviderConfig>()?.Ukprn);
+
+        var randomCourse = RandomDataGenerator.GetRandomElementFromListOfElements(courseTitles);
+
+        _fATV2StepsHelper.SearchForTrainingCourse(randomCourse).SelectFirstTrainingResult().ViewProvidersForThisCourse(filterLocation, string.Empty).RequestTrainingProvider();
+    }
+
 }
