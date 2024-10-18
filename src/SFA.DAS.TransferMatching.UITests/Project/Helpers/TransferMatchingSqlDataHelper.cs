d56ebc457af099ecece1ccb8e2d2bcf9541d6130
Will O'Grady <32494943+ogrwill@users.noreply.github.com>
2024-10-18 14:41:02 +0100

    Appman 1207 ltm auto reject and close (#1675)
    * TM_32 auto reject after 3 months.
    Other UI content changes
    
    * TM_33_AutoClose the Pledge When Balance Drops Below 2000
    
    * revert environment
    
    * correcting application status selector
    
    * adding ApplicationsWithAutomaticRejection_Code to config
    
    * updating tm function variable names
    
    * updating config name
    
    ---------
    
    Co-authored-by: ogrwill <will.ogrady@digital.education.gov.uk>

--- src/SFA.DAS.TransferMatching.UITests/Project/Helpers/TransferMatchingSqlDataHelper.cs
+++ src/SFA.DAS.TransferMatching.UITests/Project/Helpers/TransferMatchingSqlDataHelper.cs
@@ -1,7 +1,8 @@
-﻿using SFA.DAS.ConfigurationBuilder;
-using SFA.DAS.FrameworkHelpers;
+﻿using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using SFA.DAS.ConfigurationBuilder;
+using SFA.DAS.FrameworkHelpers;
 
 namespace SFA.DAS.TransferMatching.UITests.Project.Helpers
 {
@@ -31,6 +32,57 @@ namespace SFA.DAS.TransferMatching.UITests.Project.Helpers
 
             ExecuteSqlCommand(sqlQueryFromFile);
         }
+
+        public void UpdateCreatedDateForApplicationTo3MonthsAgo(string details)
+        {
+            string sqlQueryFromFile = FileHelper.GetSql("TMUpdateApplicationCreatedDate3MonthsAgo");
+
+            sqlQueryFromFile = MyRegex3().Replace(sqlQueryFromFile, details);
+
+            ExecuteSqlCommand(sqlQueryFromFile);
+        }
+
+        public (int status, int remainingAmount) GetPledgeDetails(string details)
+        {
+            waitForResults = true;
+
+            Dictionary<string, string> sqlParameters = new()
+            {
+                { "@DETAILS", details }
+            };
+
+            string query = @"SELECT 
+                            TOP (1) pl.[Status], pl.[RemainingAmount]
+                                FROM
+                            [dbo].[Application] app   
+                            inner join [dbo].[Pledge] pl
+                            on pl.Id = app.PledgeId                    
+                            Where app.Details =  @DETAILS;";
+
+            var data = GetData(query, sqlParameters);
+
+
+            return (Convert.ToInt32(data[0]), Convert.ToInt32(data[1]));
+        }
+
+        public int GetPledgeApplicationStatusByDetails(string details)
+        {
+            waitForResults = true;
+
+            Dictionary<string, string> sqlParameters = new()
+            {
+                { "@DETAILS", details }
+            };
+
+            string query = @"SELECT TOP (1) Status
+                            FROM
+                           [dbo].[Application]                       
+                         Where Details = @DETAILS;";
+
+            var data = GetData(query, sqlParameters);
+
+            return Convert.ToInt32(data[0]);
+        }              
 
         [GeneratedRegex(@"__EmployerAccountId__")]
         private static partial Regex MyRegex();
