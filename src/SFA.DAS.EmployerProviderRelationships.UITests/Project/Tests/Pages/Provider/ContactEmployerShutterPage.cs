036b8dafcd2501756b9e1de8bd16346b734a465b
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2024-10-25 14:12:25 +0100

    minor code review changes

---
+++ src/SFA.DAS.EmployerProviderRelationships.UITests/Project/Tests/Pages/Provider/ContactEmployerShutterPage.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.EmployerProviderRelationships.UITests.Project.Tests.Pages.Provider
+{
+    public class ContactEmployerShutterPage : ProviderRelationshipsBasePage
+    {
+        private static By GovBody => By.CssSelector(".govuk-body");
+
+        protected override string PageTitle => "Contact the employer";
+
+
+        public ContactEmployerShutterPage(ScenarioContext context) : base(context)
+        {
+            VerifyPage(GovBody, "You cannot add this employer because there are multiple organisations linked to their details.");
+        }
+    }
+}
