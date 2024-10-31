1e1b9ba5e30616808559d34a559f4da82016667d
Chris Foster <chrisfoster186@googlemail.com>
2024-10-31 15:30:15 +0000

    Ec 461 provider feedback UI tests (#1667)
    * Initial WIP of provider login to view feedback
    
    * Apprentice feedback on provider wip
    
    * Tidy up
    
    * Improvements
    
    * Small tweaks
    
    * Tidy up
    
    * Adds Employer feedback test
    
    * Completes fleshing out employer test
    
    * Little tweaks to test cases so that they test the calcs nicely
    
    * Adds additional tests
    
    * Update PF_P_01.feature
    
    Added project tags
    
    * Update appsettings.Project.json
    
    Updated config
    
    * Reworking sql for performance
    
    * Performance improvements
    
    * Additional data cleanup
    
    * employer fb previous yr
    
    Added comment for employer fb previous yr consolidation
    
    ---------
    
    Co-authored-by: Gabriel PT <98819464+GPeterThomas@users.noreply.github.com>

---
+++ src/SFA.DAS.ProvideFeedback.UITests/Project/Tests/Pages/Provider/FeedbackOverviewPage.cs
@@ -0,0 +1,31 @@
+﻿namespace SFA.DAS.ProvideFeedback.UITests.Project.Tests.Pages.Provider;
+
+public class FeedbackOverviewPage(ScenarioContext context, bool navigate = false) : ProviderFeedbackBasePage(context, true)
+{
+    public void VerifyApprenticeFeedbackRating(string period, string expectedRating)
+    {
+        pageInteractionHelper.VerifyText(By.Id($"apprentice-rating-description-{period}"), expectedRating);
+    }
+
+    protected override string PageTitle => "Your feedback";
+
+    public void SelectApprenticeTabForAcademicYear(string academicYear)
+    {
+        formCompletionHelper.Click(By.Id($"tab_app-{academicYear}"));
+    }
+
+    public void VerifyEmployerFeedbackRating(string period, string expectedRating)
+    {
+        pageInteractionHelper.VerifyText(By.Id($"employer-rating-description-{period}"), expectedRating);
+    }
+
+    public void SelectEmployerTabForAcademicYear(string academicYear)
+    {
+        formCompletionHelper.Click(By.Id($"tab_emp-{academicYear}"));
+    }
+
+    public void VerifyText(string expectedText)
+    {
+        pageInteractionHelper.IsElementDisplayed(By.XPath($"//*[text()='{expectedText}']"));
+    }
+}
