ed436b7323c778bfe526877a076f69f859343e7d
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2024-11-08 14:46:45 +0000

    Employer provider relationship : ProviderRequestsEmployerDeclinesCreateAccount (#1707)
    * updated tpr data helper
    
    * added EPR_06_ProviderRequestsEmployerDeclinesCreateAccount scenario
    
    * updated OpenEmpInviteFromProvider
    
    * updated VerifyPendingRequest

---
+++ src/SFA.DAS.EmployerProviderRelationships.UITests/Project/Tests/StepDefinitions/ProviderCreateAccountSteps.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace SFA.DAS.EmployerProviderRelationships.UITests.Project.Tests.StepDefinitions;
+
+[Binding]
+public class ProviderCreateAccountSteps(ScenarioContext context) : EmpProRelationBaseSteps(context)
+{
+    [Given(@"a provider requests employer to create account with all permission")]
+    public void AProviderRequestsEmployerToCreateAccountWithAllPermission()
+    {
+        GoToProviderRelationsHomePage();
+
+        eprDataHelper.EmployerEmail = objectContext.GetRegisteredEmail();
+
+        GoToEmailAccountNotFoundPage().ContinueToInvite().SubmitEmployerName().SendInvitation().GoToViewEmployersPage().VerifyPendingRequest();
+    }
+
+    [Then(@"the employer declines the create account request")]
+    public void TheEmployerDeclinesTheCreateAccountRequest()
+    {
+        OpenEmpInviteFromProvider();
+
+        new StubSignInEmployerPage(context).Register().Continue();
+
+        new CreateYourApprenticeshipServiceAccount(context).DoNotCreateAccount().ConfirmDoNotCreateAccount();
+    }
+}
