eb7fdd9b18f25d6dfbde884213a305ccd2d54e84
Kousik Vedam <146717280+kousikvedam@users.noreply.github.com>
2024-12-02 11:42:32 +0000

    Commit!

---
+++ src/SFA.DAS.ApprenticeApp.UITests/Project/Helpers/AppLogin.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using SFA.DAS.UI.Framework.TestSupport;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.ApprenticeApp.UITests.Project.Helpers
+{
+    public class AppLogin : Steps
+    {
+        protected By PageHeader => By.CssSelector("svg.govuk-header__logotype");
+
+        protected By SignIn => By.CssSelector(".app-button[onclick*='/Account/Authenticated']");
+
+        public AppLogin(ScenarioContext context)
+        {
+            
+        }
+
+        protected void Login()
+        {
+           
+        }
+    }
+}
---
+++ src/SFA.DAS.ApprenticeApp.UITests/Project/Tests/Pages/Tasks.cs
@@ -0,0 +1,63 @@
+﻿using OpenQA.Selenium;
+
+namespace SFA.DAS.ApprenticeApp.UITests.Project.Tests.Pages
+{
+    public class Tasks
+    {
+        protected static By YourTasks => By.CssSelector("h1.govuk-heading-xl.govuk-!-margin-bottom-2");
+
+        protected static By YearDropdown => By.CssSelector("button.app-dropdown__toggle[aria-expanded='false']");
+
+        protected static By Todo => By.CssSelector("a.app-tabs__tab.todo[role='tab'][aria-selected='true']");
+
+        protected static By Done => By.CssSelector("a.app-tabs__tab.done[role='tab'][aria-selected='true']");
+
+        protected static By SortByDropdown => By.CssSelector("span.app-dropdown__toggle-sort-value#sortby");
+
+        protected static By FiltersDropdown => By.CssSelector("a[href='#filter'][data-module='app-overlay'].app-icon-action");
+
+        private readonly IWebDriver _driver;
+
+        public Tasks(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        //protected void EnterText(By locator, string text)
+        //{
+        //    var element = _driver.FindElement(locator);
+        //    element.Clear();
+        //    element.SendKeys(text);
+        //}
+
+        public void EnterDate(string date)
+        {
+            _driver.FindElement(By.Id("date")).SendKeys(date);
+        }
+
+        public void EnterTime(string time)
+        {
+            _driver.FindElement(By.Id("time")).SendKeys(time);
+        }
+
+        public void SelectCategory(string categoryValue)
+        {
+            var categoryInput = _driver.FindElement(By.Id($"category_{categoryValue}"));
+            categoryInput.Click();
+        }
+        protected void ToDoAddATask()
+        {
+            // TBD
+        }
+
+        protected void DeleteTask()
+        {
+            // TBD
+        }
+
+        protected void EditTask()
+        {
+            // TBD
+        }
+    }
+}
