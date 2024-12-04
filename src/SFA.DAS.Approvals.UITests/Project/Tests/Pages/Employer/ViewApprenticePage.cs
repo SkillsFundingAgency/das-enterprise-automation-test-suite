8343efdf931bb2d75a79e2a01eb9aa1b907534aa
sony-kotha <sony.kotha@education.gov.uk>
2024-12-04 17:59:35 +0000

    UpdatePageHeaders-6

--- src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Employer/ViewApprenticePage.cs
+++ src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Employer/ViewApprenticePage.cs
@@ -5,7 +5,7 @@ namespace SFA.DAS.Approvals.UITests.Project.Tests.Pages.Employer
 {
     public class ViewApprenticePage(ScenarioContext context) : ApprovalsBasePage(context)
     {
-        protected override By PageHeader => By.CssSelector(".govuk-heading-xl");
+        protected override By PageHeader => By.CssSelector(".govuk-heading-xl, .govuk-heading-l");
         protected override string PageTitle => "View apprentice details";
 
         private static By ViewApprenticeLink => By.CssSelector("a.govuk-link.edit-apprentice");
