afe27801dc44f8fb3da0e520c7b504ec516a446d
David Rimar <david.rimar@digital.education.gov.uk>
2024-12-04 16:46:54 +0000

    EC-831 modify changed id on school field

--- src/SFA.DAS.EarlyConnectForms.UITests/Project/Tests/Pages/SchoolCollegePage.cs
+++ src/SFA.DAS.EarlyConnectForms.UITests/Project/Tests/Pages/SchoolCollegePage.cs
@@ -6,7 +6,7 @@ namespace SFA.DAS.EarlyConnectForms.UITests.Project.Tests.Pages
     public class SchoolCollegePage(ScenarioContext context) : EarlyConnectBasePage(context)
     {
         protected override string PageTitle => "What is the name of your school or college?";
-        private static By SchoolCollegeField => By.CssSelector("#schoolname");
+        private static By SchoolCollegeField => By.CssSelector("#schoolsearchterm");
         protected override By ContinueButton => By.CssSelector("button[type='submit']");
         private static By BackButton => By.CssSelector(".govuk-back-link");
         public ApprenticeshipsLevelPage EnterValidSchoolOrCollegeName()
