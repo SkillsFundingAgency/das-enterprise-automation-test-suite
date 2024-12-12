8f5f6c98fbd7843be0fdace8b58136f00a6f35bf
David Rimar <david.rimar@digital.education.gov.uk>
2024-12-12 15:53:50 +0000

    EC-832 switch to gateway url again

--- src/SFA.DAS.ProviderFeedback.APITests/Project/Outer_ProviderFeedbackApiClient.cs
+++ src/SFA.DAS.ProviderFeedback.APITests/Project/Outer_ProviderFeedbackApiClient.cs
@@ -9,8 +9,8 @@ namespace SFA.DAS.ProviderFeedback.APITests.Project
 {
     public class Outer_ProviderFeedbackApiClient(ObjectContext objectContext, Outer_ApiAuthTokenConfig config) : Outer_BaseApiRestClient(objectContext, config.Apim_SubscriptionKey)
     {
-        protected override string ApiName => "/providerFeedback";
-        protected override string ApiBaseUrl => UrlConfig.OuterApiUrlConfig.Outer_ProviderFeedbackApiBaseUrl;
-        public void GetProviderFeedback(long ukprn, HttpStatusCode expectedResponse) => Execute(Method.Get, $"/{ukprn}", string.Empty, expectedResponse);
+        protected override string ApiName => "/providerfeedback";
+        protected override string ApiBaseUrl => UrlConfig.OuterApiUrlConfig.Outer_ApiBaseUrl;
+        public void GetProviderFeedback(long ukprn, HttpStatusCode expectedResponse) => Execute(Method.Get, $"/providerFeedback/{ukprn}", string.Empty, expectedResponse);
     }
 }
