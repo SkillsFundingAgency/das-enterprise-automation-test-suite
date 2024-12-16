d3361c4eb57e93ce755dec13fb1b9f31027c8de3
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2024-12-16 12:38:28 +0000

    removed SFA.DAS.AggregatedEmployerDemand.UITests

--- src/SFA.DAS.AggregatedEmployerDemand.UITests/Project/Tests/Pages/ProviderPages/WeveSharedYourContactDetailsWithEmployersPage.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SFA.DAS.AggregatedEmployerDemand.UITests.Project.Tests.Pages.ProviderPages;
-
-public class WeveSharedYourContactDetailsWithEmployersPage(ScenarioContext context) : AedBasePage(context)
-{
-    protected override string PageTitle => "We’ve shared your contact details with employers";
-
-    protected override By PageHeader => By.ClassName("govuk-panel__title");
-}
