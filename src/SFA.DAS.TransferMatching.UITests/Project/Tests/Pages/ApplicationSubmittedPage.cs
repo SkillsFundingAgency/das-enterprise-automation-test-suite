e5973a1984603c1c67953c7200b83c597b88833b
sony-kotha <82891925+sony-kotha@users.noreply.github.com>
2024-12-16 09:58:21 +0000

    Appman 1188 changes for application confirmation page (#1745)
    * ChangesForApplicationSubmitted
    
    * Update ApplicationSubmittedPage.cs

--- src/SFA.DAS.TransferMatching.UITests/Project/Tests/Pages/ApplicationSubmittedPage.cs
+++ src/SFA.DAS.TransferMatching.UITests/Project/Tests/Pages/ApplicationSubmittedPage.cs
@@ -6,13 +6,13 @@ namespace SFA.DAS.TransferMatching.UITests.Project.Tests.Pages
 {
     public class ApplicationSubmittedPage(ScenarioContext context) : TransferMatchingBasePage(context)
     {
-        protected override string PageTitle => "Your application has been submitted";
+        protected override string PageTitle => "Application submitted";
 
         protected override By PageHeader => PanelTitle;
 
         public HomePage ContinueToMyAccount()
         {
-            formCompletionHelper.ClickLinkByText("Continue to my account");
+            formCompletionHelper.ClickLinkByText("Go to my account");
             return new HomePage(context);
         }
     }
