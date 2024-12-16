cd65d568f95124d9c661883887b1ec3071a59631
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2024-12-16 12:37:02 +0000

    removed SFA.DAS.EmployerIncentives.PaymentProcessTests

--- src/SFA.DAS.EmployerIncentives.PaymentProcessTests/Models/PeriodDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SFA.DAS.EmployerIncentives.PaymentProcessTests.Models
-{
-    public class PeriodDto
-    {
-        public byte Period { get; set; }
-        public long? ApprenticeshipId { get; set; }
-        public bool IsPayable { get; set; }
-    }
-}
--- src/SFA.DAS.EmployerIncentives.PaymentProcessTests/cmd/clear_learningtransport_folder.bat
+++
@@ -1,2 +0,0 @@
-del /q "C:\temp\learningtransport\*"
-FOR /D %%p IN ("C:\temp\learningtransport\*.*") DO rmdir "%%p" /s /q
