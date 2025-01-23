a72302a0a030f23cd7bcbab96820db9b3d6f7d82
Kwame <kwameanim@outlook.com>
2025-01-23 17:28:57 +0000

    updated webelement

--- src/SFA.DAS.ApprenticeAmbassadorNetwork.UITests/Project/Tests/Pages/Employer/RegistrationComplete_EmployerPage.cs
+++ src/SFA.DAS.ApprenticeAmbassadorNetwork.UITests/Project/Tests/Pages/Employer/RegistrationComplete_EmployerPage.cs
@@ -3,7 +3,7 @@
 public class RegistrationComplete_EmployerPage(ScenarioContext context) : AanBasePage(context)
 {
     protected override string PageTitle => "You have joined the Apprenticeship Ambassador Network";
-    private static By GoToOnlinePortal => By.CssSelector("#main-content > div > div > p:nth-child(9) > a");
+    private static By GoToOnlinePortal => By.LinkText("Go to the online portal");
 
     public Employer_NetworkHubPage ContinueToAmbassadorHub()
     {
