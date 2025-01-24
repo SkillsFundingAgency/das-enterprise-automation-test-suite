c901a70c2c535b797108009fd22642ad6dff925b
Kwame <kwameanim@outlook.com>
2025-01-24 17:30:24 +0000

    update school search to college for all regions to widen search to auto-populate

--- src/SFA.DAS.EarlyConnectForms.UITests/Project/Helpers/EarlyConnectSqlHelper.cs
+++ src/SFA.DAS.EarlyConnectForms.UITests/Project/Helpers/EarlyConnectSqlHelper.cs
@@ -12,7 +12,8 @@ public class EarlyConnectSqlHelper(ObjectContext objectContext, DbConfig config)
 
         //var names = GetListOfData(query).Select(x => (string)x[0]).ToList();
 
-        List<string> names = ["Bishop ", "Cramlington ", "North East Futures ", "Haydon Bridge High School", "East Durham College"];
+        //List<string> names = ["Bishop ", "Cramlington ", "North East Futures ", "Haydon Bridge High School", "East Durham College"];
+        List<string> names = ["College"];
 
         var name = RandomDataGenerator.GetRandomElementFromListOfElements(names);
 
