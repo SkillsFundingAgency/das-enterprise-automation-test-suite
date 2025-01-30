46c667ef1cfdfb54134dcb0871ab2cdbdb2f4c32
Kwame <kwameanim@outlook.com>
2025-01-30 16:59:14 +0000

    tidy up stepshelper class

--- src/SFA.DAS.EarlyConnectForms.UITests/Project/Helpers/EarlyConnectStepsHelper.cs
+++ src/SFA.DAS.EarlyConnectForms.UITests/Project/Helpers/EarlyConnectStepsHelper.cs
@@ -6,94 +6,23 @@ namespace SFA.DAS.EarlyConnectForms.UITests.Project.Helpers;
 
 public class EarlyConnectStepsHelper(ScenarioContext context)
 {
-    public EarlyConnectHomePage GoToEarlyConnectHomePage()
-    {
-        return new EarlyConnectHomePage(context).AcceptCookieAndAlert();
-    }
-
-    public EarlyConnectHomePage GoToEarlyConnectNorthEastAdvisorPage()
-    {
-        return new EarlyConnectHomePage(context).SelectNorthEast();
-    }
-
-    public EarlyConnectHomePage GoToEarlyConnectLancashireAdvisorPage()
-    {
-        return new EarlyConnectHomePage(context).SelectLancashire();
-    }
-
-    public EarlyConnectHomePage GoToEarlyConnectLondonAdvisorPage()
-    {
-        return new EarlyConnectHomePage(context).SelectLondon();
-    }
-
-    public GetApprenticeshipAdviserPage GoToEarlyConnectEmailPage()
-    {
-        return new GetApprenticeshipAdviserPage(context).GoToEmailAddressPage();
-    }
-
-    public EmailAuthCodePage GoToAddUniqueEmailAddressPage()
-    {
-        return  new EmailAddressPage(context).EnterNewEmailAddress();
-    }
-    public WhatsYourNamePage GoToCheckEmailAuthCodePage()
-    {
-        return new EmailAuthCodePage(context).EnterValidAuthCode();
-    }
-
-    public DateOfBirthPage GoToWhatYourNamePage()
-    {
-        return new WhatsYourNamePage(context).EnterFirstAndLastNames();
-    }
-
-    public PostcodePage GoToWhatIsYourDateOfBirthPage()
-    {
-        return new DateOfBirthPage(context).EnterValidDateOfBirth();
-    }
-
-    public TelephonePage GoToPostCodePage()
-    {
-        return new PostcodePage(context).EnterValidPostcode();
-    }
-
-    public AreasOfWorkInterestPage GoToWhatYourTelephonePage()
-    {
-        return new TelephonePage(context).EnterValidTelephoneNumber();
-    }
-
-    public SchoolCollegePage GoToAreasOfWorkInterestPage()
-    {
-        return new AreasOfWorkInterestPage(context).SelectAnyAreaInterestToYou();
-    }
-
-    public ApprenticeshipsLevelPage GoToEnterNameOfSchoolCollegePage()
-    {
-        return new SchoolCollegePage(context).EnterInvalidSchoolOrCollegeName();
-    }
-
-    public ApprenticeshipsLevelPage GoToNameOfSchoolCollegePage()
-    {
-        return new SchoolCollegePage(context).SearchValidSchoolOrCollegeName();
-    }
-    public HaveYouAppliedPage GoToApprencticeshipLevelPage()
-    {
-        return new ApprenticeshipsLevelPage(context).SelectAnyApprenticeshipLevelInterestToYou();
-    }
-
-    public AreaOfEnglandPage GoToHaveYouAppliedPage()
-    {
-        return new HaveYouAppliedPage(context).SelectAnyPastApplications();
-    }
-
-    public SupportPage GoToAreaOfEnglandPage()
-    {
-        return new AreaOfEnglandPage(context).SelectYesForTheRightApprenticeship();
-    }
-    public CheckYourAnswerPage GoToSupportPage()
-    {
-        return new SupportPage(context).SelectAnySupportThatAppliesToYou();
-    }
-    public ApplicantSurveySummitedPage GoToCheckYourAnswerPage()
-    {
-        return new CheckYourAnswerPage(context).AcceptAndSubmitForm();
-    }
+    public EarlyConnectHomePage GoToEarlyConnectHomePage() => new EarlyConnectHomePage(context).AcceptCookieAndAlert();
+    public EarlyConnectHomePage GoToEarlyConnectNorthEastAdvisorPage() => new EarlyConnectHomePage(context).SelectNorthEast();
+    public EarlyConnectHomePage GoToEarlyConnectLancashireAdvisorPage() => new EarlyConnectHomePage(context).SelectLancashire();
+    public EarlyConnectHomePage GoToEarlyConnectLondonAdvisorPage() => new EarlyConnectHomePage(context).SelectLondon();
+    public GetApprenticeshipAdviserPage GoToEarlyConnectEmailPage() => new GetApprenticeshipAdviserPage(context).GoToEmailAddressPage();
+    public EmailAuthCodePage GoToAddUniqueEmailAddressPage() => new EmailAddressPage(context).EnterNewEmailAddress();
+    public WhatsYourNamePage GoToCheckEmailAuthCodePage() => new EmailAuthCodePage(context).EnterValidAuthCode();
+    public DateOfBirthPage GoToWhatYourNamePage() => new WhatsYourNamePage(context).EnterFirstAndLastNames();
+    public PostcodePage GoToWhatIsYourDateOfBirthPage() => new DateOfBirthPage(context).EnterValidDateOfBirth();
+    public TelephonePage GoToPostCodePage() => new PostcodePage(context).EnterValidPostcode();
+    public AreasOfWorkInterestPage GoToWhatYourTelephonePage() => new TelephonePage(context).EnterValidTelephoneNumber();
+    public SchoolCollegePage GoToAreasOfWorkInterestPage() => new AreasOfWorkInterestPage(context).SelectAnyAreaInterestToYou();
+    public ApprenticeshipsLevelPage GoToEnterNameOfSchoolCollegePage() => new SchoolCollegePage(context).EnterInvalidSchoolOrCollegeName();
+    public ApprenticeshipsLevelPage GoToNameOfSchoolCollegePage() => new SchoolCollegePage(context).SearchValidSchoolOrCollegeName();
+    public HaveYouAppliedPage GoToApprencticeshipLevelPage() => new ApprenticeshipsLevelPage(context).SelectAnyApprenticeshipLevelInterestToYou();
+    public AreaOfEnglandPage GoToHaveYouAppliedPage() => new HaveYouAppliedPage(context).SelectAnyPastApplications();
+    public SupportPage GoToAreaOfEnglandPage() => new AreaOfEnglandPage(context).SelectYesForTheRightApprenticeship();
+    public CheckYourAnswerPage GoToSupportPage() => new SupportPage(context).SelectAnySupportThatAppliesToYou();
+    public ApplicantSurveySummitedPage GoToCheckYourAnswerPage() => new CheckYourAnswerPage(context).AcceptAndSubmitForm();
 }
