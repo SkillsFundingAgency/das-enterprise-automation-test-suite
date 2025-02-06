70ba3c0fb94a151ceec4ce9b7d1d9f75c5e40720
ShakilUmar <umar.shakil@education.gov.uk>
2025-02-06 11:56:46 +0000

    remove logging

--- src/SFA.DAS.FlexiPayments.E2ETests/Project/Helpers/Http/ApprenticeshipsClient.cs
+++ src/SFA.DAS.FlexiPayments.E2ETests/Project/Helpers/Http/ApprenticeshipsClient.cs
@@ -9,9 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TechTalk.SpecFlow;
 using SFA.DAS.UI.Framework.TestSupport;
-using SFA.DAS.Approvals.UITests.Project;
 using Newtonsoft.Json;
-using SFA.DAS.FrameworkHelpers;
 
 namespace SFA.DAS.FlexiPayments.E2ETests.Project.Helpers.Http
 {
@@ -19,20 +17,14 @@ namespace SFA.DAS.FlexiPayments.E2ETests.Project.Helpers.Http
     {
         private HttpClient _apiClient;
         private readonly string _functionKey;
-        private ObjectContext _objectContext;
 
         public ApprenticeshipsClient(ScenarioContext context)
         {
             var _config = context.GetPaymentsSimplificationConfig<PaymentsSimplificationConfig>();
             var baseUrl = _config.ApprenticeshipAzureFunctionBaseUrl;
 
-            _objectContext = context.Get<ObjectContext>();
-            _objectContext.Set("ApprenticeshipsBaseUrl", baseUrl);
-
-
             _apiClient = HttpClientProvider.GetClient(baseUrl);
             _functionKey = _config.ApprenticeshipAzureFunctionKey;
-            _objectContext.Set("ApprenticeshipsFunctionKey", _functionKey.Length >= 5 ? _functionKey.Substring(0, 5) + "---" + _functionKey.Substring(Math.Max(0, _functionKey.Length - 5)) : _functionKey);
         }
 
         public async Task WithdrawApprenticeship(WithdrawApprenticeshipRequestBody body)
