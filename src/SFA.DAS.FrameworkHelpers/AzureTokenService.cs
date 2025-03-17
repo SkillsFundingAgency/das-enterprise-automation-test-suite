3c9e646abfee8d8bc9c2a917ba6ccb174fd66d5d
REDDY, Harsha <Harsha.REDDY@EDUCATION.GOV.UK>
2025-03-17 10:47:19 +0000

    Revert azure tenant changes

--- src/SFA.DAS.FrameworkHelpers/AzureTokenService.cs
+++ src/SFA.DAS.FrameworkHelpers/AzureTokenService.cs
@@ -6,26 +6,13 @@ namespace SFA.DAS.FrameworkHelpers
 {
     public static class AzureTokenService
     {
-        private static readonly string tenantId = "3aacc835-4b83-483d-841d-cd787f6f1486";  //FCS
-        //private static readonly string tenantId = "1a92889b-8ea1-4a16-8132-347814051567"; //CDS
-
         public static string GetDatabaseAuthToken() => GetAzureToken("https://database.windows.net/").Result;
 
         public static string GetAppServiceAuthToken(string resource) => GetAzureToken(resource).Result;
 
         private static async Task<string> GetAzureToken(string resource)
         {
-            // Create DefaultAzureCredentialOptions and specify the tenant ID
-            var options = new DefaultAzureCredentialOptions
-            {
-                SharedTokenCacheTenantId = tenantId,
-                VisualStudioTenantId = tenantId,
-                VisualStudioCodeTenantId = tenantId,
-                ManagedIdentityClientId = tenantId,
-                InteractiveBrowserTenantId = tenantId,
-            };
-
-            var tokenCredential = new DefaultAzureCredential(options);
+            var tokenCredential = new DefaultAzureCredential();
 
             var accessToken = await tokenCredential.GetTokenAsync(new TokenRequestContext(scopes: new string[] { resource + "/.default" }) { });
 
