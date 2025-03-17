eb05012245e95e704f343959d54625b68eb2a032
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2025-03-17 11:54:55 +0000

    updated AzureTokenService

--- src/SFA.DAS.FrameworkHelpers/AzureTokenService.cs
+++ src/SFA.DAS.FrameworkHelpers/AzureTokenService.cs
@@ -6,26 +6,35 @@ namespace SFA.DAS.FrameworkHelpers
 {
     public static class AzureTokenService
     {
-        private static readonly string tenantId = "3aacc835-4b83-483d-841d-cd787f6f1486";  //FCS
+        //private static readonly string tenantId = "3aacc835-4b83-483d-841d-cd787f6f1486";  //FCS
         //private static readonly string tenantId = "1a92889b-8ea1-4a16-8132-347814051567"; //CDS
 
-        public static string GetDatabaseAuthToken() => GetAzureToken("https://database.windows.net/").Result;
+        public static string GetDatabaseAuthToken(string tenantid) => GetAzureToken("https://database.windows.net/", tenantid).Result;
 
         public static string GetAppServiceAuthToken(string resource) => GetAzureToken(resource).Result;
 
-        private static async Task<string> GetAzureToken(string resource)
+        private static async Task<string> GetAzureToken(string resource, string tenantid)
         {
             // Create DefaultAzureCredentialOptions and specify the tenant ID
             var options = new DefaultAzureCredentialOptions
             {
-                SharedTokenCacheTenantId = tenantId,
-                VisualStudioTenantId = tenantId,
-                VisualStudioCodeTenantId = tenantId,
-                ManagedIdentityClientId = tenantId,
-                InteractiveBrowserTenantId = tenantId,
+                SharedTokenCacheTenantId = tenantid,
+                VisualStudioTenantId = tenantid,
+                VisualStudioCodeTenantId = tenantid,
+                ManagedIdentityClientId = tenantid,
+                InteractiveBrowserTenantId = tenantid,
             };
 
             var tokenCredential = new DefaultAzureCredential(options);
+
+            var accessToken = await tokenCredential.GetTokenAsync(new TokenRequestContext(scopes: new string[] { resource + "/.default" }) { });
+
+            return accessToken.Token;
+        }
+
+        private static async Task<string> GetAzureToken(string resource)
+        {
+            var tokenCredential = new DefaultAzureCredential();
 
             var accessToken = await tokenCredential.GetTokenAsync(new TokenRequestContext(scopes: new string[] { resource + "/.default" }) { });
 
--- src/SFA.DAS.FrameworkHelpers/GetSqlConnectionHelper.cs
+++ src/SFA.DAS.FrameworkHelpers/GetSqlConnectionHelper.cs
@@ -6,6 +6,14 @@ public static class GetSqlConnectionHelper
 {
     internal static SqlConnection GetSqlConnection(string connectionString)
     {
-        return new() { ConnectionString = connectionString, AccessToken = connectionString.Contains("User ID=") ? null : AzureTokenService.GetDatabaseAuthToken() };
+        var tenantidkey = "TENANTID=";
+
+        var tenantidwithKey = connectionString.Split(';').ToList().Single(x=> x.StartsWith(tenantidkey, StringComparison.OrdinalIgnoreCase));
+
+        connectionString = connectionString.Replace(tenantidwithKey, string.Empty);
+
+        var tenantid = tenantidwithKey.Replace(tenantidkey, string.Empty);
+
+        return new() { ConnectionString = connectionString, AccessToken = connectionString.Contains("User ID=") ? null : AzureTokenService.GetDatabaseAuthToken(tenantid) };
     }
 }
