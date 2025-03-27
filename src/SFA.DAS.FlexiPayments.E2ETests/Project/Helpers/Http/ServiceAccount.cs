a8662a38db199b91879f577a864a717df26dc3aa
ShakilUmar <91058007+ShakilUmar@users.noreply.github.com>
2025-03-27 16:36:31 +0000

    Add functionality to generate a service bearer token to call Withdrawal endpoint (#1801)

---
+++ src/SFA.DAS.FlexiPayments.E2ETests/Project/Helpers/Http/ServiceAccount.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SFA.DAS.FlexiPayments.E2ETests.Project.Helpers.Http
+{
+    public class ServiceAccount
+    {
+        [JsonPropertyName("sub")]
+        public string? Sub { get; set; }
+    }
+
+}
