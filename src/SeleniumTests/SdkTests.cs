f84647d66fd2d7dcaf7e7ea77c64e063dea79d82
furqanagwan <furqanagwan@outlook.com>
2025-06-23 08:11:37 +0100

    test

--- src/SeleniumTests/SdkTests.cs
+++ src/SeleniumTests/SdkTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Chrome;
 using System;
 
 namespace SeleniumTests
@@ -8,13 +9,27 @@ namespace SeleniumTests
     [TestFixture]
     public class SdkTests
     {
-        IWebDriver driver;
+        private IWebDriver driver;
 
         [SetUp]
         public void SetUp()
         {
-            var caps = new DesiredCapabilities();
-            driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), caps);
+            // You can swap ChromeOptions for FirefoxOptions, etc.
+            var options = new ChromeOptions();
+            // If running on BrowserStack/Selenium Grid, add capabilities here:
+            // options.AddAdditionalOption("browserstack.user", "YOUR_USER");
+            // options.AddAdditionalOption("browserstack.key", "YOUR_KEY");
+            // options.AddAdditionalOption("os", "Windows");
+            // options.AddAdditionalOption("os_version", "10");
+            // options.AddAdditionalOption("browser", "Chrome");
+            // options.AddAdditionalOption("browser_version", "latest");
+
+            // If you're just testing locally, this URL should be your Selenium Grid/hub.
+            driver = new RemoteWebDriver(
+                new Uri("http://localhost:4444/wd/hub"),
+                options.ToCapabilities(), // <--- Important: pass options, not DesiredCapabilities
+                TimeSpan.FromSeconds(180)
+            );
         }
 
         [Test]
