a150f3a380eb486c9f88ab75dd8fa21968a4a5f4
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2025-09-23 09:41:24 +0100

    check for PP

--- src/SFA.DAS.ProviderLogin.Service/Project/Helpers/SetProviderCredsHelper.cs
+++ src/SFA.DAS.ProviderLogin.Service/Project/Helpers/SetProviderCredsHelper.cs
@@ -1,4 +1,5 @@
-﻿using SFA.DAS.DfeAdmin.Service.Project.Helpers.DfeSign.User;
+﻿using SFA.DAS.ConfigurationBuilder;
+using SFA.DAS.DfeAdmin.Service.Project.Helpers.DfeSign.User;
 using SFA.DAS.FrameworkHelpers;
 using System;
 using System.Collections.Generic;
@@ -25,9 +26,12 @@ internal static class SetProviderCredsHelper
 
         var providerName = dfeProviderDetailsList.FirstOrDefault(x => x.Ukprn == t.Ukprn);
 
-        provider.Username = $"{provider.UsernamePrefix}{t.Ukprn}@{provider.Domain}";
+        if (EnvironmentConfig.IsPPEnvironment)
+        {
+            provider.Username = $"{provider.UsernamePrefix}{t.Ukprn}@{provider.Domain}";
 
-        provider.Password = $"{provider.PasswordPrefix}{t.Ukprn}";
+            provider.Password = $"{provider.PasswordPrefix}{t.Ukprn}";
+        }       
 
         t.Username = provider.Username;
 
