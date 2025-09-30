40a6a36453845613fb862482a3d5c9e244110fea
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2025-09-30 14:10:49 +0100

    updated mfa sign in button

--- src/SFA.DAS.DfeAdmin.Service/Project/Tests/Pages/SignInBasePage.cs
+++ src/SFA.DAS.DfeAdmin.Service/Project/Tests/Pages/SignInBasePage.cs
@@ -36,7 +36,7 @@ public abstract class SignInBasePage(ScenarioContext context) : IdamsLoginBasePa
 
         private static By PasswordField => By.CssSelector("input[name=passwd][type=password]");
 
-        private static By MFASignInButton => By.CssSelector("input[type='submit'][value='Sign in']");
+        private static By MFASignInButton => By.CssSelector("[type='submit'][aria-label='Sign in']");
 
         public void SubmitValidPassword(string password)
         {
