42119caf07e241b7b9abae04df7407860a3f3237
REDDY, Harsha <Harsha.REDDY@EDUCATION.GOV.UK>
2025-10-08 16:06:50 +0100

    FAi-1237 2779 Add tests for email preferences pages; new notifications

--- src/SFA.DAS.RAAEmployer.UITests/Project/Tests/Pages/Employer/ManageYourEmailsPage.cs
+++ src/SFA.DAS.RAAEmployer.UITests/Project/Tests/Pages/Employer/ManageYourEmailsPage.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Azure.Core;
+using HtmlAgilityPack;
+using OpenQA.Selenium;
 using SFA.DAS.RAA.Service.Project.Tests.Pages;
 using TechTalk.SpecFlow;
 
@@ -10,6 +9,27 @@ namespace SFA.DAS.RAAEmployer.UITests.Project.Tests.Pages.Employer
 {
     public class ManageYourEmailsPage(ScenarioContext context) : RaaBasePage(context)
     {
-        protected override string PageTitle => "Manage your emails";
+        protected override string PageTitle => "Manage your advert notifications";
+        private static By NotificationBannerTitleLocator => By.CssSelector(".govuk-notification-banner__title");
+        private static By NotificationBannerHeadingLocator => By.CssSelector(".govuk-notification-banner__heading");
+
+        public ManageYourEmailsPage SelectAndSaveEmailPreferences()
+        {
+            SelectRadioOptionByForAttribute("approved-rejected-mine");
+            SelectRadioOptionByForAttribute("applications-mine");
+            SelectRadioOptionByForAttribute("notify-now");
+
+            formCompletionHelper.ClickButtonByText(SaveAndContinueButton, "Save settings");
+            return this;
+        }
+
+        public ManageYourEmailsPage VerifyEmailSettingsConfirmationBanner()
+        {
+            var ExpectedBannerTitle = "Success";
+            var ExpectedBannerHeading = "Advert notification settings saved.";
+            pageInteractionHelper.VerifyText(NotificationBannerTitleLocator, ExpectedBannerTitle);
+            pageInteractionHelper.VerifyText(NotificationBannerHeadingLocator, ExpectedBannerHeading);
+            return this;
+        }
     }
 }
---
+++ src/SFA.DAS.RAAProvider.UITests/Project/Tests/Features/EmailNotificationSettings/RAA_P_EN_01.feature
@@ -0,0 +1,11 @@
+﻿Feature: RAA_P_EN_01
+
+@raa
+@raaprovider
+@raae2e
+@raaprovidere2e
+@regression
+Scenario: RAA_P_EN_01 - Navigate to email settings page and set the preferences
+	Given the Provider navigates to 'Recruit' Page
+	Then the Provider can navigate to Manage your recruitment emails page
+	Then the provider sets the email preferences
