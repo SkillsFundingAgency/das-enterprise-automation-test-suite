2eb9f71cbdfe24f3f218557d9f5d45d8659461c5
sony-kotha <82891925+sony-kotha@users.noreply.github.com>
2026-01-12 16:53:09 +0000

    RemoveDurationReducedByFromRPL (#1890)

--- src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Provider/ProviderRPLDetailsPage.cs
+++ src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Provider/ProviderRPLDetailsPage.cs
@@ -10,15 +10,15 @@ namespace SFA.DAS.Approvals.UITests.Project.Tests.Pages.Provider
         protected override By ContinueButton => By.CssSelector("#main-content .govuk-button");
         private static By TrainingTotalHoursTextBox => By.Id("TrainingTotalHours");
         private static By DurationReducedByHoursTextBox => By.Id("DurationReducedByHours");
-        private static By DurationReducedByTextBox => By.Id("DurationReducedBy");
+       // private static By DurationReducedByTextBox => By.Id("DurationReducedBy");
         private static By PriceReduced => By.Id("PriceReduced");
 
         public void EnterRPLDataAndContinue()
         {
             formCompletionHelper.EnterText(TrainingTotalHoursTextBox, RPLDataHelper.TrainingTotalHours);
             formCompletionHelper.EnterText(DurationReducedByHoursTextBox, RPLDataHelper.DurationReducedByHours);
-            formCompletionHelper.SelectRadioOptionByText("Yes");
-            formCompletionHelper.EnterText(DurationReducedByTextBox, RPLDataHelper.DurationReducedBy);
+          //  formCompletionHelper.SelectRadioOptionByText("Yes");
+           // formCompletionHelper.EnterText(DurationReducedByTextBox, RPLDataHelper.DurationReducedBy);
             formCompletionHelper.EnterText(PriceReduced, RPLDataHelper.PriceReducedBy);
 
             Continue();
