5193344f2711cc8608433826ab9960d0343b62df
Najamuddin-Muhammad <najamuddin.muhammad@education.gov.uk>
2026-02-17 11:00:56 +0000

    Misc. fixes

--- src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Employer/NotificationSentToTrainingProviderPage.cs
+++ src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Employer/NotificationSentToTrainingProviderPage.cs
@@ -12,7 +12,7 @@ namespace SFA.DAS.Approvals.UITests.Project.Tests.Pages.Employer
         protected override bool TakeFullScreenShot => false;
 
         protected override By PageHeader => PanelTitle;
-        private static By DynamicHomeLink => By.CssSelector(".das-navigation__list-item");
+        private static By DynamicHomeLink => By.CssSelector(".mu-home");
 
         public DynamicHomePages ClickHomeLink()
         {
