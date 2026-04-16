67b6e9fc36c17930aefaf5d5de9e3e8159b75097
sony-kotha <sony.kotha@education.gov.uk>
2026-04-16 12:20:23 +0100

    Update FilteredManageYourApprenticesPage.cs

--- src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Common/FilteredManageYourApprenticesPage.cs
+++ src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/Common/FilteredManageYourApprenticesPage.cs
@@ -5,7 +5,7 @@ namespace SFA.DAS.Approvals.UITests.Project.Tests.Pages.Common
 {
     public class FilteredManageYourApprenticesPage(ScenarioContext context) : ApprovalsBasePage(context)
     {
-        protected override string PageTitle => "Manage your Apprentices";
+        protected override string PageTitle => "Manage your apprentices";
 
         protected override bool TakeFullScreenShot => false;
 
