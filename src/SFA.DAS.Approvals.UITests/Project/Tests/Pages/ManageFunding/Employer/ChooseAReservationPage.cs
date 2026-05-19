260a6d01a7d5bbe5e206165a8f2dac995bbcba4d
AshokKumarDfe <ashok.kumar@education.gov.uk>
2026-05-19 11:23:57 +0100

    fixed "Choose a reservation"

--- src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/ManageFunding/Employer/ChooseAReservationPage.cs
+++ src/SFA.DAS.Approvals.UITests/Project/Tests/Pages/ManageFunding/Employer/ChooseAReservationPage.cs
@@ -6,7 +6,7 @@ namespace SFA.DAS.Approvals.UITests.Project.Tests.Pages.ManageFunding.Employer
 {
     public class ChooseAReservationPage(ScenarioContext context) : ApprovalsBasePage(context)
     {
-        protected override string PageTitle => "Choose a Reservation";
+        protected override string PageTitle => "Choose a reservation";
 
         protected override bool TakeFullScreenShot => false;
 
